75d32b6d35ecb2f8d23018bf3c3ca49669b1c3b3
Marcos Torres <marcostor13@gmail.com>
2019-09-05 14:06:58 -0500

    HOla

--- resources/views/landings/mafchile.blade.php
+++ resources/views/landings/mafchile.blade.php
@@ -28,7 +28,7 @@
                 </p>
                 <div class="date btn bg-danger text-white col-12 mt-4">
                     <span>AGENDAR COMPROMISO DE PAGO</span>
-                    <input id="date1"  type="date" class="btn-date text-danger" style="border: none;" />
+                    <input id="date1"  type="date" class="btn-date text-danger" style="border: none;" min="<?php echo date('Y-m-d') ?>"/>
                 </div>
                 <button onclick='window.location.href="https://www.mafchile.com/client/login"' class="btn bg-danger text-white col-12 mt-4">PAGAR AHORA</button>
                 <button onclick="sendMail('El cliente indica que ya pagó'); return false;" class="btn bg-danger text-white col-12 mt-4">YA PAGUE</button>
@@ -36,103 +36,107 @@
                     <a href="https://api.whatsapp.com/send?phone=56985296912&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/whatsapp.png"></a>
                     <a href="tel:+56985296912"><img width="40" src="https://img.icons8.com/wired/64/FFFFFF/phonelink-ring.png"></a>
                     <a href="mailto:contacto@binteraction.com"><img width="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/email.png"></a>
-
                 </div>
+                <h5 id="message"class="text-white text-center mt-5 hide"></h5>
             </div>
 
         </div>
-
-        
-        <script>
-
-            //EVENT 1
-            
-
-            let event1 = function(){
-                               
-                let dataSend = {
-                    'fourRut': $('#rut').val()
-                } 
-                
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-                $.post( "/validateRut", dataSend,function() {
-                    console.log(dataSend);
-                })
-                .done(function(e) {
-                    console.log(e);
-                    e = JSON.parse(e); 
-
-                    if(e.code == 200){
-                        $('#cont1').addClass('hide');
-                        $('#name').text(e.data.name);
-                        $('#pay').text(e.data.pay);
-                        $('#cont2').removeClass('hide');
-                    }else{
-                        $('#error').text(e.msg);
-                    }
-                })
-                .fail(function() {
-                    console.log( "error" );
-                });
-
-            }
-
-            let sendMail = function(msg = false){
-                
-                let data; 
-                if(msg !== false){
-                    data = {
-                        'mensaje': msg,
-                        'nombre': $('#name').text(),
-                        'saldo': $('#pay').text()
-                    }
-                }else{
-                    let date = $('#date1').val();
-                    data = {
-                        'fecha': date,
-                        'nombre': $('#name').text(),
-                        'saldo': $('#pay').text()
-                    } 
-                }
-
-                let dataSend = {
-                    'data': JSON.stringify(data),
-                    'email': 'contacto@binteraction.com'
-                }
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-                $.post( "/sendMail", dataSend,function() {
-                    console.log(dataSend);
-                })
-                .done(function(e) {
-                    console.log(e);
-                })
-                .fail(function() {
-                    console.log( "error" );
-                });
-
-            }
-
-            $('#date1').change(sendMail());
-            
-
-            
-
-            
-           
-        
-        </script>
-
-                
-
     </div>
+
+    <script>
+
+        //EVENT 1
+        
+
+        function event1(){
+                            
+            let dataSend = {
+                'fourRut': $('#rut').val()
+            } 
+            
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.post( "/validateRut", dataSend,function() {
+                console.log(dataSend);
+            })
+            .done(function(e) {
+                console.log(e);
+                e = JSON.parse(e); 
+
+                if(e.code == 200){
+                    $('#cont1').addClass('hide');
+                    $('#name').text(e.data.name);
+                    $('#pay').text(e.data.pay);
+                    $('#cont2').removeClass('hide');
+                    $('#date1').on('change', function(){
+                        if($('#date1').val() != ''){
+                            sendMail();
+                        }
+                    });
+                }else{
+                    $('#error').text(e.msg);
+                }
+            })
+            .fail(function() {
+                console.log( "error" );
+            });
+
+        }
+
+        function sendMail(msg = false){
+            
+            let data; 
+            if(msg !== false){
+                data = {
+                    'mensaje': msg,
+                    'nombre': $('#name').text(),
+                    'saldo': $('#pay').text()
+                }
+            }else{
+                let date = $('#date1').val();
+                data = {
+                    'fecha': date,
+                    'nombre': $('#name').text(),
+                    'saldo': $('#pay').text()
+                } 
+            }
+
+            let dataSend = {
+                'data': JSON.stringify(data),
+                // 'email': 'contacto@binteraction.com'
+                'email': 'marcostor13@gmail.com'
+            }
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.post( "/sendMail", dataSend,function() {
+                console.log(dataSend);
+                if(msg !== false){
+                    $('#message').removeClass('hide');
+                    $('#message').text('Gracias por su información');
+                }else{
+                    $('#message').removeClass('hide');
+                    $('#message').text('Gracias, Su compromiso de pago fue agendado');
+                }
+            })
+            .done(function(e) {
+                console.log(e);
+            })
+            .fail(function() {
+                console.log( "error" );
+            });
+
+        }
+
+        
+        
+    
+    </script>
     
 
 @endsection
