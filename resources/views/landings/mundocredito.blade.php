a23412a4e009ac77f5ea1372cdb312db0e7cde65
jsuscortz <cortezjesus01@gmail.com>
2019-09-13 14:13:46 -0300

    Update mundocredito.blade.php

--- resources/views/landings/mundocredito.blade.php
+++ resources/views/landings/mundocredito.blade.php
@@ -10,7 +10,8 @@
 }
 .input{
 
-     background-color: #3490DC !important;
+    outline: none;
+   border: 1px solid #A1CBF3;
 }
 </style>
     <div id="mafchile" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;">
@@ -20,7 +21,7 @@
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
             <img class="img-fluid col-10 col-md-3 mt-2" src="{{$landing->logo}}" alt="mundocredito">
 
-            <div id="cont1" class="p-5 ">|
+            <div id="cont1" class="p-5 ">
                 <h3 class="text-white text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
                 <input id="rut" type="number" class="form-control mt-4 text-center text-blue input">
                 <button onclick="event1(); return false;" class="btn bg-primary text-white col-12 mt-4">Validar</button>
