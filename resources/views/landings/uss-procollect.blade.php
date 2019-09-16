5f979a690e3932e29e462f36b8ae00a44b81d8d5
jsuscortz <cortezjesus01@gmail.com>
2019-09-16 10:26:11 -0300

    Update uss-procollect.blade.php
    error en correos procollect

--- resources/views/landings/uss-procollect.blade.php
+++ resources/views/landings/uss-procollect.blade.php
@@ -135,8 +135,8 @@
 
             let dataSend = {
                 'data': JSON.stringify(data),
-                'email': 'contacto@binteraction.com',
-                // 'email': {!! $landing->email !!}
+                //'email': 'contacto@binteraction.com',
+                'email': {!! $landing->email !!}
                 //'email': 'marcostor13@gmail.com'
             }
             $.ajaxSetup({
