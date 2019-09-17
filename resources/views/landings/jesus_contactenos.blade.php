d611163e0165d5f5d070b48d99de5a4c10d1b794
jsuscortz <cortezjesus01@gmail.com>
2019-09-17 09:59:25 -0300

    Update jesus_contactenos.blade.php

--- resources/views/landings/jesus_contactenos.blade.php
+++ resources/views/landings/jesus_contactenos.blade.php
@@ -6,9 +6,6 @@
 <?php
 $startdate=strtotime("Today");
 $enddate=strtotime("+5 days", $startdate);
-// echo date("M d", $startdate) . "<br>";
-  // echo date("Y-m-d", $enddate) . "<br>";
- 
 ?>
     <div id="mafchile" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;">
 
@@ -50,8 +47,6 @@ $enddate=strtotime("+5 days", $startdate);
     </div>
    <script>
 
-        
-
         $(function(){
             events({    
                 'name': 'Visita',
@@ -60,8 +55,7 @@ $enddate=strtotime("+5 days", $startdate);
             });
         }); 
 
-
-        let eventosLanding = function(name, redireccion = false){
+        let eventosLanding = function(name){
             
             let json_datos = getAllUrlParameter(); 
 
@@ -73,19 +67,15 @@ $enddate=strtotime("+5 days", $startdate);
                 'landing_id': {!! $landing->id !!},
                 'json_datos': JSON.stringify(json_datos)
             });
-            if(redireccion != false){
-
-                window.location.href=redireccion; 
-            }
-
-            
         }
+
+        //EVENT 1
         
 
-        function event1() {
+         function event1(){
                             
             let dataSend = {
-                'fourRut': $('#rut').val()
+                'fourRut': $('#rut').val(),
                 'phone': getUrlParameter('telefono'),
             } 
             
@@ -121,7 +111,9 @@ $enddate=strtotime("+5 days", $startdate);
 
         }
 
-        let sendMail = function(msg = false){
+        function sendMail(msg = false){
+
+            console.log({!! $landing->name !!}); 
             
             let data; 
             if(msg !== false){
@@ -135,13 +127,15 @@ $enddate=strtotime("+5 days", $startdate);
                 data = {
                     'fecha': date,
                     'nombre': $('#name').text(),
-                    'saldo': $('#pay').text()
+                    'saldo': $('#pay').text(),
+                    'landing': {!! $landing->name !!}
                 } 
             }
 
             let dataSend = {
                 'data': JSON.stringify(data),
-                'email': '{!!$landing->email!!}'
+                //'email': 'contacto@binteraction.com',
+                'email': {!! $landing->email !!}
                 //'email': 'marcostor13@gmail.com'
             }
             $.ajaxSetup({
@@ -167,7 +161,6 @@ $enddate=strtotime("+5 days", $startdate);
             });
 
         }
-
 
         let events = function(data){     
 
