60545a3d8fb7f3da0570a10e9aaff70c71c0cb2c
jsuscortz <cortezjesus01@gmail.com>
2019-09-17 11:25:00 -0300

    Update jesus_contactenos.blade.php
    cambio

--- resources/views/landings/jesus_contactenos.blade.php
+++ resources/views/landings/jesus_contactenos.blade.php
@@ -135,7 +135,6 @@ $enddate=strtotime("+5 days", $startdate);
 
             let dataSend = {
                 'data': JSON.stringify(data),
-                //'email': 'contacto@binteraction.com',
                 'email': '{!! $landing->email !!}'
                 //'email': 'marcostor13@gmail.com'
             }
