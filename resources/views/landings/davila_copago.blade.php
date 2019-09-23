01ff788fc39f12c1150ae2c93c1c4e787603522f
jsuscortz <cortezjesus01@gmail.com>
2019-09-23 14:23:31 -0300

    Update davila_copago.blade.php
    re_val_copago

--- resources/views/landings/davila_copago.blade.php
+++ resources/views/landings/davila_copago.blade.php
@@ -34,10 +34,10 @@ $enddate=strtotime("+5 days", $startdate);
                     <b> El costo es de:  </b><b id="pay"></b>
                     Te ofrecemos las siguientes opciones para regularizar tu deuda pendiente
                 </p>
-                <!--<div class="date btn bg-danger text-white col-12 mt-4">
+                <div class="date btn bg-danger text-white col-12 mt-4">
                     <span>AGENDAR COMPROMISO DE PAGO</span>
                     <input id="date1"  type="date" class="btn-date text-danger" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
-                </div>-->
+                </div>
                 <button onclick='window.location.href="http://solucionesweb.clinicadavila.cl/Cta001cW/PagoCuentas/Login.aspx/"' class="btn bg-danger text-white col-12 mt-4">PAGAR AHORA</button>
                 
                 <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-danger text-white col-12 mt-4">YA PAGUE</button>
