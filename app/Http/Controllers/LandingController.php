dd11d8b8e12fe63c7a1d06ba9236c25f96bef262
jsuscortz <cortezjesus01@gmail.com>
2019-09-28 01:34:28 -0300

    Update LandingController.php

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -65,8 +65,8 @@ class LandingController extends Controller
         $landing_id = $request->input('landing_id'); 
         
         $data = DatosLanding::where('rut', 'LIKE', $fourRut.'%')     
-                    ->where('landing_id', $landing_id);              
-                    //->first();
+                    ->where('landing_id', $landing_id)               
+                    ->first();
        
         if(is_object($data) && $fourRut == substr($data->rut, 0, 4)){
             if(isset($request['id'])){
