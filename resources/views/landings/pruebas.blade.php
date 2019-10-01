3518357d84757f1f3a9759fea915d8c7c4880a47
jsuscortz <cortezjesus01@gmail.com>
2019-10-01 12:51:32 -0300

    Update pruebas.blade.php

--- resources/views/landings/pruebas.blade.php
+++ resources/views/landings/pruebas.blade.php
@@ -1,10 +1,17 @@
-@extends('layouts.landing')
+@xtends('layouts.landing')
 
-@section('title', 'CLINICA DAVILA')
+@section('title', 'Mundo Credito')
 
 @section('content')
-
 <style type="text/css">
+.opaco{
+    display: none !important; 
+
+}
+.input{
+  border-color: #A1CBF3 !important;
+    
+}
     
 .card {
   box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
@@ -42,58 +49,51 @@
     }
     
 </style>
+    <div id="mafchile" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;">
 
-     <div id="CLINICA_DAVILA" class="oscurecer" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;" >
+        <div class="opaco"></div>
 
-     <div class="opaco">
+        <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
+            <img class="img-fluid col-10 col-md-3 mt-2" src="{{$landing->logo}}" alt="mundocredito">
 
-        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-            <img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="CLINICA_DAVILA">
-            
-            <div class=col-md-12>
-           
-                     <div id="cont1" class="p-2 pl-5 pr-5">
-                         <div class="card">
-                        <div class="card-body">
-                        <h5 class="text-grey text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h5>
-                        <input id="rut" type="number" class="form-control mt-4 text-center text-grey">
-                        <button onclick="event1(); return false;" class="btn bg-primary text-white col-12 mt-4">Validar</button>
-                        <h5 id="error"class="text-grey text-center mt-5"></h5>
-                       </div>
-                       </div> 
-                    </div>
+            <div id="cont1" class="p-5 ">
+                <h3 class="text-white text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
+                <input id="rut" type="number" class="form-control mt-4 text-center text-blue input" autofocus>
+                <button onclick="event1(); return false;" class="btn bg-primary text-white col-12 mt-4">Validar</button>
+                <h5 id="error"class="text-white text-center mt-5"></h5>
+            </div>
 
+            <div id="cont2" class="p-5 hide">
+                <h3 class="text-black text-center">Estimado <span id="name"></span></h3>
 
-            <div id="cont2" class="p-5 hide">|
-                <h3 class="text-grey text-center">Estimado(a) <span id="name"></span></h3>
-
-                <p class="text-grey">
-                    Te informamos que presentas un retraso en el pago tus cuotas por un monto total de, mas recargos por mora:
-                    <b>$ </b><b id="pay"></b>
-                    Te ofrecemos las siguientes opciones para regularizar tu deuda pendiente
+                <p class="text-black">
+                   Te informamos que presentas un retraso en el pago de cuotas de tu crédito automotriz.
+                    
+                    Te ofrecemos las siguientes opciones para regularizar tu deuda pendiente.
                 </p>
                 <div class="date btn bg-primary text-white col-12 mt-4">
                     <span>AGENDAR COMPROMISO DE PAGO</span>
-                    <input id="date1"  type="date" class="btn-date text-white" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
+                    <input id="date1"  type="date" class="btn-date text-primary" style="border: none;" min="<?php echo date('Y-m-d') ?>"/>
                 </div>
-                <button onclick='window.location.href="https://pagos.uss.cl/"' class="btn bg-primary text-white col-12 mt-4">PAGAR AHORA</button>
-                
+                <button onclick='window.location.href="http://www.mundocredito.cl/pagos.html"' class="btn bg-primary text-white col-12 mt-4">PAGAR AHORA</button>
                 <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-primary text-white col-12 mt-4">YA PAGUE</button>
-                
-                <div class="d-flex justify-content-around align-content-center mt-4">
-                    <a onclick="eventosLanding('Whatsapp', 'https://api.whatsapp.com/send?phone=+56985296912&text=Hola,%20tengo%20una%20consulta')"><img width="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/whatsapp.png"></a>
-                    <a onclick="eventosLanding('Llamar', 'tel:+56985296912') "><img width="40" src="https://img.icons8.com/wired/64/FFFFFF/phonelink-ring.png"></a>
-                   <a onclick="eventosLanding('Correo', 'mailto:gbahamondes@gmail.com');" ><img width="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/email.png"></a>
+                <div class="d-flex justify-content-around align-content-center mt-4"> 
+                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56985296912&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/whatsapp.png"></a>
+                    <a onclick="eventosLanding('Llamar');" href="tel:+56985296912"><img width="40" src="https://img.icons8.com/wired/64/FFFFFF/phonelink-ring.png"></a>
+                    <a onclick="eventosLanding('Correo');" href="mailto:contacto@binteraction.com"><img width="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/email.png"></a>
                 </div>
                 
             </div>
-           <div id="cont3" class="p-2 pl-5 pr-5 hide">
-                        <div class="card">     
-                            <div class="card-body">
-                                <h5 id="message"class="text-grey text-center mt-3 hide"></h5>
-                            </div>
-                        </div>
+            <div id="cont3" class="p-2 pl-5 pr-5 hide">
+                <div class="card">     
+                    <div class="card-body">
+                        <h5 id="message"class="text-grey text-center mt-3 hide"></h5>
                     </div>
+                </div>
+            </div>
+
+
+
         </div>
     </div>
 
