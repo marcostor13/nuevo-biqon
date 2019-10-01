98978b8ca076bacde365c23b817a25dd5aedd9b9
jsuscortz <cortezjesus01@gmail.com>
2019-10-01 14:43:41 -0300

    Update davila_copago01.blade.php
    name_image

--- resources/views/landings/davila_copago01.blade.php
+++ resources/views/landings/davila_copago01.blade.php
@@ -85,9 +85,9 @@
                                 </button>-->
                                     <div class="d-flex justify-content-around align-content-center mt-4">
                                                        
-                                    <a onclick="eventosLanding('Llamar');" href="tel:+56967664209"><img width="60" src="https://c7.uihere.com/files/292/768/978/call-centre-cartoon-customer-service-stock-photography-secret-agent-thumb.jpg"></a>
+                                    <a onclick="eventosLanding('Llamar');" href="tel:+56967664209">Contactar<img width="75" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
                                    
-                                    <a onclick="eventosLanding('Correo');" href="mailto:cdavila.binteraction@gmail.com"><img width="60" src="http://ojwgq1ostm42ulxuw45kfbt8-wpengine.netdna-ssl.com/hoosnetwork/wp-content/uploads/sites/30/2015/02/Email-image.jpg"></a>
+                                    <a onclick="eventosLanding('Correo');" href="mailto:cdavila.binteraction@gmail.com">Email<img width="75" src="http://ojwgq1ostm42ulxuw45kfbt8-wpengine.netdna-ssl.com/hoosnetwork/wp-content/uploads/sites/30/2015/02/Email-image.jpg"></a>
                                     </div>
 
                                <!--<div class="d-flex justify-content-around align-content-center mt-4">
