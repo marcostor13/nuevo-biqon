eb40ba682772a231e264de4ed9ce894ed222eff2
jsuscortz <cortezjesus01@gmail.com>
2019-10-01 11:07:15 -0300

    Update pruebas.blade.php

--- resources/views/landings/pruebas.blade.php
+++ resources/views/landings/pruebas.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.landing')
 
-@section('title', 'USS')
+@section('title', 'Puebas')
 
 @section('content')
 <?php
@@ -9,9 +9,6 @@ $enddate=strtotime("+5 days", $startdate);
 ?>
 
 <style type="text/css">
-  .opaco{
-    display: none !important; 
-
 }
 .input{
   border-color: #A1CBF3 !important;
@@ -63,26 +60,26 @@ $enddate=strtotime("+5 days", $startdate);
 
              <div id="cont1" class="p-5 ">|
                 <h3 class="text-white text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
-                <input id="rut" type="number" class="form-control mt-4 text-center text-white">
-                <button onclick="event1(); return false;" class="btn bg-danger text-white col-12 mt-4">Validar</button>
+                <input id="rut" type="number" class="form-control mt-4 text-center text-grey">
+                <button onclick="event1(); return false;" class="btn bg-primary text-white col-12 mt-4">Validar</button>
                 <h5 id="error"class="text-white text-center mt-5"></h5>
             </div>
 
             <div id="cont2" class="p-5 hide">|
                 <h3 class="text-white text-center">Estimado(a) <span id="name"></span></h3>
 
-                <p class="text-white">
+                <p class="text-grey">
                     Te informamos que presentas un retraso en el pago tus cuotas por un monto total de, mas recargos por mora:
                     <b>$ </b><b id="pay"></b>
                     Te ofrecemos las siguientes opciones para regularizar tu deuda pendiente
                 </p>
-                <div class="date btn bg-danger text-white col-12 mt-4">
+                <div class="date btn bg-primary text-white col-12 mt-4">
                     <span>AGENDAR COMPROMISO DE PAGO</span>
-                    <input id="date1"  type="date" class="btn-date text-danger" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
+                    <input id="date1"  type="date" class="btn-date text-white" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
                 </div>
-                <button onclick='window.location.href="https://pagos.uss.cl/"' class="btn bg-danger text-white col-12 mt-4">PAGAR AHORA</button>
+                <button onclick='window.location.href="https://pagos.uss.cl/"' class="btn bg-primary text-white col-12 mt-4">PAGAR AHORA</button>
                 
-                <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-danger text-white col-12 mt-4">YA PAGUE</button>
+                <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-primary text-white col-12 mt-4">YA PAGUE</button>
                 
                 <div class="d-flex justify-content-around align-content-center mt-4">
                     <a onclick="eventosLanding('Whatsapp', 'https://api.whatsapp.com/send?phone=+56985296912&text=Hola,%20tengo%20una%20consulta')"><img width="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/whatsapp.png"></a>
