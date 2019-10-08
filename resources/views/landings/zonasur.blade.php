095946772bbfb9371d800e57c5e208f0e18704a1
jsuscortz <cortezjesus01@gmail.com>
2019-10-08 17:52:55 -0300

    Update zonasur.blade.php
    varzs

--- resources/views/landings/zonasur.blade.php
+++ resources/views/landings/zonasur.blade.php
@@ -69,7 +69,7 @@ $enddate=strtotime("+5 days", $startdate);
                 <h3 class="text-black text-center">Estimado <span id="name"></span></h3>
 
                 <h4><p class="text-black">
-                   Te informamos que presentas un retraso en el pago de cuotas de tu crédito por un monto total de: <b>$ </b><b id="pay"></b> mas recargos por mora.
+                   Te informamos que presentas un retraso en el pago de cuotas de tu crédito por un monto nominal de: <b>$ </b><b id="pay"></b>, mas recargos por mora.
                      
                     Te ofrecemos las siguientes opciones para regularizar tu deuda pendiente.
                 </p></h4>
