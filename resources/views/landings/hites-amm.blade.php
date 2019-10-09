e5eda48527435db0d2a3a998914f8f7959bdab40
jsuscortz <cortezjesus01@gmail.com>
2019-10-09 19:10:30 -0300

    Update hites-amm.blade.php
    var

--- resources/views/landings/hites-amm.blade.php
+++ resources/views/landings/hites-amm.blade.php
@@ -19,7 +19,7 @@
 
             <div id="cont1">|
                 <div class="videoWrapper">        
-                    <iframe width="460" height="320" src="{{asset('/files/hites_amm.mp4')}}" frameborder="0" autoplay allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="420" height="320" src="{{asset('/files/hites_amm.mp4')}}" frameborder="0" autoplay allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 
             </div>
