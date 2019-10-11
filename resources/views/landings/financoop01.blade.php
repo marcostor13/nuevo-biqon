9ab4795762f7336a3f84e82893c66318b6354032
jsuscortz <cortezjesus01@gmail.com>
2019-10-11 16:30:37 -0300

    Update financoop01.blade.php
    var02

--- resources/views/landings/financoop01.blade.php
+++ resources/views/landings/financoop01.blade.php
@@ -96,7 +96,7 @@ $enddate=strtotime("+5 days", $startdate);
                 <button onclick="sendMail('El cliente indica que desea ser contactado'); eventosLanding('contacto'); return false;" class="btn bg-warning text-black col-12 mt-4">CLICK AQUÍ SI DESEAS QUE TE CONTACTEMOS</button>
                 
                 <div class="d-flex justify-content-around align-content-center mt-4"> 
-                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56985296912text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
+                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56985296912&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
                    
                     <a onclick="eventosLanding('Llamar');" href="tel:+56985296912"><img width="40" src="https://i.imgur.com/Oh5DCRW.png"></a>
                    
