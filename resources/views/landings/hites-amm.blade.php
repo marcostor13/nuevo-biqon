42910a163321e8c51cd3c3bd86432de0942fc721
jsuscortz <cortezjesus01@gmail.com>
2019-10-11 11:54:56 -0300

    Update hites-amm.blade.php
    hites

--- resources/views/landings/hites-amm.blade.php
+++ resources/views/landings/hites-amm.blade.php
@@ -15,7 +15,7 @@
         <div class="opaco"></div>
 
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-            <img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="CLINICA_DAVILA">
+            <img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="HITES">
 
             <div id="cont1">|
                 <div class="videoWrapper">        
