2712980161c35f09f348e9fad9d566a0071e07a6
jsuscortz <cortezjesus01@gmail.com>
2019-10-11 16:23:48 -0300

    Update detacoop.blade.php
    lgo

--- resources/views/landings/detacoop.blade.php
+++ resources/views/landings/detacoop.blade.php
@@ -98,7 +98,7 @@ $enddate=strtotime("+5 days", $startdate);
                 <div class="d-flex justify-content-around align-content-center mt-4"> 
                     <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56985296912text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
                    
-                    <a onclick="eventosLanding('Llamar');" href="tel:+56985296912"><img width="40" src="https://i.imgur.com/Oh5DCRW.png"></a>
+                    <a onclick="eventosLanding('Llamar');" href="tel:+56985296912"><img width="40" src="https://image.flaticon.com/icons/png/512/40/40316.png"></a>
                    
                     <a onclick="eventosLanding('Correo');" href="mailto: contacto@binteraction.com"><img width="40" src="https://es.seaicons.com/wp-content/uploads/2015/10/Email-icon.png"></a>
                 </div>
