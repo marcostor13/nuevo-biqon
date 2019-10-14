cd6eece7bf23b432664f6c43ca1bc6c4aac7b704
jsuscortz <cortezjesus01@gmail.com>
2019-10-14 17:10:49 -0300

    Update fonasa.blade.php
    fsd

--- resources/views/landings/fonasa.blade.php
+++ resources/views/landings/fonasa.blade.php
@@ -51,7 +51,7 @@ $enddate=strtotime("+5 days", $startdate);
     )
     }
 </style>
-    <div id="HerediaAbogados" class="oscurecer p-5" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;" >
+    <div id="HerediaAbogados" class="oscurecer p-5" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: contain;" >
 
         <div class="opaco"></div>
 
