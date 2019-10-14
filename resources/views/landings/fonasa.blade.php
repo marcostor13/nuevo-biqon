96ca236f04335354947572d60342cf6949d18e76
jsuscortz <cortezjesus01@gmail.com>
2019-10-14 15:20:46 -0300

    Update fonasa.blade.php
    descmas01

--- resources/views/landings/fonasa.blade.php
+++ resources/views/landings/fonasa.blade.php
@@ -188,6 +188,7 @@ $enddate=strtotime("+5 days", $startdate);
                     'monto': getUrlParameter('monto'),
                     'RUT': getUrlParameter('rut'),
                     'Telefono': getUrlParameter('telefono'),
+                    'landing': '{!! $landing->name !!}'
                    
                     
                 }
