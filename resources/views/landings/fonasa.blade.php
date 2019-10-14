06db61429607e8ec72d4c06f55b7ee309cc063d1
jsuscortz <cortezjesus01@gmail.com>
2019-10-14 17:56:45 -0300

    Update fonasa.blade.php
    lg_mens

--- resources/views/landings/fonasa.blade.php
+++ resources/views/landings/fonasa.blade.php
@@ -55,7 +55,7 @@ $enddate=strtotime("+5 days", $startdate);
     <div id="HerediaAbogados" class="oscurecer" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: 100% 100%;" >
 
        <!-- <div class="opaco"></div>-->
-<div class="p-5"></div>
+
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
             <img style="max-width: 50%" class="img-fluid col-md-3 mt-1" src="{{$landing->logo}}" alt="HerediaAbogados">
 
