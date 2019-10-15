caf8de9f1e8125f7d49bfb79d03f9e002895a6f8
jsuscortz <cortezjesus01@gmail.com>
2019-10-15 10:50:08 -0300

    Update maipo.blade.php
    maxlogo

--- resources/views/landings/maipo.blade.php
+++ resources/views/landings/maipo.blade.php
@@ -55,7 +55,7 @@ $enddate=strtotime("+5 days", $startdate);
 
         <!--<div class="opaco"></div>-->
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-            <img style="max-width: 50%" class="img-fluid col-md-12 mt-1" src="{{$landing->logo}}" alt="HerediaAbogados">
+            <img style="max-width: 90%" class="img-fluid col-md-12 mt-1" src="{{$landing->logo}}" alt="HerediaAbogados">
 
             <div id="cont1" class="p-5 ">
                 <h3 class="text-white text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
