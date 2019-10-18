7a336fb76a39a81b9df4c766227ef8e71a2d2fa2
jsuscortz <cortezjesus01@gmail.com>
2019-10-18 13:29:19 -0300

    Update amicar.blade.php
    fin02_ami

--- resources/views/landings/amicar.blade.php
+++ resources/views/landings/amicar.blade.php
@@ -71,7 +71,7 @@ $enddate=strtotime("+15 days", $startdate);
                     
                         
                     
-                    <input id="date1" type="date" id="datepicker">
+                    <input class="md-form col-12  m-0" id="date1" type="date" id="datepicker">
                         
                      <button onclick="sendMail(); return false;" class="btn bg-danger text-white col-12 mt-4">Visitar</button>
                        </div>
