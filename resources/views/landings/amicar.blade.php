5284fc6e3858f2dd454e4b65f3fdcbeea7443533
jsuscortz <cortezjesus01@gmail.com>
2019-10-18 13:24:46 -0300

    Update amicar.blade.php
    datoami

--- resources/views/landings/amicar.blade.php
+++ resources/views/landings/amicar.blade.php
@@ -73,7 +73,7 @@ $enddate=strtotime("+15 days", $startdate);
                     
                     <input id="date1" type="date" id="datepicker">
                         
-                     <button onclick="sendMail('El cliente indica visita'); eventosLanding('Agenda Visita'); return false;" class="btn bg-danger text-white col-12 mt-4">Visitar</button>
+                     <button onclick="sendMail(); return false;" class="btn bg-danger text-white col-12 mt-4">Visitar</button>
                        </div>
                        </div>
              </div>
