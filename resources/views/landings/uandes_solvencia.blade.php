b48231b20ae05dced580872a5e4aa81c927175ea
jsuscortz <cortezjesus01@gmail.com>
2019-10-22 09:54:25 -0300

    Update uandes_solvencia.blade.php
    sol_uands_4

--- resources/views/landings/uandes_solvencia.blade.php
+++ resources/views/landings/uandes_solvencia.blade.php
@@ -47,7 +47,7 @@ $enddate=strtotime("+5 days", $startdate);
     
 </style>
 
-    <div id="UANDES" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: contain;">
+    <div id="UANDES" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;">
 
         <div class="opaco"></div>
 
