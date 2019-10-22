d98925b95803b94b093dc86b5b953b8ab5fc067f
jsuscortz <cortezjesus01@gmail.com>
2019-10-22 10:02:40 -0300

    Update uandes_solvencia.blade.php
    sol_andes_05

--- resources/views/landings/uandes_solvencia.blade.php
+++ resources/views/landings/uandes_solvencia.blade.php
@@ -54,7 +54,7 @@ $enddate=strtotime("+5 days", $startdate);
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
             <img style="max-width: 50%" class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="UANDES">
 
-             <div id="cont1" class="p-5 ">|
+             <div id="cont1" class="p-5 ">
                 <h3 class="text-white text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
                 <input id="rut" type="number" class="form-control mt-4 text-center text-black">
                 <button onclick="event1(); return false;" class="btn bg-danger text-white col-12 mt-4"> Validar </button>
