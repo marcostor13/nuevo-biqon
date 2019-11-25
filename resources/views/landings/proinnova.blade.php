40cc114f5cd487827cb4dae3af3db982a940a49b
jsuscortz <cortezjesus01@gmail.com>
2019-11-25 14:03:45 -0300

    Update proinnova.blade.php
    var02

--- resources/views/landings/proinnova.blade.php
+++ resources/views/landings/proinnova.blade.php
@@ -52,7 +52,7 @@ $enddate=strtotime("+5 days", $startdate);
         <div class="opaco"></div>
 
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
-            <img style="max-width:180px; max-height:180px " class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="ProInnova">
+            <img style="max-width:250px; max-height:250px " class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="ProInnova">
 
              <div id="cont1" class="p-5 ">
                 <h3 class="text-black text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
