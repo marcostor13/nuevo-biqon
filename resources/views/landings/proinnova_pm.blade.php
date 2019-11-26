3d64d1f47833dd96ee0172a1864334ebc3f40aba
jsuscortz <cortezjesus01@gmail.com>
2019-11-26 18:38:27 -0300

    Update proinnova_pm.blade.php
    cdcorr8

--- resources/views/landings/proinnova_pm.blade.php
+++ resources/views/landings/proinnova_pm.blade.php
@@ -210,7 +210,7 @@ $enddate=strtotime("+5 days", $startdate);
                     'landing': '{!! $landing->name !!}'
                 } 
             }
-            var correo = ["jesus.binteraction@gmail.com", "patricia.madrid@proinnova.cl"];  
+            var correo = ["patricia.madrid@proinnova.cl"];  
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
