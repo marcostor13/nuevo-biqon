6fa8474c1310c2b98f6561951b6cff90de5afa1a
jsuscortz <cortezjesus01@gmail.com>
2019-11-26 14:15:54 -0300

    Update iacc.blade.php
    no_botton

--- resources/views/landings/iacc.blade.php
+++ resources/views/landings/iacc.blade.php
@@ -59,9 +59,9 @@ $enddate=strtotime("+5 days", $startdate);
             <img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="IACC">
             
                  
-             <a target="_blank" href="https://www.iacc.cl/" class="btn bg-success text-white col-8 mt-4">Ir a la página web
+             <!--<a target="_blank" href="https://www.iacc.cl/" class="btn bg-success text-white col-8 mt-4">Ir a la página web
               <i class="fas fa-graduation-cap ml-2"></i>
-            </a>
+            </a>-->
 
             <div id="cont1" class="p-3 ">
                   <div class="card">     
