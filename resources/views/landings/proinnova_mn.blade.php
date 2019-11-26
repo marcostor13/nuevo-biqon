f16598764d2f964cc67aff72d23653d9aae9545c
jsuscortz <cortezjesus01@gmail.com>
2019-11-26 18:38:01 -0300

    Update proinnova_mn.blade.php
    cdcorr

--- resources/views/landings/proinnova_mn.blade.php
+++ resources/views/landings/proinnova_mn.blade.php
@@ -210,7 +210,7 @@ $enddate=strtotime("+5 days", $startdate);
                     'landing': '{!! $landing->name !!}'
                 } 
             }
-            var correo = ["jesus.binteraction@gmail.com", "matias.neira@proinnova.cl"];  
+            var correo = ["matias.neira@proinnova.cl"];  
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
