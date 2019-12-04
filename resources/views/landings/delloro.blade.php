a78622d0fba994140c2a00820af5f2ce08a6ab5f
jsuscortz <cortezjesus01@gmail.com>
2019-12-04 15:15:43 -0300

    Update delloro.blade.php
    tel_ws_delloro

--- resources/views/landings/delloro.blade.php
+++ resources/views/landings/delloro.blade.php
@@ -90,16 +90,16 @@ $enddate=strtotime("+5 days", $startdate);
 
                 <!-- < <div class="btn bg-danger text-white col-12 mt-4">-->
                     <!--<span>Convenio de pago, Contactar</span>-->
-                    <a onclick="eventosLanding('Convenio'); window.location.href='tel:+56957657363' ;" class="btn bg-danger text-white col-12 mt-4"> Convenio de pago, Contactar</a> 
+                    <a onclick="eventosLanding('Convenio'); window.location.href='tel:+56967688359' ;" class="btn bg-danger text-white col-12 mt-4"> Convenio de pago, Contactar</a> 
                 <!-- <</div>-->
                <!-- <button onclick='window.location.href="https://vtr.com/?pagoexpress=1"; eventosLanding("Pagina de Pagos"); ' class="btn bg-danger text-white col-12 mt-4">PAGAR AHORA</button>-->
                 
                 <button onclick="Mens(); eventosLanding('Ya pagué'); return false;" class="btn bg-danger text-white col-12 mt-4">YA PAGUE</button>
                 
                 <div class="d-flex justify-content-around align-content-center mt-4">
-                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=+56957657363&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
+                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=+56967688359&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
                 
-                    <a onclick="eventosLanding('Llamar');" href="tel:+56957657363 "><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
+                    <a onclick="eventosLanding('Llamar');" href="tel:+56967688359 "><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
 
                     <a onclick="eventosLanding('Correo');" href="mailto:f.belloy@delloroabogados.cl?subject=Landing%20DellOro&body=Adjuto%20es%20el%20Comprobante%20de%20Pago">
                         <img width="40" src="https://es.seaicons.com/wp-content/uploads/2015/10/Email-icon.png"></a>
