07b1a84e0fbab33be7dbe41c7b052b6ba7497512
jsuscortz <cortezjesus01@gmail.com>
2019-12-05 16:58:15 -0300

    Update financoop01.blade.php
    finan

--- resources/views/landings/financoop01.blade.php
+++ resources/views/landings/financoop01.blade.php
@@ -118,7 +118,7 @@ $name = $_GET['NOMBRE'];
                    
                     <a onclick="eventosLanding('Llamar');" href="tel:+56944735189"><img width="40" src="https://i.imgur.com/Oh5DCRW.png"></a>
                    
-                    <a onclick="eventosLanding('Correo');" href="mailto: rodrigo.ramirez@financoop.cl?subject=Landing%20Financoop"><img width="40" src="https://es.seaicons.com/wp-content/uploads/2015/10/Email-icon.png"></a>
+                    <a onclick="eventosLanding('Correo');" href="mailto: rodrigo.ramirez@financoop.cl?subject=Solicito%20Informacion"><img width="40" src="https://es.seaicons.com/wp-content/uploads/2015/10/Email-icon.png"></a>
                 </div>
                 
             </div>
