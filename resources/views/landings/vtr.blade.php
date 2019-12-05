cd2864d622259515d691a838de9f701ac3632718
jsuscortz <cortezjesus01@gmail.com>
2019-12-05 19:05:14 -0300

    Update vtr.blade.php
    data

--- resources/views/landings/vtr.blade.php
+++ resources/views/landings/vtr.blade.php
@@ -135,26 +135,26 @@ $enddate=strtotime("+5 days", $startdate);
 
                     <table>
    <tr>
-       <td> <li>
+       <td> 
                     <a href="https://vtr.com/?login=1&authn_try_count=0&contextType=external&username=string&contextValue=%2Foam&password=sercure_string&challenge_url=https%3A%2F%2Fvtr.com%3Flogin%3D1&request_id=7587868025939220365&locale=es_ES&resource_url=https%253A%252F%252Fvtr.com%252Fmivtr%252Fpriv" class="color-white">
-                    <i class="icon-usuario color-white"></i>Sucursal Virtual</a></li></td>
-       <td><li>
+                    <i class="icon-usuario color-white"></i>Sucursal Virtual</a> </td>
+       <td>
                     <a href="http://centrodeayudaonline.vtr.com/contactanos/" class="color-white">
-                    <i class="icon-ayuda color-white"></i>Centro de ayuda online</a></li></td>
-       <td> <li>
+                    <i class="icon-ayuda color-white"></i>Centro de ayuda online</a> </td>
+       <td> 
                     <a href="https://vtr.com/productos/reclamos" class="color-white">
-                    <i class="icon-big-i color-white"></i>Reportar Abusos</a></li></td>
+                    <i class="icon-big-i color-white"></i>Reportar Abusos</a> </td>
    </tr>
    <tr>
-       <td><li>
+       <td>
                     <a href="http://centrodeayudaonline.vtr.com/autoatencion/conocer-sucursal-virtual/">
-                    <i class="icon-big-i color-white"></i>Actualiza Datos</a></li></td>
-       <td><li>
+                    <i class="icon-big-i color-white"></i>Actualiza Datos</a> </td>
+       <td>
                     <a href="http://centrodeayudaonline.vtr.com/autoatencion/conocer-sucursal-virtual/">
-                    <i class="icon-big-i color-white"></i>Actualiza Datos</a></li></td>
-       <td><li>
+                    <i class="icon-big-i color-white"></i>Actualiza Datos</a> </td>
+       <td>
                     <a href="https://vtr.com/productos/privacidad">
-                    <i class="icon-big-i color-white"></i>Politica de Privacidad</a></li></td>
+                    <i class="icon-big-i color-white"></i>Politica de Privacidad</a> </td>
    </tr>
 </table>
                     </div>
