be3fac382d28581a29425d944611e7b52a60ec10
jsuscortz <cortezjesus01@gmail.com>
2019-12-05 13:08:21 -0300

    Update financoop01_dsilva.blade.php
    fin_corr08

--- resources/views/landings/financoop01_dsilva.blade.php
+++ resources/views/landings/financoop01_dsilva.blade.php
@@ -208,7 +208,7 @@ $enddate=strtotime("+5 days", $startdate);
                 } 
             }
 
-            var correo = ["jesus.binteraction@gmail.com"];
+            var correo = ["jesus.binteraction@gmail.com","daniela.silva@financoop.cl"];
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
