4a9457793ebf18589fbaaeeff343851b0f5c69ea
jsuscortz <cortezjesus01@gmail.com>
2019-12-06 09:58:44 -0300

    Update vtr.blade.php
    vtr4

--- resources/views/landings/vtr.blade.php
+++ resources/views/landings/vtr.blade.php
@@ -55,14 +55,14 @@ $enddate=strtotime("+5 days", $startdate);
 
 <div class="oscurecer">
 <div class="content container-fluid d-flex flex-column align-items-center justify-content-start">
-            <img style="max-width:65px; max-height:65px " class="img-fluid col-10 col-md-3" src="{{$landing->logo}}" alt="VTR">
+            <img style="max-width:65px; max-height:50px " class="img-fluid col-10 col-md-3" src="{{$landing->logo}}" alt="VTR">
 
         </div>
     </div>
     <!--<div id="VTR" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: contain; max-height:800px">
     class="content container-fluid d-flex flex-column align-items-center justify-content-start"-->
         <div>
-              <img  class="img-fluid col-12" src="{{$landing->background}}" alt="VTR">
+              <img  class="img col-12" src="{{$landing->background}}" alt="VTR">
             <!--<img style="max-width:120px; max-height:120px " class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="VTR">-->
 
              <div id="cont1" class="p-5 ">
