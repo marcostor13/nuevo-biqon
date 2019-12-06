57a08e3f2e5060efbfd75765b05245ae495dc2a7
jsuscortz <cortezjesus01@gmail.com>
2019-12-06 10:57:10 -0300

    Update vtr.blade.php
    vtr6.7

--- resources/views/landings/vtr.blade.php
+++ resources/views/landings/vtr.blade.php
@@ -30,7 +30,7 @@ $enddate=strtotime("+5 days", $startdate);
   transition: 0.2s ease-in-out;
 }
 .card .card-body .card-text {
-  color: #747373;
+  text: #747373;
   font-size: 0.9rem;
   font-weight: 400;
 }
@@ -48,7 +48,7 @@ $enddate=strtotime("+5 days", $startdate);
 
 .accesos-directo {
     /*position: relative;*/
-    background-color: #262626;
+    background-text: #262626;
     padding: 40px 30px;
 }
     
@@ -138,28 +138,28 @@ $enddate=strtotime("+5 days", $startdate);
                     <ul>
                     <li>
                     <a test="4"  onclick="eventosLanding('Planes');" href="http://centrodeayudaonline.vtr.com/consulte-su-boleta/cobro-reposicion-servicios/" class="text-white" data-scroll-link="true" data-trigger-btn="triggered-a">
-                    <i class="fas fa-mobile-alt color-danger"></i> Planes y Tarifas</a></li>
+                    <i class="fas fa-mobile-alt text-danger"></i> Planes y Tarifas</a></li>
                     <li>
                     <a test="3" onclick="eventosLanding('Lugares de Pago');" href="https://vtr.com/productos/lugares-de-pago" class="text-white" target="pago-express" data-scroll-link="true" data-deploy-element="pago-express">
-                    <i class="fas fa-map-marked-alt color-danger"></i> Lugares de Pago</a></li>
+                    <i class="fas fa-map-marked-alt text-danger"></i> Lugares de Pago</a></li>
                     <li>
                     <a test="4"  onclick="eventosLanding('Sucursales');"href="https://vtr.com/sucursales/v" class="text-white" target="pago-express" data-scroll-link="true">
-                    <i class="fas fa-globe color-danger"></i> Sucursales</a></li>
+                    <i class="fas fa-globe text-danger"></i> Sucursales</a></li>
                     <li>
                     <a test="4" onclick="eventosLanding('Sucursal Virtual');" href="https://vtr.com/?login=1&authn_try_count=0&contextType=external&username=string&contextValue=%2Foam&password=sercure_string&challenge_url=https%3A%2F%2Fvtr.com%3Flogin%3D1&request_id=7587868025939220365&locale=es_ES&resource_url=https%253A%252F%252Fvtr.com%252Fmivtr%252Fpriv" class="text-white" data-scroll-link="true">
-                    <i class="fas fa-user color-danger"></i> Sucursal Virtual</a></li>
+                    <i class="fas fa-user text-danger"></i> Sucursal Virtual</a></li>
                     <li>
                     <a test="4" onclick="eventosLanding('Centro de ayuda');" href="http://centrodeayudaonline.vtr.com/contactanos/" class="text-white" target="pago-express" data-scroll-link="true">
-                    <i class="fas fa-question color-danger"></i> Centro de ayuda online</a></li>
+                    <i class="fas fa-question text-danger"></i> Centro de ayuda online</a></li>
                     <li>
                     <a test="4"  onclick="eventosLanding('Reportar Abusos');"href="https://vtr.com/productos/reclamos" class="text-white" data-scroll-link="true">
-                    <i class="fas fa-exclamation-circle color-danger"></i> Reportar Abusos</a></li>
+                    <i class="fas fa-exclamation-circle text-danger"></i> Reportar Abusos</a></li>
                     <li>
                     <a test="4"  onclick="eventosLanding('Actualiza Datos');"href="http://centrodeayudaonline.vtr.com/autoatencion/conocer-sucursal-virtual/" class="text-white" data-scroll-link="true">
-                    <i class="fas fa-user color-danger"></i> Actualiza Datos</a></li>
+                    <i class="fas fa-user text-danger"></i> Actualiza Datos</a></li>
                     <li>
                     <a test="4" onclick="eventosLanding('Privacidad');" href="https://vtr.com/productos/privacidad" class="text-white" data-scroll-link="true">
-                    <i class="fas fa-user-shield color-danger"></i> Politica de Privacidad</a></li>
+                    <i class="fas fa-user-shield text-danger"></i> Politica de Privacidad</a></li>
                     </ul>
                     </div>
         </div></div>
