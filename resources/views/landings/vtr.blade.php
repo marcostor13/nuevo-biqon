00a457fdf292d587704997360b077d19c7743b11
jsuscortz <cortezjesus01@gmail.com>
2019-12-06 10:56:19 -0300

    Update vtr.blade.php
    vtr6.5

--- resources/views/landings/vtr.blade.php
+++ resources/views/landings/vtr.blade.php
@@ -137,29 +137,29 @@ $enddate=strtotime("+5 days", $startdate);
 
                     <ul>
                     <li>
-                    <a test="4"  onclick="eventosLanding('Planes');" href="http://centrodeayudaonline.vtr.com/consulte-su-boleta/cobro-reposicion-servicios/" class="" data-scroll-link="true" data-trigger-btn="triggered-a">
+                    <a test="4"  onclick="eventosLanding('Planes');" href="http://centrodeayudaonline.vtr.com/consulte-su-boleta/cobro-reposicion-servicios/" class="text-white" data-scroll-link="true" data-trigger-btn="triggered-a">
                     <i class="fas fa-mobile-alt color-danger"></i> Planes y Tarifas</a></li>
                     <li>
-                    <a test="3" onclick="eventosLanding('Lugares de Pago');" href="https://vtr.com/productos/lugares-de-pago" class="" target="pago-express" data-scroll-link="true" data-deploy-element="pago-express">
-                    <i class="fas fa-map-marked-alt color-primary"></i> Lugares de Pago</a></li>
+                    <a test="3" onclick="eventosLanding('Lugares de Pago');" href="https://vtr.com/productos/lugares-de-pago" class="text-white" target="pago-express" data-scroll-link="true" data-deploy-element="pago-express">
+                    <i class="fas fa-map-marked-alt color-danger"></i> Lugares de Pago</a></li>
                     <li>
-                    <a test="4"  onclick="eventosLanding('Sucursales');"href="https://vtr.com/sucursales/v" class="" target="pago-express" data-scroll-link="true">
-                    <i class="fas fa-globe color-primary"></i> Sucursales</a></li>
+                    <a test="4"  onclick="eventosLanding('Sucursales');"href="https://vtr.com/sucursales/v" class="text-white" target="pago-express" data-scroll-link="true">
+                    <i class="fas fa-globe color-danger"></i> Sucursales</a></li>
                     <li>
-                    <a test="4" onclick="eventosLanding('Sucursal Virtual');" href="https://vtr.com/?login=1&authn_try_count=0&contextType=external&username=string&contextValue=%2Foam&password=sercure_string&challenge_url=https%3A%2F%2Fvtr.com%3Flogin%3D1&request_id=7587868025939220365&locale=es_ES&resource_url=https%253A%252F%252Fvtr.com%252Fmivtr%252Fpriv" class="" data-scroll-link="true">
-                    <i class="fas fa-user color-primary"></i> Sucursal Virtual</a></li>
+                    <a test="4" onclick="eventosLanding('Sucursal Virtual');" href="https://vtr.com/?login=1&authn_try_count=0&contextType=external&username=string&contextValue=%2Foam&password=sercure_string&challenge_url=https%3A%2F%2Fvtr.com%3Flogin%3D1&request_id=7587868025939220365&locale=es_ES&resource_url=https%253A%252F%252Fvtr.com%252Fmivtr%252Fpriv" class="text-white" data-scroll-link="true">
+                    <i class="fas fa-user color-danger"></i> Sucursal Virtual</a></li>
                     <li>
-                    <a test="4" onclick="eventosLanding('Centro de ayuda');" href="http://centrodeayudaonline.vtr.com/contactanos/" class="" target="pago-express" data-scroll-link="true">
-                    <i class="fas fa-question color-primary"></i> Centro de ayuda online</a></li>
+                    <a test="4" onclick="eventosLanding('Centro de ayuda');" href="http://centrodeayudaonline.vtr.com/contactanos/" class="text-white" target="pago-express" data-scroll-link="true">
+                    <i class="fas fa-question color-danger"></i> Centro de ayuda online</a></li>
                     <li>
-                    <a test="4"  onclick="eventosLanding('Reportar Abusos');"href="https://vtr.com/productos/reclamos" class="" data-scroll-link="true">
-                    <i class="fas fa-exclamation-circle color-primary"></i> Reportar Abusos</a></li>
+                    <a test="4"  onclick="eventosLanding('Reportar Abusos');"href="https://vtr.com/productos/reclamos" class="text-white" data-scroll-link="true">
+                    <i class="fas fa-exclamation-circle color-danger"></i> Reportar Abusos</a></li>
                     <li>
-                    <a test="4"  onclick="eventosLanding('Actualiza Datos');"href="http://centrodeayudaonline.vtr.com/autoatencion/conocer-sucursal-virtual/" class="" data-scroll-link="true">
-                    <i class="fas fa-user color-primary"></i> Actualiza Datos</a></li>
+                    <a test="4"  onclick="eventosLanding('Actualiza Datos');"href="http://centrodeayudaonline.vtr.com/autoatencion/conocer-sucursal-virtual/" class="text-white" data-scroll-link="true">
+                    <i class="fas fa-user color-danger"></i> Actualiza Datos</a></li>
                     <li>
-                    <a test="4" onclick="eventosLanding('Privacidad');" href="https://vtr.com/productos/privacidad" class="" data-scroll-link="true">
-                    <i class="fas fa-user-shield color-primary"></i> Politica de Privacidad</a></li>
+                    <a test="4" onclick="eventosLanding('Privacidad');" href="https://vtr.com/productos/privacidad" class="text-white" data-scroll-link="true">
+                    <i class="fas fa-user-shield color-danger"></i> Politica de Privacidad</a></li>
                     </ul>
                     </div>
         </div></div>
