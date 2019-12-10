1ea1a0ee37d6a1b8630e28e693345e4b693209e0
jsuscortz <cortezjesus01@gmail.com>
2019-12-10 13:05:59 -0300

    Update cmp_abogados.blade.php
    nn

--- resources/views/landings/cmp_abogados.blade.php
+++ resources/views/landings/cmp_abogados.blade.php
@@ -66,7 +66,7 @@ $enddate=strtotime("+5 days", $startdate);
                         <div class="card">     
                             <div class="card-body">
                                  <h5 id="error"class="text-black text-center mt-5"></h5>
-                                 <a href="tel:+56957657363" class="btn bg-primary text-black col-12 mt-4"> Contactar</a>
+                                 <a href="tel:+56930683241" class="btn bg-primary text-black col-12 mt-4"> Contactar</a>
                             </div>
                         </div>
                     </div>
