a130da7b38fd9d307207cd2e4af373f88c55ef46
jsuscortz <cortezjesus01@gmail.com>
2019-12-16 16:46:59 -0300

    Update zonasur.blade.php
    ZonaSUR1

--- resources/views/landings/zonasur.blade.php
+++ resources/views/landings/zonasur.blade.php
@@ -59,7 +59,7 @@ $enddate=strtotime("+5 days", $startdate);
         <div class="opaco"></div>
 
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
-            <img style="max-width: 60%" class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="ZonaSur">
+            <img style="max-width: 60% max-height:60%" class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="ZonaSur">
 
             <div id="cont1" class="p-5 ">
                 <h3 class="text-black text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
