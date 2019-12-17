2f9e06d34ddfb5d0164013a0c728e70936722a31
jsuscortz <cortezjesus01@gmail.com>
2019-12-17 09:35:11 -0300

    Update amicar.blade.php
    amc4

--- resources/views/landings/amicar.blade.php
+++ resources/views/landings/amicar.blade.php
@@ -104,7 +104,7 @@ $enddate=strtotime("+15 days", $startdate);
                     	<label>Fecha:</label>
     					<!--<input type="date" id="date1" onclick="" class="mt-4 form-control">
     					<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
-    					 <input class="md-form col-12  m-0" type="date" id="datepicker" >
+    					 <input class="md-form col-12  m-0" id="datepicker" >
                         
                     </div>
                    
