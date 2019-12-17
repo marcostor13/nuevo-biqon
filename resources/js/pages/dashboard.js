4122228c01a0ce6057d8458402803972db1db100
jsuscortz <cortezjesus01@gmail.com>
2019-12-17 15:50:25 -0300

    Update dashboard.js
    desa1

--- resources/js/pages/dashboard.js
+++ resources/js/pages/dashboard.js
@@ -244,7 +244,7 @@ if (path == '/dashboard') {
                    }
                },
                tooltip: {
-                   headerFormat: '<span style="font-size:15px">{point.key}</span><table>',
+                   headerFormat: '<span style="font-size:50px">{point.key}</span><table>',
                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
                        '<td style="padding:0"><b>{point.y:1f}</b></td></tr>',
                    footerFormat: '</table>',
