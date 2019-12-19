035a73932639d7ce6a23892b16bf01516a6a2a11
jsuscortz <cortezjesus01@gmail.com>
2019-12-19 13:18:31 -0300

    Update proinnova_cbonilla.blade.php

--- resources/views/landings/proinnova_cbonilla.blade.php
+++ resources/views/landings/proinnova_cbonilla.blade.php
@@ -74,7 +74,7 @@ $enddate=strtotime("+5 days", $startdate);
                 <h4 class="text-black text-center">Estimado(a) <span id="name"></span></h4>
 
                 <p class="text-black">
-                  Le Informamos que su deuda de: <strong> ISAPRE NUEVA MAS VIDA S.A. </strong> se encuentra en cobranza judicial, por concepto de no pago. <br> Con el objeto de no proseguir con las acciones judiciales, le ofrecemos las siguientes opciones:
+                  Le Informamos que su deuda de: <strong> ISAPRE NUEVA MAS VIDA S.A. </strong> se encuentra en cobranza judicial, por concepto de no pago. <br> Con el objeto de no proseguir con las acciones de cobranza, le ofrecemos las siguientes opciones:
                 </p>
 
                 
