171494738e3b231dae5fbf7aaf7315a120c35d6c
jsuscortz <cortezjesus01@gmail.com>
2019-12-24 12:29:48 -0300

    Update navidad_binter.blade.php
    nav2

--- resources/views/landings/navidad_binter.blade.php
+++ resources/views/landings/navidad_binter.blade.php
@@ -10,14 +10,14 @@
 }
 </style>
 
-    <!--<div id="mafchile" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;">-->
+    <div id="Binter" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;">
 
-        <div class="opaco"></div>
+        <div class="opaco">  </div>
 
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-            <img style="max-width: 40%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="Binter">
+            <!--<img style="max-width: 40%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="Binter">-->
 
-            <div id="cont1">|
+            <div id="cont1">
                 <div class="card">     
                 <div class="card-body">
                      <h4 class="dark-grey-text text-center">
