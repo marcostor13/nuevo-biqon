0bcb5a0f8e25ce4c185d6cd75193c0548946ff9c
jsuscortz <cortezjesus01@gmail.com>
2019-12-24 12:52:58 -0300

    Update navidad_binter.blade.php
    bas

--- resources/views/landings/navidad_binter.blade.php
+++ resources/views/landings/navidad_binter.blade.php
@@ -18,14 +18,12 @@
             <!--<img style="max-width: 40%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="Binter">-->
 
             <div id="cont1" class="p-5 ">
-                <br>
-                <br>
-                <br>
-                <br>
+                <br> <br>
+                
                      <h4 class="dark-grey-text text-center p-5">
                 <strong>Binteraction Chile,<br> su Proveedor de SMS, les agradece la confianza depositada en nuestros servicios.<br>
                 Les Deseamos una Feliz Navidad y un Prospero Año 2020 </strong><br></h4>
-                 <br><br>
+               
                 <div class="videoWrapper">        
                     <iframe width="400" height="320" src="{{asset('/files/Navidad.mp4')}}" frameborder="0" autoplay allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
