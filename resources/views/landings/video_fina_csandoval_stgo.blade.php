8ce6e7661c6d1b9bd39c8c74e24537eacac01ee9
jsuscortz <cortezjesus01@gmail.com>
2020-01-08 12:30:56 -0300

    Update video_fina_csandoval_stgo.blade.php
    03

--- resources/views/landings/video_fina_csandoval_stgo.blade.php
+++ resources/views/landings/video_fina_csandoval_stgo.blade.php
@@ -110,8 +110,8 @@
             
             let json_datos = getAllUrlParameter(); 
 
-            json_datos.nombre = $('#name').text();
-            json_datos.monto = $('#pay').text();
+          //  json_datos.nombre = $('#name').text();
+           // json_datos.monto = $('#pay').text();
 
             events({    
                 'name': name,
