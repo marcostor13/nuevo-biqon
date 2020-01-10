c7064b05b85ab16ec07b1844933d0836bed83fbb
jsuscortz <cortezjesus01@gmail.com>
2020-01-10 10:26:34 -0300

    Update Marubeni_lrijo.blade.php
    n

--- resources/views/landings/Marubeni_lrijo.blade.php
+++ resources/views/landings/Marubeni_lrijo.blade.php
@@ -51,7 +51,7 @@ $enddate=strtotime("+7 days", $startdate);
         <div class="opaco"></div>
 
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
-            <img style="max-width:250px; max-height:250px" class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="Marubeni">
+            <img style="max-width:250px; max-height:100px" class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="Marubeni">
 
             <div id="cont1" class="p-5 ">|
                 <h3 class="text-white text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
