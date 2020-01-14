473ba20ec8655dcb92fdd0ff9688a821d32a5a17
jsuscortz <cortezjesus01@gmail.com>
2020-01-14 10:57:41 -0300

    Update vtr.blade.php
    va

--- resources/views/landings/vtr.blade.php
+++ resources/views/landings/vtr.blade.php
@@ -473,10 +473,16 @@ $enddate=strtotime("+3 days", $startdate);
         } 
 
         function url2(){
+            function(){
             $('#cont2').addClass('hide');
             $('#calend').removeClass('hide');
+            $('#date1').on('change', function(){
+                        if($('#date1').val() != ''){
+                            sendMail();
+                        }
+                    }
 
-        }
+        } }
 
 
             function url5(){
