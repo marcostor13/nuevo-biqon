ef64f8e2a9400621e988f071adb5666e56059426
jsuscortz <cortezjesus01@gmail.com>
2020-01-15 11:42:58 -0300

    Update vtr.blade.php
    vtr

--- resources/views/landings/vtr.blade.php
+++ resources/views/landings/vtr.blade.php
@@ -278,6 +278,20 @@ $enddate=strtotime("+3 days", $startdate);
             <img style="max-width:100px; max-height:100px" class="img-fluid col-10 col-md-3" src="{{$landing->logo}}" alt="VTR"> 
         </div>
 
+        <table class="table table-bordered table-dark">
+  <thead>
+    <tr>
+      <th scope="col">1</th>
+      <th scope="col">2</th>
+      <th scope="col">3</th>
+      <th scope="col">4</th>
+      <th scope="col">5</th>
+      <th scope="col">6</th>
+      <th scope="col">7</th>
+      <th scope="col">8</th>
+    </tr>
+  </thead>
+
         </div>
 
 
