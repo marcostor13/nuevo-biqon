01e062cfe67ab6cf8a21ca6a596f281f5726bce2
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-01-17 10:34:44 -0300

    Update binteraction.blade.php
    add

--- resources/views/landings/binteraction.blade.php
+++ resources/views/landings/binteraction.blade.php
@@ -29,9 +29,10 @@ $enddate=strtotime("+15 days", $startdate);
                     <input id="date1"  type="date" class="btn-date text-primary" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
                      </div>
                     <a href="https://api.whatsapp.com/send?phone=56942918783&text=Hola,%20tengo%20una%20consulta"><button class="btn bg-warning text-white col-12 mt-4">WHATTSAPP</button></a> 
-                       </div> </div>
-                       <a href="tel:+56942918783"><button class="btn bg-warning text-white col-12 mt-4">LLAMAR</button></a></a>
+                    <a href="tel:+56942918783"><button class="btn bg-warning text-white col-12 mt-4">LLAMAR</button></a></a>
                        <a href="mailto:maicol.binteraction@gmail.com?subject=Landing%20Binteraction"><button class="btn bg-warning text-white col-12 mt-4">CORREO</button></a>
+                       </div> </div>
+
                 
 
                
