69c9f98e432d2313575132d324920033c82415fd
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-01-17 18:23:50 -0300

    Update pruebamegateleserie.blade.php
    add

--- resources/views/landings/pruebamegateleserie.blade.php
+++ resources/views/landings/pruebamegateleserie.blade.php
@@ -20,12 +20,17 @@ $enddate=strtotime("+15 days", $startdate);
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
             <img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="MEGA-YOSOYLORENZO">
 
-            <div id="cont1">
-                <div class="videoWrapper">     
-                    <h1><strong>Disfruta de nuestra Teleserie a las 20:00hrs </strong></h1>
+            <h1><strong>Disfruta de nuestra Teleserie a las 20:00hrs </strong></h1>
 
-                    <iframe width="400" height="320" src="{{asset('/files/ysl.mp4')}}" frameborder="0" autoplay allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div id="cont1">
+                <div class="videoWrapper">   
+                <center>
+                     <iframe width="400" height="320" src="{{asset('/files/ysl.mp4')}}" frameborder="0" autoplay allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
+                </center>  
+                    
+
+                   
 
                  <div class="d-flex justify-content-around align-content-center mt-1"> 
                   
