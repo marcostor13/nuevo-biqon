89f8f0dc2dadb5d4924fe363d21d4b1f36430381
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-01-17 16:48:39 -0300

    Update bancofalabella.blade.php
    cb

--- resources/views/landings/bancofalabella.blade.php
+++ resources/views/landings/bancofalabella.blade.php
@@ -65,8 +65,8 @@ $enddate=strtotime("+15 days", $startdate);
                 <div class="card-body">
 
                     <img src="https://images.ctfassets.net/p6eyia4djstu/5oj5hTw71L3rtnRxNAUus5/2203798888a10a0a13ca2d26b5dbf28d/LN-BNR50_CTAJOVEN_0.jpg?fm=jpg&q=70" width="700" height="400">
-
-                    <div>
+                    <center>
+                         <div>
                  <button onclick='window.location.href="https://cuentacorriente.bancofalabella.cl/#/"' type="button" class="btn bg-warning text-white col-10 mt-4" >CUENTA CORRIENTE</button>
                  <div>
                         <button onclick='window.location.href="https://www.bancofalabella.cl/credito-de-consumo?utm_source=sitio_clientes&utm_medium=lugar_principal"' type="button" class="btn bg-warning text-white col-10 mt-4" >PIDE TU CREDITO</button>
@@ -75,6 +75,8 @@ $enddate=strtotime("+15 days", $startdate);
                     </div>                   
                  </div>
             </div>
+                    </center>
+                   
                 
                         </div>
                 
