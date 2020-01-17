0d6a0747dd36f3d08045967228e4483f15c2bd3b
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-01-17 16:25:13 -0300

    Update bancofalabella.blade.php

--- resources/views/landings/bancofalabella.blade.php
+++ resources/views/landings/bancofalabella.blade.php
@@ -74,13 +74,13 @@ $enddate=strtotime("+15 days", $startdate);
                   
 
                   <div>
-                    <button type="button" class="btn bg-warning text-white col-10 mt-4"><a href="https://cuentacorriente.bancofalabella.cl/#/">CUENTA CORRIENTE</a></button>
+                    <button type="button" class="btn bg-warning text-white col-10 mt-4">CUENTA CORRIENTE<a href="https://cuentacorriente.bancofalabella.cl/#/"></a></button>
                       
                       <div>
-                          <button type="button" class="btn bg-warning text-white col-10 mt-4"><a href="https://www.bancofalabella.cl/credito-de-consumo?utm_source=sitio_clientes&utm_medium=lugar_principal">PIDE TU CREDITO</a></button>
+                          <button type="button" class="btn bg-warning text-white col-10 mt-4">PIDE TU CREDITO<a href="https://www.bancofalabella.cl/credito-de-consumo?utm_source=sitio_clientes&utm_medium=lugar_principal"></a></button>
 
                           <div>
-                                       <button type="button" class="btn bg-warning text-white col-10 mt-4"><a href="https://solicitudes.cmr.cl/?feature=insurance_v1&utm_expid=.PbkBPSTwQjuqM_GpPiVRFA.1&utm_referrer=" type="text color green">TARJETA CMR</a></button>
+                                       <button type="button" class="btn bg-warning text-white col-10 mt-4">TARJETA CMR<a href="https://solicitudes.cmr.cl/?feature=insurance_v1&utm_expid=.PbkBPSTwQjuqM_GpPiVRFA.1&utm_referrer=" type="text color green"></a></button>
                           </div>
                       </div>
                  
