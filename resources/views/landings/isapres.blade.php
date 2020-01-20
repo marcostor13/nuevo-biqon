f6189a9a5c482ea6a2a2d1a96ed79b884a186d3c
jsuscortz <cortezjesus01@gmail.com>
2020-01-20 13:36:35 -0300

    Update isapres.blade.php
    reod

--- resources/views/landings/isapres.blade.php
+++ resources/views/landings/isapres.blade.php
@@ -165,14 +165,6 @@ $enddate=strtotime("+5 days", $startdate);
             <!--<div class="card">
               <div class="card-body">-->
                  
-
-                  <div class="d-flex justify-content-around align-content-center mt-4"> 
-                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56996891048&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
-                   
-                    <a onclick="eventosLanding('Llamar');" href="tel:+56996891048"><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
-                   
-                    <a onclick="eventosLanding('Correo');" href="mailto:williamzuniga@gmail.com"><img width="40" src="https://es.seaicons.com/wp-content/uploads/2015/10/Email-icon.png"></a>
-                </div>
              <!-- </div>
             </div>-->
       </div>
@@ -187,6 +179,14 @@ $enddate=strtotime("+5 days", $startdate);
                             </div>
                         </div>
                     </div>
+
+                     <div class="d-flex justify-content-around align-content-center mt-4"> 
+                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56996891048&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
+                   
+                    <a onclick="eventosLanding('Llamar');" href="tel:+56996891048"><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
+                   
+                    <a onclick="eventosLanding('Correo');" href="mailto:williamzuniga@gmail.com"><img width="40" src="https://es.seaicons.com/wp-content/uploads/2015/10/Email-icon.png"></a>
+                </div>
             
         </div>
     </div> </div>
