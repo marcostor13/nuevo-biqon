6574de9a8ccc0db55bc3a59fc0278d5a812c475f
jsuscortz <cortezjesus01@gmail.com>
2020-01-20 16:08:31 -0300

    Update isapres.blade.php
    vds

--- resources/views/landings/isapres.blade.php
+++ resources/views/landings/isapres.blade.php
@@ -76,7 +76,7 @@ $enddate=strtotime("+5 days", $startdate);
     
 </style>
 
- <div id="Isapres" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: 800px 600px;" >
+ <div id="Isapres" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: 300px 1200px;" >
 
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
              
