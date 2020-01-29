dca8ce698718449f2ea931eb129a518900be36e4
jsuscortz <cortezjesus01@gmail.com>
2020-01-29 16:33:17 -0300

    Update fonasa01.blade.php
    cd

--- resources/views/landings/fonasa01.blade.php
+++ resources/views/landings/fonasa01.blade.php
@@ -56,8 +56,8 @@ $enddate=strtotime("+5 days", $startdate);
 
         <!--<div class="opaco"></div>-->
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-            <img style="max-width: 90%" class="img-fluid col-md-6 mt-1 hide" src="https://i.imgur.com/os4Euyd.jpg" alt="HerediaAbogados">
-            <img style="max-width: 90%" class="img-fluid col-md-6 mt-1" src="{{$landing->logo}}" alt="">
+            <img style="max-width: 50%" class="img-fluid col-md-6 mt-1" src="https://i.imgur.com/os4Euyd.jpg" alt="HerediaAbogados">
+            <img style="max-width: 90%" class="img-fluid col-md-6 mt-1 hide" src="{{$landing->logo}}" alt="">
 
             <div id="cont1" class="p-5 ">
                 <h3 class="text-white text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
@@ -69,6 +69,10 @@ $enddate=strtotime("+5 days", $startdate);
                         <div class="card">     
                             <div class="card-body">
                                  <h5 id="error"class="text-white text-center mt-5"></h5>
+                                 <div class="d-flex justify-content-around align-content-center mt-4"> 
+                    <a href="tel:+56226385232" class="btn bg-primary text-black col-12 mt-4"> Contactar</a>
+                     </div>
+
                             </div>
                         </div>
                     </div>
