52d59b2e28e0d8258c1f59d213ab0b7c2fdf0311
jsuscortz <cortezjesus01@gmail.com>
2020-01-29 16:44:18 -0300

    Update fonasa01.blade.php
    cdsaz

--- resources/views/landings/fonasa01.blade.php
+++ resources/views/landings/fonasa01.blade.php
@@ -56,9 +56,9 @@ $enddate=strtotime("+5 days", $startdate);
 
         <!--<div class="opaco"></div>-->
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-            <img style="height:120px;width:120px;" class="img-fluid col-md-6 mt-1" src="https://i.imgur.com/os4Euyd.jpg" alt="HerediaAbogados">
+            <img style="height:180px; width:120px;" class="img-fluid col-md-6 mt-1" src="https://i.imgur.com/os4Euyd.jpg" alt="HerediaAbogados">
 
-            
+
            <!-- <img style="max-width: 90%" class="img-fluid col-md-6 mt-1 hide" src="{{$landing->logo}}" alt="">-->
 
             <div id="cont1" class="p-5 ">
@@ -96,7 +96,7 @@ $enddate=strtotime("+5 days", $startdate);
                 <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-danger text-white col-12 mt-4">Ya pague</button>
                 
                 
-                <div class="d-flex justify-content-around align-content-center mt-6"> 
+                <div class="d-flex justify-content-around align-content-center mt-2"> 
 
                     <div class="card">     
                             <div class="card-body">
