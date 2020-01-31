92925e23bc4e3a662d937f7f322d88f90ef43f73
jsuscortz <cortezjesus01@gmail.com>
2020-01-31 14:50:21 -0300

    Update fuentealcazar.blade.php
    cds

--- resources/views/landings/fuentealcazar.blade.php
+++ resources/views/landings/fuentealcazar.blade.php
@@ -62,8 +62,8 @@ $enddate=strtotime("+5 days", $startdate);
      
 
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-          <div class="med align-items-center "> <br>
-            <img class="img" src="{{$landing->logo}}" alt="FUENTE-ALCAZAR"> <br>  <br> 
+          <div class="med align-items-center mt-4"> <br>
+            <img class="img mt-2" src="{{$landing->logo}}" alt="FUENTE-ALCAZAR"> <br> 
             <br>
           </div>
          
