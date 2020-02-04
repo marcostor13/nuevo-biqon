24c9cf8bbaa5a3a89faddcc31351f4bb21234e0a
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-02-04 12:45:46 -0300

    Update pliderofertas.blade.php
    add cr

--- resources/views/landings/pliderofertas.blade.php
+++ resources/views/landings/pliderofertas.blade.php
@@ -64,7 +64,39 @@ $enddate=strtotime("+15 days", $startdate);
                             <div class="card">     
                 <div class="card-body">
 
-                    <img src="https://chocale.cl/wp-content/uploads/2019/02/mil-productos-a-mil-pesos-lider-feb-2019-1280x720.jpg" width="350" height="200">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="https://chocale.cl/wp-content/uploads/2019/02/mil-productos-a-mil-pesos-lider-feb-2019-1280x720.jpg" width="350" height="200"" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="https://na001.leafletcdns.com/cl/data/2/6779/0_s.jpg?t=1580136831" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="https://na001.leafletcdns.com/cl/data/2/6672/0_s.jpg?t=1578906378" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
                 
                         </div>
                 
