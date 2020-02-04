87c388365b39566110ac4a1e56853ba449d1ee26
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-02-04 13:02:43 -0300

    Update pliderofertas.blade.php

--- resources/views/landings/pliderofertas.blade.php
+++ resources/views/landings/pliderofertas.blade.php
@@ -7,6 +7,14 @@
 $startdate=strtotime("Today");
 $enddate=strtotime("+15 days", $startdate);
 ?>
+<head>
+    <!-- Compiled and minified Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<!-- Minified JS library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+</head>
 <style>
 .opaco{
     display: none !important; 
