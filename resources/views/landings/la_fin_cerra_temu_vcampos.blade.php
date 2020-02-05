afff3e12e0ed35eafad04fe1765c6b44b62b8ed5
jsuscortz <cortezjesus01@gmail.com>
2020-02-05 18:13:47 -0300

    Update la_fin_cerra_temu_vcampos.blade.php
    fdds

--- resources/views/landings/la_fin_cerra_temu_vcampos.blade.php
+++ resources/views/landings/la_fin_cerra_temu_vcampos.blade.php
@@ -55,7 +55,7 @@ $enddate=strtotime("+5 days", $startdate);
     }
 </style>
 
-   <div id="Financoop" class="oscurecer" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;" >
+   <!--<div id="Financoop" class="oscurecer" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;" >-->
 
         
         <div class="opaco"></div>
