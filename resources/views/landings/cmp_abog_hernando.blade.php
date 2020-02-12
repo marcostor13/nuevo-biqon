c3f92f33979806259271fdfdf0288ae279413d2b
jsuscortz <cortezjesus01@gmail.com>
2020-02-12 11:17:23 -0300

    Update cmp_abog_hernando.blade.php
    23

--- resources/views/landings/cmp_abog_hernando.blade.php
+++ resources/views/landings/cmp_abog_hernando.blade.php
@@ -66,7 +66,7 @@ $enddate=strtotime("+5 days", $startdate);
                         <div class="card">     
                             <div class="card-body">
                                  <h5 id="error"class="text-black text-center mt-5"></h5>
-                                 <a href="tel:+56930681341" class="btn bg-primary text-black col-12 mt-4"> Contactar</a>
+                                 <a href="tel:+56949288698" class="btn bg-primary text-black col-12 mt-4"> Contactar</a>
                             </div>
                         </div>
                     </div>
