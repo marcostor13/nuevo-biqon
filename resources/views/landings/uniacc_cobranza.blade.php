6a677a54cda4e67838b56c053dcea421092a15c1
jsuscortz <cortezjesus01@gmail.com>
2020-03-02 12:33:28 -0300

    Update uniacc_cobranza.blade.php
    fd

--- resources/views/landings/uniacc_cobranza.blade.php
+++ resources/views/landings/uniacc_cobranza.blade.php
@@ -65,7 +65,7 @@ $enddate=strtotime("+5 days", $startdate);
             <img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="UNIACC">
 
              <div id="cont1" class="p-5 ">
-                <h4 class="text-black text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h4>
+                <h4 class="text-white text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h4>
                 <input id="rut" type="number" class="form-control mt-4 text-center text-black">
                 <button onclick="event1(); return false;" class="btn orange text-white col-12 mt-4"> Validar </button>
             </div>
