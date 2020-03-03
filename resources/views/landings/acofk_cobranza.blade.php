983e6f57c55698b3a5cc5c3a5208e345e470baed
jsuscortz <cortezjesus01@gmail.com>
2020-03-03 13:03:51 -0300

    Update acofk_cobranza.blade.php
    fdsewqaa

--- resources/views/landings/acofk_cobranza.blade.php
+++ resources/views/landings/acofk_cobranza.blade.php
@@ -233,7 +233,7 @@ $enddate=strtotime("+5 days", $startdate);
                     $('#message').removeClass('hide');
                      $('#cont2').addClass('hide');
                     $('#cont3').removeClass('hide');
-                    $('#message').text('Muchas Gracias. Su Solicitud Fue enviada a nuestra área. Nos pondremos en contacto con usted en los próximos días');
+                    $('#message').text('Muchas Gracias. Su Solicitud Fue enviada a nuestra área. Nos pondremos en contacto con usted.');
                 }else{
                     $('#message').removeClass('hide');
                      $('#cont2').addClass('hide');
