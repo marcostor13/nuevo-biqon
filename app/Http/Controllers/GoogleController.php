daad48802658762dd95cd8bd7ca797f4abeb517f
Marcos Torres <marcostor13@gmail.com>
2020-03-16 03:35:07 -0500

    info

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -11,6 +11,8 @@ class GoogleController extends Controller
     public function uploadDataBigQuery(Request $request){
 
 
+        return $request->pathname;
+
         $pos = strpos($request->pathname, 'bint'); 
         if ($pos === false) {
             $keyPath = 'C:\Marcos\MT\Proyectos\NUevo biqon\bigquery\bigquery.json';
