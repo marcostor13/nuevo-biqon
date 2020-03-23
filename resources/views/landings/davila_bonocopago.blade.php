aa8df8d95806c53deb125568ac682e8d356f4d0e
Jesus Cortez <cortezjesus01@gmail.com>
2020-03-23 14:50:34 -0300

    Update davila_bonocopago.blade.php
    chn_dav05

--- resources/views/landings/davila_bonocopago.blade.php
+++ resources/views/landings/davila_bonocopago.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'CLINICA DAVILA')
 
 @section('content')
+<?php
+$startdate=strtotime("Today");
+$enddate=strtotime("+5 days", $startdate);
+?>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
