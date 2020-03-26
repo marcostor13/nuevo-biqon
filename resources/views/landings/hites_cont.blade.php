276ae888f5d2b999dbf90b139e4239a23b5b7ae2
Jesus Cortez <cortezjesus01@gmail.com>
2020-03-26 11:39:09 -0300

    Update hites_cont.blade.php
    ver

--- resources/views/landings/hites_cont.blade.php
+++ resources/views/landings/hites_cont.blade.php
@@ -18,7 +18,7 @@
 
             <div id="cont1">
                 <div class="videoWrapper">        
-                    <iframe width="400" height="320" src="{{asset('/files/Hites02_35seg.mp4')}}" frameborder="0" autoplay allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="380" height="300" src="{{asset('/files/Hites02_35seg.mp4')}}" frameborder="0" autoplay allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 
             </div>
