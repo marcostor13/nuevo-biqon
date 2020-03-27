2f24e175134a5c83c4c7a0277ff5beba7daa1226
Jesus Cortez <cortezjesus01@gmail.com>
2020-03-27 16:18:26 -0300

    Update davila_copago02.blade.php
    prueba

--- resources/views/landings/davila_copago02.blade.php
+++ resources/views/landings/davila_copago02.blade.php
@@ -101,7 +101,7 @@
                                                        
                                     <a onclick="eventosLanding('Llamar'); Mens();"><img width="85" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
                                    
-                                    <a onclick="eventosLanding('Correo');" href="mailto:infopagatucuenta@davila.cl?subject=Pago%20de%20Cuentas"><img width="85" src="https://www.internet-didactica.es/wp-content/uploads/que-es-email-correo-electronico-640x640.jpg"></a>
+                                   <a onclick="eventosLanding('Correo'); sendMail('El cliente quiere ser contactado');"><img width="85" src="https://www.internet-didactica.es/wp-content/uploads/que-es-email-correo-electronico-640x640.jpg"></a>
                                     </div>
                             </div>   
                         </div>
