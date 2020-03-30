260a6ccdec07a175d9ed8736195687cb8e4981b7
Jesus Cortez <cortezjesus01@gmail.com>
2020-03-30 15:34:42 -0300

    Update cmp_abog_rodolfo_mcredit.blade.php
    ds14

--- resources/views/landings/cmp_abog_rodolfo_mcredit.blade.php
+++ resources/views/landings/cmp_abog_rodolfo_mcredit.blade.php
@@ -91,9 +91,9 @@ $enddate=strtotime("+5 days", $startdate);
                     <!--<span>Convenio de pago, Contactar</span>-->
                     <a href="tel:+56932245496" class="btn bg-primary text-black col-12 mt-4"> Convenio de pago, Contactar</a> 
                 <!-- <</div>-->
-               <!-- <button onclick='window.location.href="https://vtr.com/?pagoexpress=1"; eventosLanding("Pagina de Pagos"); ' class="btn bg-primary text-black col-12 mt-4">PAGAR AHORA</button>-->
+               <button onclick='window.location.href="http://servipag.mundocredito.cl/PagoCuotas.php"; eventosLanding("Pagina de Pagos"); ' class="btn bg-primary text-black col-12 mt-4">PAGAR AHORA</button>
                 
-                <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-primary text-black col-12 mt-4">YA PAGUE</button>
+               <!-- <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-primary text-black col-12 mt-4">YA PAGUE</button>-->
                 
                 <div class="d-flex justify-content-around align-content-center mt-4">
                     <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=+56932245496&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
