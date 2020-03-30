473bf64436db2347709e8d849c95d9eac9d65ae8
Jesus Cortez <cortezjesus01@gmail.com>
2020-03-30 16:29:52 -0300

    Update cmp_abog_tanner.blade.php
    vdf

--- resources/views/landings/cmp_abog_tanner.blade.php
+++ resources/views/landings/cmp_abog_tanner.blade.php
@@ -76,7 +76,7 @@ $enddate=strtotime("+5 days", $startdate);
                     </div>
             <div id="cont2" class="p-5 ">
                 <h4 class="text-black text-center">Estimado(a) <span id="name"></span></h4>
-  <img  class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/mZPtMyS.jpg" alt="CMPAbogados">
+  <img style="max-width: 60%"  class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/mZPtMyS.jpg" alt="CMPAbogados">
                 <p class="text-black">
                  Preparamos una campaña especial solo para los clientes que están al día y que hayan pagado su cuota de marzo 2020 antes del 31 de marzo a las 23:59 hrs. Si cumples con estos requisitos podrás refinanciar tu crédito automotriz y desfasar el pago de hasta 3 cuotas a tasa preferencial.
 <br>
