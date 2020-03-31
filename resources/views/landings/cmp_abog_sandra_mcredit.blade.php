4f75f6eee13c087c1006e82bb866b874d0e74b77
Jesus Cortez <cortezjesus01@gmail.com>
2020-03-31 13:26:01 -0300

    Update cmp_abog_sandra_mcredit.blade.php
    fd

--- resources/views/landings/cmp_abog_sandra_mcredit.blade.php
+++ resources/views/landings/cmp_abog_sandra_mcredit.blade.php
@@ -78,7 +78,7 @@ $enddate=strtotime("+5 days", $startdate);
                 </p>
 
                 
-              <div class="date btn bg-primary text-black col-12 mt-4">
+              <div class="date btn bg-primary text-white col-12 mt-4">
                     <span>AGENDAR COMPROMISO DE PAGO</span>
                     <input id="date1"  type="date" class="btn-date text-primary" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
                 </div>
@@ -89,9 +89,9 @@ $enddate=strtotime("+5 days", $startdate);
 
                 <!-- < <div class="btn bg-primary text-black col-12 mt-4">-->
                     <!--<span>Convenio de pago, Contactar</span>-->
-                    <a href="tel:+56932505512" class="btn bg-primary text-black col-12 mt-4"> Convenio de pago, Contactar</a> 
+                    <a href="tel:+56932505512" class="btn bg-primary text-white col-12 mt-4"> Convenio de pago, Contactar</a> 
                 <!-- <</div>-->
-               <button onclick='window.location.href="http://servipag.mundocredito.cl/PagoCuotas.php"; eventosLanding("Pagina de Pagos"); ' class="btn bg-primary text-black col-12 mt-4">PAGAR AHORA</button>
+               <button onclick='window.location.href="http://servipag.mundocredito.cl/PagoCuotas.php"; eventosLanding("Pagina de Pagos"); ' class="btn bg-primary text-white col-12 mt-4">PAGAR AHORA</button>
                 
                <!-- <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-primary text-black col-12 mt-4">YA PAGUE</button>-->
                 
