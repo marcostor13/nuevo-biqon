a9ee5c34313ea3ea22976c0b0909cb9ece1132f6
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-04-02 12:25:05 -0300

    Update bint.blade.php

--- resources/views/landings/bint.blade.php
+++ resources/views/landings/bint.blade.php
@@ -37,7 +37,7 @@ $enddate=strtotime("+7 days", $startdate);
                     <span>AGENDAR COMPROMISO DE PAGO</span>
                     <input id="date1"  type="date" class="btn-date text-black" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
                 </div>
-                <button onclick='window.location.href="wwww.binteraction.cl";eventosLanding("Pagina de Pagos");' class="btn bg-success text-black col-12 mt-4">PAGAR AHORA</button>
+                <button onclick='window.location.href="https://www.binteraction.cl";eventosLanding("Pagina de Pagos");' class="btn bg-success text-black col-12 mt-4">PAGAR AHORA</button>
 
                 <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-success text-black col-12 mt-4">YA PAGUE</button>
                 
