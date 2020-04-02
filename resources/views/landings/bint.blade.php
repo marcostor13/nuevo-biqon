63c8b516697a801b8b8ef25abcde2f8d85ae1c2e
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-04-02 14:15:42 -0300

    Update bint.blade.php

--- resources/views/landings/bint.blade.php
+++ resources/views/landings/bint.blade.php
@@ -55,12 +55,12 @@ $enddate=strtotime("+5 days", $startdate);
         <div class="opaco"></div>
 
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
-            <img style="max-width:250px; max-height:200px " class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="CMPAbogados">
+            <img style="max-width:250px; max-height:200px " class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="CMPAbogados" height="1000" width="400">
 
              <div id="cont1" class="p-5 ">
                 <h4 class="text-black text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h4>
                 <input id="rut" type="number" class="form-control mt-4 text-center text-black">
-                <button onclick="event1(); return false;" class="btn bg-primary text-black col-12 mt-4"> Validar </button>
+                <button onclick="event1(); return false;" class="btn bg-primary text-white col-12 mt-4"> Validar </button>
             </div>
             <div id="cont5" class="p-2 pl-3 pr-3 hide">
                         <div class="card">     
