19596d6ff91f9474da9d5532a3bf9f0b365901ee
Jesus Cortez <cortezjesus01@gmail.com>
2020-04-08 12:50:56 -0400

    Update finan_lan_pagar.blade.php
    cd

--- resources/views/landings/finan_lan_pagar.blade.php
+++ resources/views/landings/finan_lan_pagar.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'FINANCOOP')
 
 @section('content')
+
+<?php
+$startdate=strtotime("Today");
+$enddate=strtotime("+5 days", $startdate);
+?>
+
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
