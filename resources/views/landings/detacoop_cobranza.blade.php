1d36050710743d0430320c0fe6fa57e850e25af3
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-04-08 16:20:14 -0400

    Update detacoop_cobranza.blade.php

--- resources/views/landings/detacoop_cobranza.blade.php
+++ resources/views/landings/detacoop_cobranza.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.landing')
 
-@section('title', 'BETACOOP')
+@section('title', 'DETACOOP')
 
 @section('content')
 
@@ -57,7 +57,7 @@ $enddate=strtotime("+5 days", $startdate);
      <div class="opaco">
 
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-            <img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="BETACOOP">
+            <img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="DETACOOP">
             
             <div class=col-md-12>
            
