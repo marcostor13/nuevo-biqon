2c6cedccd50851b9ed845a684dc972bcdb2ff530
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-04-08 14:38:22 -0400

    Update finan_lan_pagar.blade.php

--- resources/views/landings/finan_lan_pagar.blade.php
+++ resources/views/landings/finan_lan_pagar.blade.php
@@ -94,6 +94,7 @@ $enddate=strtotime("+5 days", $startdate);
                                 <div class="date btn bg-success text-white col-12 mt-4">
                                  <span>AGENDAR VISITA</span>
                                  <input id="date1"  type="date" class="btn-date text-white" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
+                               </div>
                                 <button onclick="Mens4(); eventosLanding('Correo');" class="btn bg-primary text-white col-12 mt-2">CONSULTAS POR CORREO
                                 </button>
 
@@ -363,7 +364,7 @@ $enddate=strtotime("+5 days", $startdate);
         } 
 
 
-    function Mens3(){
+    function Mens2(){
 
         window.location.href = "https://api.whatsapp.com/send?phone=56944735189&text=Hola,%20tengo%20una%20consulta";
     
