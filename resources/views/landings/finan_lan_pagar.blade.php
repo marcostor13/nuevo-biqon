f54d49bf67cb8cd562242692f238e735a8330f3d
Jesus Cortez <cortezjesus01@gmail.com>
2020-04-09 15:14:04 -0400

    Update finan_lan_pagar.blade.php
    vs

--- resources/views/landings/finan_lan_pagar.blade.php
+++ resources/views/landings/finan_lan_pagar.blade.php
@@ -94,7 +94,7 @@ $startdate=('Y-m-d\TH:i:s');
                                   <button onclick="Mens2(); eventosLanding('WhatsApp');" class="btn bg-primary text-white col-12 mt-2">CONTACTAR POR WHATSAPP</button>
 
                                 <div class="date btn bg-primary text-white col-12 mt-4">
-                                 <span>PROGRAMAR VISITA VISITA MOTOBOY</span>
+                                 <span>PROGRAMAR VISITA MOTOBOY</span>
                                  <input id="date1"  type="datetime-local" class="btn-date text-white" style="border: none;" min="<?php echo date('Y-m-d\TH:i:s') ?>" max="<?php //echo date("Y-m-d", $enddate) ?>"/>
                                </div>
                                <!-- <div class="date btn bg-primary text-white col-12 mt-4">
