22b0bf5da2be346d3732b87051a02660f3f349cf
Jesus Cortez <cortezjesus01@gmail.com>
2020-04-15 12:06:15 -0400

    Update finan_lan_pagar_vvasquez.blade.php
    azxc

--- resources/views/landings/finan_lan_pagar_vvasquez.blade.php
+++ resources/views/landings/finan_lan_pagar_vvasquez.blade.php
@@ -378,12 +378,15 @@ $name = $_GET['Nombre'];
 
      
 
-            function Mens4(){     
+           
+function Mens4(){     
          var rut; 
          rut= getUrlParameter('rut'); 
-   window.location.href = "mailto:valeska.vasquez@financoop.cl?subject=Pago%20de%20Cuenta&body=RUT:  "+rut;
-
-
+         var Nombre; 
+         Nombre= getUrlParameter('Nombre'); 
+         var Telefono; 
+         Telefono= getUrlParameter('Telefono'); 
+   window.location.href = "mailto:valeska.vasquez@financoop.cl?subject=Necesito%20Informacion&body=RUT:  "+rut+" Nombre: "+Nombre+" Telefono: "+Telefono;
  }
     </script>
     
