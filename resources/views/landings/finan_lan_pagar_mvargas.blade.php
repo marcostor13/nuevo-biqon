253efc1633d6102483bd9b900ed38c06e1bb6fd5
Jesus Cortez <cortezjesus01@gmail.com>
2020-04-15 12:05:42 -0400

    Update finan_lan_pagar_mvargas.blade.php
    hg

--- resources/views/landings/finan_lan_pagar_mvargas.blade.php
+++ resources/views/landings/finan_lan_pagar_mvargas.blade.php
@@ -376,13 +376,15 @@ $name = $_GET['Nombre'];
 
 
 
-     
 
-            function Mens4(){     
+function Mens4(){     
          var rut; 
          rut= getUrlParameter('rut'); 
-   window.location.href = "mailto:maribel.vargas@financoop.cl?subject=Pago%20de%20Cuenta&body=RUT:  "+rut;
-
+         var Nombre; 
+         Nombre= getUrlParameter('Nombre'); 
+         var Telefono; 
+         Telefono= getUrlParameter('Telefono'); 
+   window.location.href = "mailto:maribel.vargas@financoop.cl?subject=Necesito%20Informacion&body=RUT:  "+rut+" Nombre: "+Nombre+" Telefono: "+Telefono;
 
  }
     </script>
