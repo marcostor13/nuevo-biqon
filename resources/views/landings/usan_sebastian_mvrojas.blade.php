3909663cd98d1ca37545346bbfe4d33cced8142f
Jesus Cortez <cortezjesus01@gmail.com>
2020-04-16 14:26:41 -0400

    Update usan_sebastian_mvrojas.blade.php
    cc

--- resources/views/landings/usan_sebastian_mvrojas.blade.php
+++ resources/views/landings/usan_sebastian_mvrojas.blade.php
@@ -292,7 +292,7 @@ $enddate=strtotime("+5 days", $startdate);
                     'landing': '{!! $landing->name !!}'
                 } 
             }
-            var correo = ["jesus.binteraction@gmail.com"];
+            var correo = ["mariavictoria.rojas@uss.cl"];
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
