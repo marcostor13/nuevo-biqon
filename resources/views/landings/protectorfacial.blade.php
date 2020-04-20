1f81220f2bff95efb645a99ded0cf8a6a342e9ec
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-04-20 19:17:52 -0400

    Update protectorfacial.blade.php
    mod

--- resources/views/landings/protectorfacial.blade.php
+++ resources/views/landings/protectorfacial.blade.php
@@ -101,7 +101,7 @@ $enddate=strtotime("+15 days", $startdate);
                          <div>
                  <button onclick="Mens2(); eventosLanding('WhatsApp');" class="btn bg-primary text-white col-12 mt-2">CONTACTAR POR WHATSAPP</button>
 
-                  <a onclick="eventosLanding('Correo'); Mens();" class="btn bg-primary text-white col-12 mt-2">ESCRÍBENOS</a> 
+                  <a onclick="eventosLanding('Correo'); Mens();" class="btn bg-primary text-white col-12 mt-2">CONTACTAR POR CORREO</a> 
 
                 
             </div>
@@ -365,9 +365,9 @@ $enddate=strtotime("+15 days", $startdate);
 
         // var body_message = "%3C%2Fbr%3E Estimado paciente,%3C%2Fbr%3E favor envíe su consulta relacionada al pago de su cuenta hospitalaria. %3C%2Fbr%3E Saludos cordiales %3C%2Fbr%3E %3C%2Fbr%3E Atte. Contact Center %3C%2Fbr%3E Clínica Dávila %3C%2Fbr%3E Fono: 22730800 opción 2";
 
-         var body_message = "%0A%20Estimado%20Cliente,%0A%20favor%20env%C3%ADe%20su%20consulta%20relacionada%20a%20la%20compra%20de%20protectores%20faciales.%20%0A%20Saludos%20cordiales%20%0A%20%0A%20Atte.%20Fernando%20Vergara%20%0A%20%0A%20Fono:%2056975885064";
+         var body_message = "%0A%20Estimado%20Cliente,%0A%20favor%20env%C3%ADe%20su%20consulta%20relacionada%20a%20la%20compra%20de%20protectores%20faciales.%20%0A%20Saludos%20cordiales%20%0A%20%0A%20Atte.%20Fernando%20Vergara%20%0A%20Fono:%2056975885064";
 
-   window.location.href = "mailto:maicol.binteraction@gmail.com?subject=Quiero%20Consultar%20Sobre%20las%20Mascarillas&body=%20RUT:%20"+rut+" "+body_message;
+   window.location.href = "mailto:maicol.binteraction@gmail.com?subject=Quiero%20Consultar%20Sobre%20Los%20Protecectores%20Faciales&body=%20RUT:%20"+rut+" "+body_message;
 
  }
     </script>
