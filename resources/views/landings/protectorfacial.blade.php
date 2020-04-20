ad627933320a398fceafa0cdf2d9b762cff2661d
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-04-20 17:19:37 -0400

    Update protectorfacial.blade.php
    add img

--- resources/views/landings/protectorfacial.blade.php
+++ resources/views/landings/protectorfacial.blade.php
@@ -7,6 +7,14 @@
 $startdate=strtotime("Today");
 $enddate=strtotime("+15 days", $startdate);
 ?>
+<head>
+    <!-- Compiled and minified Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<!-- Minified JS library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+</head>
 <style>
 .opaco{
     display: none !important; 
@@ -98,6 +106,41 @@ $enddate=strtotime("+15 days", $startdate);
                     </center>
 
                     </div>
+
+
+                     <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="https://i.imgur.com/kboog8K.jpg"  alt="img1">
+    </div>
+
+    <div class="item">
+      <img src="https://i.imgur.com/dHMNNmf.jpg" alt="img2">
+    </div>
+
+    <div class="item">
+      <img src="https://i.imgur.com/lQYtCVE.jpg" alt="img3">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
           
                    
