467bd43846c8842c4a5f5d3031606a3ec3645217
Jesus Cortez <cortezjesus01@gmail.com>
2020-04-23 15:51:58 -0400

    Update simple.blade.php
    bvc01

--- resources/views/landings/simple.blade.php
+++ resources/views/landings/simple.blade.php
@@ -56,19 +56,19 @@
 
             <div  id="cont5" class="p-2 pl-3 pr-3 hide">
                 <div>
-                <img class="img-fluid align-items-center col-10 col-md-3 mt-4" src="https://i.imgur.com/VpN3RT0.png" alt="simple">
+                <img class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/VpN3RT0.png" alt="simple">
                 <a onclick="Cor1();" class="btn bg-danger text-white col-12 mt-2">Comprar</a>
 
-                <img class="img-fluid align-items-center col-10 col-md-3 mt-4" src="https://i.imgur.com/0hhuMHg.png" alt="simple">
+                <img class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/0hhuMHg.png" alt="simple">
                 <a onclick="Cor2();" class="btn bg-danger text-white col-12 mt-2">Comprar</a>
                 <br>
                 </div>
 
                 <div>
-                <img class="img-fluid align-items-center col-10 col-md-3 mt-4" src="https://i.imgur.com/nIKsFm0.png" alt="simple">
+                <img class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/nIKsFm0.png" alt="simple">
                 <a onclick="Cor3();" class="btn bg-danger text-white col-12 mt-2">Comprar</a>
-                
-                <img class="img-fluid align-items-center col-10 col-md-3 mt-4" src="https://i.imgur.com/IUGXSOF.png" alt="simple">
+
+                <img class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/IUGXSOF.png" alt="simple">
                 <a onclick="Cor4();" class="btn bg-danger text-white col-12 mt-2">Comprar</a>
                 <br>
                </div>
