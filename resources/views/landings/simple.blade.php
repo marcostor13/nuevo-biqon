891b7f7432b9a2c5e2397b37a45335457f5b5809
Jesus Cortez <cortezjesus01@gmail.com>
2020-04-24 17:43:29 -0400

    Update simple.blade.php
    bvc

--- resources/views/landings/simple.blade.php
+++ resources/views/landings/simple.blade.php
@@ -58,20 +58,24 @@
 
             <div  id="cont7" class="p-2 pl-3 pr-3 hide">
                 <div>
-                <img class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/VpN3RT0.png" alt="simple">
-                <a onclick="Cor1();" class="btn bg-danger text-white col-12 mt-2">Comprar</a>
+                <img class="img-fluid col-5 col-md-3 mt-4" src="https://i.imgur.com/VpN3RT0.png" alt="simple">             
 
-                <img class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/0hhuMHg.png" alt="simple">
-                <a onclick="Cor2();" class="btn bg-danger text-white col-12 mt-2">Comprar</a>
+                <img class="img-fluid col-5 col-md-3 mt-4" src="https://i.imgur.com/0hhuMHg.png" alt="simple">
+
+                <a onclick="Cor1();" class="btn bg-danger text-white col-5 mt-2">Comprar</a>
+
+                <a onclick="Cor2();" class="btn bg-danger text-white col-5 mt-2">Comprar</a>
                 <br>
                 </div>
 
                 <div>
-                <img class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/nIKsFm0.png" alt="simple">
-                <a onclick="Cor3();" class="btn bg-danger text-white col-12 mt-2">Comprar</a>
+                <img class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/nIKsFm0.png" alt="simple">            
 
                 <img class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/IUGXSOF.png" alt="simple">
-                <a onclick="Cor4();" class="btn bg-danger text-white col-12 mt-2">Comprar</a>
+
+                <a onclick="Cor3();" class="btn bg-danger text-white col-5 mt-2">Comprar</a>
+
+                <a onclick="Cor4();" class="btn bg-danger text-white col-5 mt-2">Comprar</a>
                 <br>
                </div>
             </div>
