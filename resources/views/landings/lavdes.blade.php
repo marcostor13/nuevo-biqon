6a571d011f43fd086446aa96d883422d1e915cf7
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-04-28 13:05:47 -0400

    Update lavdes.blade.php

--- resources/views/landings/lavdes.blade.php
+++ resources/views/landings/lavdes.blade.php
@@ -64,7 +64,7 @@ $enddate=strtotime("+15 days", $startdate);
 </style>
 
 
- <div id="LAVDES" class="oscurecer" style="background: url('https://i.imgur.com/Y36Y2nZ.jpg'); background-repeat: no-repeat; background-size: cover; background-opacity: 10%" >
+ <div id="LAVDES" class="oscurecer" style="background: url('https://i.imgur.com/Y36Y2nZ.jpg'); background-repeat: no-repeat; background-size: cover; background-opacity: 5%" >
 
         <div class="opaco"></div>
 
