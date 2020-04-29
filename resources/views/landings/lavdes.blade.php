9ac494114f92f8650542ae304f87f94d92bbb4a8
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-04-29 09:53:14 -0400

    Update lavdes.blade.php

--- resources/views/landings/lavdes.blade.php
+++ resources/views/landings/lavdes.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.landing')
 
-@section('title', 'LAVAGGIO & DESINFECKTION SPA')
+@section('title', 'PROTECTOR FACIAL')
 
 @section('content')
 <?php
@@ -61,23 +61,18 @@ $enddate=strtotime("+15 days", $startdate);
       rgba(0, 0, 0, 0.5)
     )
     }
-}
 </style>
 
- <div class="opaco"></div>
+        <div class="opaco"></div>
 
        <div align="center" class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-             <img style="max-width: 90%" class="img-fluid col-md-3 mt-2" src="https://i.imgur.com/FD5RsDA.png" alt="LAVAGGIO & DESINFECKTION SPA">
+            <img style="max-width: 40%" class="img-fluid col-md-3 mt-2" src="https://i.imgur.com/FD5RsDA.png" alt="LAVAGGIO & DESINFECKTION SPA" width="500" height="1000">
 
-             <center><img src="https://i.imgur.com/vCuU0aH.png"></center>
+               <center><img src="https://i.imgur.com/vCuU0aH.png"></center>
 
             <div id="cont1">
                             <div class="card">     
                 <div class="card-body" >
-
-                  
-
-
 
 
                     <div class="text-align:right">
@@ -99,17 +94,66 @@ $enddate=strtotime("+15 days", $startdate);
 
                         <h3 >Consultas a: </h3>
 
-                         <button onclick="Mens2(); eventosLanding('WhatsApp');" class="btn bg-danger text-black col-8 mt-2"> <strong>CONTACTAR POR WHATSAPP</strong></button>
-
-                         <a onclick="eventosLanding('Correo'); Mens();" class="btn bg-danger text-black col-8 mt-2"> <strong>CONTACTAR POR CORREO </strong></a>
-                        </p>
                     <center>
                         </div>
                          <div>
+                 <button onclick="Mens2(); eventosLanding('WhatsApp');" class="btn bg-danger text-black col-8 mt-2"> <strong>CONTACTAR POR WHATSAPP</strong></button>
+
+                         <a onclick="eventosLanding('Correo'); Mens();" class="btn bg-danger text-black col-8 mt-2"> <strong>CONTACTAR POR CORREO </strong></a>
+
+                
+            </div>
+                    </center>
+
+                    </div>
 
 
+                     <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
 
- 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="https://i.imgur.com/kboog8K.jpg"  alt="img1">
+    </div>
+
+    <div class="item">
+      <img src="https://i.imgur.com/dHMNNmf.jpg" alt="img2">
+    </div>
+
+    <div class="item">
+      <img src="https://i.imgur.com/lQYtCVE.jpg" alt="img3">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+          
+                   
+                
+                        </div>
+                
+        </div>
+
+         <div class="d-flex justify-content-around align-content-center mt-1"> 
+
+            
+                                                            
+            </div>
           
      <script>
    //EVENT 1
@@ -305,7 +349,7 @@ $enddate=strtotime("+15 days", $startdate);
         };
 
       
-        function Mens2(){
+       function Mens2(){
 
         window.location.href = "https://api.whatsapp.com/send?phone=56981868778&text=Hola,%20tengo%20una%20consulta";
     
