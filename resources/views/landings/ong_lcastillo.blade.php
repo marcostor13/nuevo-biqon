991344d639108a9babae7bc016490ba9d51dae4d
Jesus Cortez <cortezjesus01@gmail.com>
2020-05-05 14:08:25 -0400

    Update ong_lcastillo.blade.php
    mn

--- resources/views/landings/ong_lcastillo.blade.php
+++ resources/views/landings/ong_lcastillo.blade.php
@@ -88,8 +88,8 @@ $name = $_GET['NOMBRE'];
                             En estos difíciles días que estamos pasando en el país,</strong> <strong class="text-primary">FINANCOOP</strong><strong>, tiene un Crédito refinanciamiento con dinero disponible </strong><strong class="text-primary">para los socios al día</strong></h5>
                                   <h5> <strong> 
                               <br>* Nuevo monto de crédito $ 1.600.000<br>
-                              <br>* Plazo 90  días, descuento en Junio<br>
-                              <br>* Créditos socios nuevos o Compras de cartera hasta $ 1.500.000<br>
+                              <br>* Plazo 90  días, descuento en Julio<br>
+                              <!--<br>* Créditos socios nuevos o Compras de cartera hasta $ 1.500.000<br>-->
                               <br>* Enviar foto por WhatsApp de Ultima Liquidación de Sueldo + C. de Identidad + Comprobante de Domicilio<br>
                                    </strong> </h5>
                                </p></h4>
