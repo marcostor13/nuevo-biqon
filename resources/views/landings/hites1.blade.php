1ca02b47d5c782a5e4c54db832dbff577c4f6b49
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2020-05-13 12:09:41 -0400

    Update hites1.blade.php
    md

--- resources/views/landings/hites1.blade.php
+++ resources/views/landings/hites1.blade.php
@@ -331,7 +331,7 @@ $enddate=strtotime("+5 days", $startdate);
 
         // var body_message = "%3C%2Fbr%3E Estimado paciente,%3C%2Fbr%3E favor envíe su consulta relacionada al pago de su cuenta hospitalaria. %3C%2Fbr%3E Saludos cordiales %3C%2Fbr%3E %3C%2Fbr%3E Atte. Contact Center %3C%2Fbr%3E Clínica Dávila %3C%2Fbr%3E Fono: 22730800 opción 2";
 
-         var body_message = "%0A%20Estimado%20Cliente,%0A%20favor%20env%C3%ADe%20su%20consulta%20relacionada%20al%20pago%20de%20su%20cuenta%20.%20%0A%20Saludos%20cordiales%20%0A%20%0A%20Atte.%20Hites";
+         var body_message = "%0A%20Estimado%20Cliente,%0A%20favor%20env%C3%ADe%20su%20consulta%20relacionada%20al%20pago%20de%20su%20cuenta.%20%0A%20Saludos%20cordiales%20%0A%20%0A%20Atte.%20Hites";
 
    window.location.href = "mailto:jesus.binteraction@gmail.com?subject=Pago%20de%20Cuenta%20&body= "+body_message;
 
