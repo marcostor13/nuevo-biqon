80f5b294bf27a74f8e2cc91998d095d09eb205d2
Jesus Cortez <cortezjesus01@gmail.com>
2020-05-13 09:48:07 -0400

    Update hites2.blade.php
    vcfd

--- resources/views/landings/hites2.blade.php
+++ resources/views/landings/hites2.blade.php
@@ -50,11 +50,11 @@ $enddate=strtotime("+5 days", $startdate);
 
                 <button onclick='mas(); eventosLanding("Pagina de Pagos");' class="btn bg-primary text-white col-12 mt-4">VER CANALES DE PAGO</button>
 
-                <a onclick="eventosLanding('Llamar'); Mens();" class="btn bg-primary text-white col-12 mt-2"><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png">LLÁMANOS</a>
+                <a onclick="eventosLanding('Llamar'); Mens();" class="btn bg-primary text-white col-12 mt-2"><img width="30" src="https://img.icons8.com/wired/64/FFFFFF/phonelink-ring.png"> LLÁMANOS</a>
 
-                <a onclick="eventosLanding('Correo'); Mens2();" class="btn bg-primary text-white col-12 mt-2"><img width="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/email.png">ESCRÍBENOS</a>
+                <a onclick="eventosLanding('Correo'); Mens2();" class="btn bg-primary text-white col-12 mt-2"><img width="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/email.png"> ESCRÍBENOS</a>
 
-                <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-primary text-white col-12 mt-4">YA PAGUE</button>
+                <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-primary text-white col-12 mt-4"><img width="30" src="https://upload.wikimedia.org/wikipedia/commons/b/b0/Light_green_check.svg"> YA PAGUE</button>
             </div>
             
 
