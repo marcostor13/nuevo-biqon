41e9ca375dd08187f8488520c85cc0c301df4f2d
Jesus Cortez <cortezjesus01@gmail.com>
2020-05-13 10:14:27 -0400

    Update hites2.blade.php
    vc03

--- resources/views/landings/hites2.blade.php
+++ resources/views/landings/hites2.blade.php
@@ -7,6 +7,10 @@
 $startdate=strtotime("Today");
 $enddate=strtotime("+5 days", $startdate);
 ?>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <style>
 .opaco{
     display: none !important; 
