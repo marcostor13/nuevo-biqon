09cf6e881ceef00f2652bd30e49c6ef44554ec08
Jesus Cortez <cortezjesus01@gmail.com>
2020-05-22 15:25:26 -0400

    Update bancofalabella.blade.php
    vc45

--- resources/views/landings/bancofalabella.blade.php
+++ resources/views/landings/bancofalabella.blade.php
@@ -80,7 +80,7 @@ $enddate=strtotime("+15 days", $startdate);
             <div > <!--texto--><br>
                 <h3>¡Paga tu CMR en línea!</h3>
                 <h3>¿Solo has pagado en sucursales y nunca por la web?</h3>
-                <div class="card-body" > 
+                <div  class="p-2 pl-3 pr-3"> 
                     <h3>¡No te preocupes!<br>
                     nosotros te enseñamos </h3> <img class="img-fluid col-md-3 mt-2" src="https://i.imgur.com/DCPCot4.jpg" alt="FALABELLA">
                 </div>  
