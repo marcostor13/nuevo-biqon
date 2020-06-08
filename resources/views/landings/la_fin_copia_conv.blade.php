b4cdf7064892394ae60319bc7014973455eee655
Jesus Cortez <cortezjesus01@gmail.com>
2020-06-08 16:53:20 -0400

    Update la_fin_copia_conv.blade.php
    vc2

--- resources/views/landings/la_fin_copia_conv.blade.php
+++ resources/views/landings/la_fin_copia_conv.blade.php
@@ -89,7 +89,7 @@ $name = $_GET['Nombre'];
                 <div class="card-body">
                   <h3 class="text-black text-center"> <strong> Estimado(a) <span><?php echo $name; ?></span>  </strong> </h3>
                    
-                <h4> Aprovecha nuestro convenio y solicita tu crédito pre aprobado con Financoop, para lo que necesites </h4>
+                <h4> Financoop tiene buenas noticias para ti, tienes un crédito aprobado en excelentes condiciones, para mas información comunícate con tu ejecutivo en el siguiente link. </h4>
             
             </div> 
 
