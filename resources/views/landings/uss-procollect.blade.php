39161394a8c3ad5f4ff8198c4848c77b385bd4b5
Marcos Torres <marcostor13@gmail.com>
2020-06-11 16:56:08 -0500

    ol

--- resources/views/landings/uss-procollect.blade.php
+++ resources/views/landings/uss-procollect.blade.php
@@ -120,14 +120,14 @@
     <script>
    //EVENT 1
         
-        $(function(){
+        $(async function(){
             events({    
                 'name': 'Visita',
                 'landing_id': {!! $landing->id !!},
                 'json_datos': JSON.stringify(getAllUrlParameter())
             });
 
-            console.log('getAllUrlParameter', getAllUrlParameter().getValue());
+            console.log('getAllUrlParameter', await getAllUrlParameter().getValue());
         }); 
 
       
