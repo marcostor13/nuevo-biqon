5baad1d8d2dd688f54a3ccbfb4cedd51571aa600
Marcos Torres <marcostor13@gmail.com>
2020-06-11 16:52:52 -0500

    ol

--- resources/views/landings/uss-procollect.blade.php
+++ resources/views/landings/uss-procollect.blade.php
@@ -127,7 +127,7 @@
                 'json_datos': JSON.stringify(getAllUrlParameter())
             });
 
-            console.log(getAllUrlParameter());
+            console.log('getAllUrlParameter', getAllUrlParameter());
         }); 
 
       
