e75f7e5b8312e09355344504fffc2a762e007058
Jesus Cortez <cortezjesus01@gmail.com>
2020-07-10 13:32:47 -0400

    Update ugm.blade.php
    vfr

--- resources/views/landings/ugm.blade.php
+++ resources/views/landings/ugm.blade.php
@@ -155,7 +155,7 @@ $enddate=strtotime("+5 days", $startdate);
           </div>
 
                
-            </div>
+           
             <div id="cont3" class="p-2 pl-3 pr-3 hide">
                         <div class="card">     
                             <div class="card-body">
@@ -163,9 +163,8 @@ $enddate=strtotime("+5 days", $startdate);
                             </div>
                         </div>
                     </div>
-
-        </div>
-    </div>
+ </div>
+    
 
      <script>
    //EVENT 1
