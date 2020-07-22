7b80e2afc4746e6b71d5bf1f12be1b2b95bf1b84
Jesus Cortez <cortezjesus01@gmail.com>
2020-07-22 11:19:26 -0400

    Update amicar_hyundai.blade.php
    vc09

--- resources/views/landings/amicar_hyundai.blade.php
+++ resources/views/landings/amicar_hyundai.blade.php
@@ -39,13 +39,13 @@ $enddate=strtotime("+15 days", $startdate);
 
                        </div>
                    </div>-->
-                    <span align="text-center">GRAND i10</span><br>
+                    <label align="text-center">GRAND i10</label><br>
                  <img style="max-width: 60%" class="img-fluid col-3 col-md-3 mt-2" src="https://i.imgur.com/u7eUfAn.png" alt="simple"><a onclick="port1();" class="btn bg-warning text-white col-3 mt-2">Contactar</a>
                  <br>  <br>  <br>
-                  <span>ATOS</span><br>
+                  <label align="text-center">ATOS</label><br>
                 <img  style="max-width: 60%" class="img-fluid col-3 col-md-3 mt-2" src="https://i.imgur.com/ForBfaH.png" alt="simple"><a onclick="port2();" class="btn bg-warning text-white col-3 mt-2">Contactar</a>
                 <br>  <br>  <br>
-                    <span>VERNA</span><br>
+                    <label align="text-center" class=" col-6 col-md-12 mt-2">VERNA</label><br>
                 <img style="max-width: 60%" class="img-fluid col-3 col-md-3 mt-2" src="https://i.imgur.com/CUNfPsP.png" alt="simple"><a onclick="port2();" class="btn bg-warning text-white col-3 mt-2">Contactar</a>
 
                
