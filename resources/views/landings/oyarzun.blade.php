e70105cfdd6e4ff69944b31bce286e1a0c1eab8f
Jesus Cortez <cortezjesus01@gmail.com>
2020-07-31 14:12:48 -0400

    Update oyarzun.blade.php
    hd

--- resources/views/landings/oyarzun.blade.php
+++ resources/views/landings/oyarzun.blade.php
@@ -86,11 +86,11 @@ $enddate=strtotime("+5 days", $startdate);
                
 
                 <button onclick="Mens1(); eventosLanding('Pago Transferencia');" class="btn bg-success text-white col-12 mt-2">PAGAR AHORA </button>
-                   <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-success text-white col-12 mt-2">YA PAGUE</button>
-                    <!--<div class="date btn bg-success text-white col-12 mt-2">
+                   <!--<button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-success text-white col-12 mt-2">YA PAGUE</button>-->
+                    <div class="date btn bg-success text-white col-12 mt-2">
                     <span>AGENDAR COMPROMISO DE PAGO</span>
-                    <input id="date1"  type="date" class="btn-date text-black" style="border: none;" min="<?php //echo date('Y-m-d') ?>" max="<?php //echo date("Y-m-d", $enddate) ?>"/>
-                </div>-->
+                    <input id="date1"  type="date" class="btn-date text-black" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
+                </div>
                 
                 
                 <button onclick="Mens2(); eventosLanding('Llamar');" class="btn bg-success text-white col-12 mt-2">LLAMAR A EJECUTIVO </button>   
