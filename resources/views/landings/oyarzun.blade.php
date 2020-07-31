3681d2599aa6584e1ec979a31712718a33a5722e
Jesus Cortez <cortezjesus01@gmail.com>
2020-07-31 08:27:15 -0400

    Update oyarzun.blade.php
    vc3

--- resources/views/landings/oyarzun.blade.php
+++ resources/views/landings/oyarzun.blade.php
@@ -57,6 +57,7 @@ $enddate=strtotime("+5 days", $startdate);
         <div class="opaco"></div>
 
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
+            <br><br>
             <!-- <img style="max-width:320px; max-height:180px" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="Oyarzun"> --><h1> <strong>Oyarzun y Cia Ltda </strong></h1>
 
              <div id="cont1" class="p-5 hide">
