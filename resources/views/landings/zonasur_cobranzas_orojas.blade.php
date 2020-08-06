ba245170527925d9fe51cc3ab17bf3cb02df19ce
Jesus Cortez <cortezjesus01@gmail.com>
2020-08-06 19:36:14 -0400

    Update zonasur_cobranzas_orojas.blade.php
    26

--- resources/views/landings/zonasur_cobranzas_orojas.blade.php
+++ resources/views/landings/zonasur_cobranzas_orojas.blade.php
@@ -71,28 +71,22 @@ $enddate=strtotime("+5 days", $startdate);
                             </div>
                         </div>
                     </div>
-            <div id="cont2" class="p-5 hide">
-                <h3 class="text-black text-center">Estimado(a) <span id="name"></span></h3>
+               <div id="cont2" class="p-5 hide">
+                <h3 class="text-black text-center">Estimado <span id="name"></span></h3>
 
-                <p class="text-black" align="text-center">
-                    La seguridad de nuestro equipo y de nuestros clientes es lo primero. Por eso, aborda tus inquietudes por nuestros medios de contacto a distancia. Así, evitamos que te expongas.  
-<br>
- Prefiere lo digital <a href="www.tanner.cl"> www.tanner.cl </a>
-<br>
-#EstamosJuntosEnEsto
-                </p>
-               
-               
+               <p class="text-black" align="text-center">
+                    Informamos que su crédito automotriz se encuentra en mora.
+                    <br>Para regularizar su situación tiene un ejecutivo asignado con quien evaluar las alternativas disponibles.<br> #EstamosJuntosEnEsto  <br></p>
 
-                <button onclick="Mens1(); eventosLanding('Pago Transferencia');" class="btn bg-success text-white col-12 mt-2">PAGAR AHORA </button>
-                   <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-success text-white col-12 mt-2">YA PAGUE</button>
-                    <div class="date btn bg-success text-white col-12 mt-2">
-                    <span>AGENDAR COMPROMISO DE PAGO</span>
-                    <input id="date1"  type="date" class="btn-date text-black" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
-
-                </div>
+                    <button onclick="Mens1(); eventosLanding('Pago Transferencia');" class="btn bg-success text-white col-12 mt-2">PAGAR AHORA </button>
                 
-                <button onclick="Mens2(); eventosLanding('Llamar');" class="btn bg-success text-white col-12 mt-2">LLAMAR A EJECUTIVO </button>   
+
+                <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-success text-white col-12 mt-2">YA PAGUE</button>
+
+                 <button class="btn bg-success text-white col-12 mt-2" onclick="sendMail('El cliente solicito que contactarse');eventosLanding('Contactarse');">QUIERO QUE ME CONTACTEN</button>
+            
+                
+                 <button onclick="Mens2(); eventosLanding('Llamar');" class="btn bg-success text-white col-12 mt-2">LLAMAR A EJECUTIVO </button>   
 
                  <button onclick="Mens3(); eventosLanding('whatsapp');" class="btn bg-success text-white col-12 mt-2">WHATSAPP EJECUTIVO </button>
 
