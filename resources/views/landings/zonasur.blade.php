eed256e5268fcfc8150e778533f6e56f68bf70f7
Jesus Cortez <cortezjesus01@gmail.com>
2020-08-07 10:40:42 -0400

    Update zonasur.blade.php
    vc

--- resources/views/landings/zonasur.blade.php
+++ resources/views/landings/zonasur.blade.php
@@ -200,7 +200,7 @@ $enddate=strtotime("+5 days", $startdate);
                     'landing': '{!! $landing->name !!}'
                 } 
             }
-            var correo = ["jesus.binteraction@gmail.com", "arojas@zonasursa.cl"];  
+            var correo = ["arojas@zonasursa.cl"];  
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
