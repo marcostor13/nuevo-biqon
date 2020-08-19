e9a3b770f52798927d6b78fba9a4b4232d5e6b13
Jesus Cortez <cortezjesus01@gmail.com>
2020-08-19 08:58:35 -0400

    Update davila_copago02.blade.php
    vc2

--- resources/views/landings/davila_copago02.blade.php
+++ resources/views/landings/davila_copago02.blade.php
@@ -175,7 +175,7 @@
                 if(e.code == 200){
                     $('#cont1').addClass('hide');
                     $('#name').text(e.data.nombre);
-                    $('#dato1').text(id_adm);
+                    $('#dato2').text(id_adm);
                     $('#cont2').removeClass('hide');
                     $('#date1').on('change', function(){
                         if($('#date1').val() != ''){
