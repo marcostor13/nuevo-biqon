e2d8719c32562d35d6f86e42f9e3a100030bd01a
Jesus Cortez <cortezjesus01@gmail.com>
2020-08-24 09:24:58 -0400

    Update acme.blade.php
    bvf

--- resources/views/landings/acme.blade.php
+++ resources/views/landings/acme.blade.php
@@ -8,7 +8,7 @@
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <style type="text/css">
-    
+       
 .card {
   box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
   border: 0;
