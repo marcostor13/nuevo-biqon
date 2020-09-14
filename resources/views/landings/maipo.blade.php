ed83a45e52bbf0729ad5ffa467ec9d3ca2f46b59
Jesus Cortez <cortezjesus01@gmail.com>
2020-09-14 13:34:43 -0300

    Update maipo.blade.php
    vc3

--- resources/views/landings/maipo.blade.php
+++ resources/views/landings/maipo.blade.php
@@ -58,7 +58,7 @@ $name = $_GET['NOMBRE'];
         <!--<div class="opaco"></div>-->
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
    <img style="height:180px; width:250px;" class="img-fluid col-md-6 mt-1 hide" src="https://i.imgur.com/Z7pozIJ.jpeg" alt="HerediaAbogados">
-   <img style="height:100px; width:500px;" class="img-fluid col-md-6 mt-1 " src="https://i.imgur.com/8E4iFKk.png" alt="HerediaAbogados">
+   <img style="height:100px; width:500px;" class="img-fluid col-md-6 mt-1 hide" src="https://i.imgur.com/8E4iFKk.png" alt="HerediaAbogados">
 
    <img style="height:180px; width:450px;" class="img-fluid col-md-6 mt-1" src="https://i.imgur.com/Z7pozIJ.jpeg" alt="HerediaAbogados">
 
