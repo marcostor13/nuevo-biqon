cd4af9029fb1b72ff9c1c6d4c4fe79ec674e4f1a
Jesus Cortez <cortezjesus01@gmail.com>
2020-10-22 10:51:58 -0300

    Update prain_mcredit_dpulido.blade.php
    vc3

--- resources/views/landings/prain_mcredit_dpulido.blade.php
+++ resources/views/landings/prain_mcredit_dpulido.blade.php
@@ -1,91 +1,72 @@
 @extends('layouts.landing')
 
-@section('title', 'MUNDOCREDITO')
+@section('title', 'Mundo Credito')
 
 @section('content')
-
- <?php
+<?php
 $startdate=strtotime("Today");
-$enddate=strtotime("+7 days", $startdate);
+$enddate=strtotime("+6 days", $startdate);
+$codigo = $_GET['DATA2'];
 ?>
-<style type="text/css">
-    
-.card {
-  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-  border: 0;
-  font-weight: 400;
-}
-.card[class*=border] {
-  border: 1px solid #9e9e9e;
-  box-shadow: none;
-||.card .card-body h1, .card .card-body h2, .card .card-body h3, .card .card-body h4, .card .card-body h5, .card .card-body h6 {
-  font-weight: 400;
-}
-.card .card-body .card-title a {
-  transition: 0.2s ease-in-out;
-}
-.card .card-body .card-title a:hover {
-  transition: 0.2s ease-in-out;
-}
-.card .card-body .card-text {
-  color: #747373;
-  font-size: 0.9rem;
-  font-weight: 400;
-}
-.card .md-form label {
-  font-weight: 300;
-}
+<style>
+.opaco{
+    display: none !important; 
 
-.oscurecer {
-  background-image: 
-    linear-gradient(
-      rgba(0, 0, 0, 0.5),
-      rgba(0, 0, 0, 0.5)
-    )
-    }
+}
+.input{
+  border-color: #A1CBF3 !important;
     
+}
 </style>
-   <!-- <div id="marubeni" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;">-->
-
+   <!-- <div id="mundocredito" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;">-->
 
         <div class="opaco"></div>
 
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
-            <img style="max-width:250px; max-height:100px" class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="MUNDOCREDITO">
+            <img style="max-width:250px; max-height:100px " class="img-fluid col-10 col-md-3 mt-2" src="{{$landing->logo}}" alt="mundocredito">
 
-            <div id="cont1" class="p-3 ">|
-                <h3 class="text-white text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
-                <input id="rut" type="number" class="form-control mt-4 text-center text-black">
-                <button onclick="event1(); return false;" class="btn bg-danger text-white col-12 mt-4">Validar</button>
-                </div>
+            <div id="cont1" class="p-5 ">
+                <h3 class="text-black text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
+                <input id="rut" type="number" class="form-control mt-4 text-center text-blue input" autofocus>
+                <button onclick="event1(); return false;" class="btn bg-primary text-white col-12 mt-4">Validar </button>
+            </div>
 
-                <div id="cont5" class="p-2 pl-3 pr-3 hide">
+            <div id="cont5" class="p-2 pl-3 pr-3 hide">
                         <div class="card">     
                             <div class="card-body">
                                  <h5 id="error"class="text-black text-center mt-5"></h5>
                             </div>
                         </div>
                     </div>
-            <div id="cont2" class="p-5 hide">|
-                <h3 class="text-white text-center">Estimado <span id="name"></span></h3>
 
-                <p class="text-white">
-                    Te informamos que presentas un retraso en el pago de cuotas de tu crédito automotriz.
-                    
-                    Te ofrecemos las siguientes opciones para regularizar tu deuda pendiente.
-                </p>
-                <div class="date btn bg-danger text-white col-12 mt-4">
-                    <span>AGENDAR COMPROMISO DE PAGO</span>
-                    <input id="date1"  type="date" class="btn-date text-danger" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
-                </div>
-                <button onclick='window.location.href="https://www.marubeni.cl/contacto/"; eventosLanding("Pagina de Pagos");' class="btn bg-danger text-white col-12 mt-4">PAGAR AHORA</button>
-                <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-danger text-white col-12 mt-4">YA PAGUE</button>
-                <div class="d-flex justify-content-around align-content-center mt-4">
-                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56967599323&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/whatsapp.png"></a>
-                    <a onclick="eventosLanding('Llamar');" href="tel:+56967599323"><img width="40" src="https://img.icons8.com/wired/64/FFFFFF/phonelink-ring.png"></a>
-                    <a onclick="eventosLanding('Correo');" href="mailto:marubenicredit@prainabogados.cl"><img width="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/email.png"></a>
-                </div>
-                
+            <div id="cont2" class="p-5 hide">
+                <h3 class="text-black text-center"> Señor (a): <span id="name"></span></h3>
+
+                <p class="text-black">  
+                    De nuestra consideración; <br>
+                    <br>
+                    Nuestro cliente MUNDO CREDITO es consciente de la situación actual del país y por tal motivo reiteramos la importancia de mantener la comunicación y lograr brindar la mejor asesoría y respuesta para la regularización de su financiamiento.
+                    <br>
+                    Informamos a usted que se procederá  a interponer  acciones de cobranza en su contra por la deuda que mantiene vigente de su crédito Automotriz correspondiente al código: <?php echo $codigo; ?>
+                    <br>
+                    Le recordamos que el contrato de Prenda sin desplazamiento firmado por usted y la Ley 20.190, contienen una cláusula de aceleración que estipula que el no pago de dos o más cuotas hará exigible el total de la deuda.
+                    <br>
+                    Si requiere información le ofrecemos las siguientes opciones:
+                    <br>
+                                    </p>
+                               <div class="date btn bg-primary text-white col-12 mt-4">
+                        <span>Agendar Compromiso de Pago</span>
+                          <input id="date1"  type="date" class="btn-date text-primary" style="border: none;" min="<?php echo date('Y-m-d') ?>" max="<?php echo date("Y-m-d", $enddate) ?>"/>
+                      </div>          
+
+                      <a onclick="eventosLanding('Llamar');" href="tel:+56967599323" class="btn bg-primary text-white col-12 mt-4">Llamar a Ejecutivo</a> 
+
+                      <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=+56967599323&text=Hola,%20tengo%20una%20consulta"  class="date btn bg-primary text-white col-12 mt-4">Contactar por WhatsApp</a>  
+
+                      <a onclick="eventosLanding('Correo'); Mens2();" class="btn bg-primary text-white col-12 mt-4">Correo a Ejecutivo</a>
+
+                       <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-primary text-white col-12 mt-4">Ya Pague</button>
+
             </div>
             <div id="cont3" class="p-2 pl-3 pr-3 hide">
                         <div class="card">     
@@ -93,12 +74,13 @@ $enddate=strtotime("+7 days", $startdate);
                                 <h5 id="message"class="text-grey text-center mt-3 hide"></h5>
                             </div>
                         </div>
+            </div>
         </div>
-    </div>
- 
+   <!-- </div>-->
+
     <script>
 
-        $(function(){
+     $(function(){
             events({    
                 'name': 'Visita',
                 'landing_id': {!! $landing->id !!},
@@ -154,8 +136,7 @@ $enddate=strtotime("+7 days", $startdate);
             let json_datos = getAllUrlParameter(); 
 
             json_datos.nombre = $('#name').text();
-            json_datos.monto = $('#pay').text();
-
+            
             events({    
                 'name': name,
                 'landing_id': {!! $landing->id !!},
@@ -174,10 +155,11 @@ $enddate=strtotime("+7 days", $startdate);
                 data = {
                     'mensaje': msg,
                     'Nombre': $('#name').text(),
-                    'monto': getUrlParameter('monto'),
+                    //'monto': getUrlParameter('monto'),
                     'RUT': getUrlParameter('rut'),
                     'Telefono': getUrlParameter('telefono'),
-                   
+                     'ejecutivo': getUrlParameter('data1'),
+                   'landing': '{!! $landing->name !!}'
                     
                 }
             }else{
@@ -185,14 +167,15 @@ $enddate=strtotime("+7 days", $startdate);
                 data = {
                     'fecha': date,
                     'nombre': $('#name').text(),
-                    'monto': getUrlParameter('monto'),
+                   // 'monto': getUrlParameter('monto'),
                     'rut': getUrlParameter('rut'),
                     'phone': getUrlParameter('telefono'),
+                     'ejecutivo': getUrlParameter('data1'),
                     'landing': '{!! $landing->name !!}'
                 } 
             }
 
-            var correo = ["marubenicredit@prainabogados.cl"];
+            var correo = ["dpulido@prainabogados.cl"];
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
@@ -283,7 +266,10 @@ $enddate=strtotime("+7 days", $startdate);
         };
 
       
-        
+            function Mens2(){
+         var rut; 
+         rut= getUrlParameter('rut'); 
+   window.location.href = "mailto:dpulido@prainabogados.cl?subject=Pago%20de%20Cuenta%20%20RUT:%20"+rut;
     
     </script>
     
