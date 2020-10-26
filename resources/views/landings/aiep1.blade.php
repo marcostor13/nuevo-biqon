7310e45340058438de53e368ad8d46b8d2534f11
Jesus Cortez <cortezjesus01@gmail.com>
2020-10-26 18:05:15 -0300

    Update aiep1.blade.php
    VC3

--- resources/views/landings/aiep1.blade.php
+++ resources/views/landings/aiep1.blade.php
@@ -733,6 +733,15 @@ function url23(){ //tarifa
             sendMail(msg1);
         }
     </script>
-    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="js/jquery-ui-datepicker.min.js"></script>
+<script>
+    $('#calendar').datepicker({
+        inline: true,
+        firstDay: 1,
+        showOtherMonths: true,
+        dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    });
+</script>
 
 @endsection
