83f6ebfa0f0dec76d441dc3340dc586744b62df4
Jesus Cortez <cortezjesus01@gmail.com>
2020-10-27 11:58:10 -0300

    Update aiep1.blade.php
    vc

--- resources/views/landings/aiep1.blade.php
+++ resources/views/landings/aiep1.blade.php
@@ -323,9 +323,6 @@ $monto=$_GET["MONTO"];
     prevText: '<i class="fa fa-fw fa-angle-left"></i>',
     nextText: '<i class="fa fa-fw fa-angle-right"></i>'
   });
-   $("#date1").datepicker({
-    dateFormat: 'dd/mm/yy',
-    }).datepicker("setDate", new Date());
 });
 
    //EVENT 1
