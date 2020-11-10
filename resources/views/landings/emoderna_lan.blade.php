162d6ec5afec44142dbf30ff499977038c8ad7a4
Jesus Cortez <cortezjesus01@gmail.com>
2020-11-10 11:30:11 -0300

    Update emoderna_lan.blade.php
    vc3

--- resources/views/landings/emoderna_lan.blade.php
+++ resources/views/landings/emoderna_lan.blade.php
@@ -62,7 +62,7 @@ $name = $_GET['NOMBRE'];
      <div class="opaco">
 
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-            <img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="EMODERNA">
+            <img style="max-width: 80%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="EMODERNA">
             
             <div class=col-md-12>
            
