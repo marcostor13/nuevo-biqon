bbb3939d6cf08d8803b900e929f5835acb5aba53
Jesus Cortez <cortezjesus01@gmail.com>
2020-11-11 08:51:51 -0300

    Update zonasur_cobranzas_cindymonsalves.blade.php
    vc2

--- resources/views/landings/zonasur_cobranzas_cindymonsalves.blade.php
+++ resources/views/landings/zonasur_cobranzas_cindymonsalves.blade.php
@@ -74,8 +74,8 @@ $enddate=strtotime("+5 days", $startdate);
             <div id="cont2" class="p-5 hide">
                 <h3 class="text-black text-center">Estimado(a) <span id="name"></span></h3>
 
-                <p class="text-black" align="text-center">
-                    Informamos que su crédito automotriz se encuentra en mora.
+                 <p class="text-black" align="text-center">
+                    Su crédito automotriz mantiene cuotas pendientes de pago.
                     <br>Para regularizar su situación tiene un ejecutivo asignado con quien evaluar las alternativas disponibles.<br> #Contacta a continuación  <br></p>
 
 
