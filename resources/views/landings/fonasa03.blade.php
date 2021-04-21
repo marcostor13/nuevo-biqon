7bc30f03304fba4b43f57c2b5b00189445ed6658
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2021-04-21 12:55:36 -0400

    Update fonasa03.blade.php

--- resources/views/landings/fonasa03.blade.php
+++ resources/views/landings/fonasa03.blade.php
@@ -82,7 +82,8 @@ $enddate=strtotime("+5 days", $startdate);
                     </div>
 
             <div id="cont2" class="p-5 ">
-                <h5 class="text-black text-center">Sr(A) <strong><span><?php echo $name; ?></span></strong></h5>
+                <!--<h5 class="text-black text-center">Sr(A) <strong><span><?php echo $name; ?></span></strong></h5>-->
+                <h5 class="text-black text-center">Sr(a) <span id="name"></span></h5>
 
                 <h5><p class="text-black"  align="justify">
                   Hemos ingresado una demanda por cobro de cotizaciones previsionales de nuestro mandante Fondo Nacional de Salud. <br>
