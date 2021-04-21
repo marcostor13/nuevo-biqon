2652a15eb4c85511d51e52c8b1060619bb9db741
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-04-21 10:42:42 -0400

    Update fonasa02.php
    rgkg

--- resources/views/landings/fonasa02.php
+++ resources/views/landings/fonasa02.php
@@ -6,7 +6,7 @@
 <?php
 $startdate=strtotime("Today");
 $enddate=strtotime("+5 days", $startdate);
-$name = $_GET['NOMBRE'];
+//$name = $_GET['NOMBRE'];
 
 ?>
 <style>
@@ -82,7 +82,7 @@ $name = $_GET['NOMBRE'];
                     </div>
 
             <div id="cont2" class="p-5 ">
-              <h5 class="text-black text-center">Estimado(a) <span id="NOMBRE"></span></h5>
+              <h5 class="text-black text-center">Estimado(a) <span id="name"></span></h5>
                
 
                 <h5><p class="text-black"  align="justify">
