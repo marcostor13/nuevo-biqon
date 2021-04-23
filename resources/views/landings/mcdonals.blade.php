796502f9ff052cd70677bc627ff0df651b086b8e
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2021-04-23 18:17:56 -0400

    Update mcdonals.blade.php

--- resources/views/landings/mcdonals.blade.php
+++ resources/views/landings/mcdonals.blade.php
@@ -75,6 +75,8 @@ $enddate=strtotime("+15 days", $startdate);
                   <a class="ml-2" onclick="eventosLanding('facebook');" href="https://www.facebook.com/McDonaldsChile/?__tn__=%2Cd%2CP-R&eid=ARBpyD1ayIw7ugjuuexEPEuWBi-WFByeU2kr2pIt6lkKeu0NqI_jMrZiihsZaUiRGo6QLsqFLAbBl0TB"><img width="70" src="https://www.freepngimg.com/thumb/facebook/10-2-facebook-png-image.png"></a>
                          <a class="ml-4" onclick="eventosLanding('Instagram');" href="https://www.instagram.com/mcdonalds_cl/?hl=es-la"><img width="60" src="http://pluspng.com/img-png/instagram-png-instagram-png-icon-1024.png"></a>
 
+                         hola mundo
+
    
                 </div>
           
