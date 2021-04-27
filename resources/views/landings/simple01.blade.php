1144a5c6277f4e35ee91d7f007e0da0915ba92cf
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-04-27 13:12:19 -0400

    Update simple01.blade.php
    cddfd

--- resources/views/landings/simple01.blade.php
+++ resources/views/landings/simple01.blade.php
@@ -63,13 +63,12 @@
 
                 <img class="img-fluid col-5 col-md-3 mt-4" src="https://imgur.com/PVz3F0f.jpg" alt="simple">
 
-                <br>
-
+            
                 <!--<a onclick="port1();" class="btn bg-danger text-white col-5 mt-2">Comprar</a>
 
                 <a onclick="port2();" class="btn bg-danger text-white col-5 mt-2">Comprar</a>-->
 
-                <img class="img-fluid col-5 col-md-3 mt-4" src="https://i.imgur.com/nIKsFm0.png" alt="simple">       
+                <img class="img-fluid col-5 col-md-3 mt-4" src="https://imgur.com/OpQ5lD6.jpg" alt="simple">       
 
                 <img class="img-fluid col-5 col-md-3 mt-4" src="https://i.imgur.com/IUGXSOF.png" alt="simple">
 
