8dabee71ca56519ecf5dfcb4c8fe14d982230697
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-04-27 13:50:41 -0400

    Update simple01.blade.php
    wdwd

--- resources/views/landings/simple01.blade.php
+++ resources/views/landings/simple01.blade.php
@@ -68,27 +68,55 @@
            <div  id="cont7" class="p-2 pl-3 pr-3 hide"> 
 
             <center>         
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="https://imgur.com/WRLHfWq.jpg" alt="Chania">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>LA is always so much fun!</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="https://imgur.com/WRLHfWq.jpg" alt="Chicago">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="https://imgur.com/WRLHfWq.jpg" alt="New York">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 
-                <img class="img-fluid col-5 col-md-3 mt-4" src="https://imgur.com/WRLHfWq.jpg" alt="
-                simple">
 
-                <img class="img-fluid col-5 col-md-3 mt-4" src="https://imgur.com/ZasX3xC.jpg" alt="simple">
-
-                <img class="img-fluid col-5 col-md-3 mt-4" src="https://imgur.com/PVz3F0f.jpg" alt="simple">
-
-                <img class="img-fluid col-5 col-md-3 mt-4" src="https://imgur.com/0hH25Ae.jpg" alt="simple">       
-
-                <img class="img-fluid col-5 col-md-3 mt-4" src="https://imgur.com/dfJDrfs.jpg" alt="simple">
-
-                <img class="img-fluid col-5 col-md-3 mt-4" src="https://imgur.com/dfJDrfs.jpg" alt="simple">
-
-                <br>
-
-                <!--<a onclick="port3();" class="btn bg-danger text-white col-5 mt-2">Comprar</a>
-
-                <<a onclick="port4();" class="btn bg-danger text-white col-5 mt-2">Comprar</a>-->
-                <br> 
               
            </center>
 
