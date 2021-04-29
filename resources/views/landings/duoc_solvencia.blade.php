550195b7aa87ec2c4124ddc00245fb9127de12f7
Jesus Cortez <cortezjesus01@gmail.com>
2021-04-29 17:50:36 -0400

    Update duoc_solvencia.blade.php
    gt

--- resources/views/landings/duoc_solvencia.blade.php
+++ resources/views/landings/duoc_solvencia.blade.php
@@ -80,7 +80,7 @@ $enddate=strtotime("+5 days", $startdate);
                 </p><br>
 
                  <div class="mt-2 text-center" style="background:#384259;"> 
-<p class="text-white text-center">Selecciona alguna de estas opciones para comunicarte con un ejecutivo quien podrá asesorale y resolver su consulta</p>
+<p class="text-white text-center">Selecciona alguna de estas opciones para comunicarte con un ejecutivo quien podrá asesorarle y resolver su consulta</p>
                      <div class="d-flex justify-content-around align-content-center text-center"> 
       
                      <span class="text-white  text-center">Mi Cuenta<br>
