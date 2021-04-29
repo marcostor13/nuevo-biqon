27492ca02bac7ebc114da5d3bca7ce336b441ff3
Jesus Cortez <cortezjesus01@gmail.com>
2021-04-29 14:58:44 -0400

    Create duoc_solvencia.blade.php
    re

---
+++ resources/views/landings/duoc_solvencia.blade.php
@@ -0,0 +1,310 @@
+@extends('layouts.landing')
+
+@section('title', 'UANDES')
+
+@section('content')
+<?php
+$startdate=strtotime("Today");
+$enddate=strtotime("+5 days", $startdate);
+?>
+
+<style type="text/css">
+    
+.card {
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  border: 0;
+  font-weight: 400;
+}
+.card[class*=border] {
+  border: 1px solid #9e9e9e;
+  box-shadow: none;
+}
+.card .card-body h1, .card .card-body h2, .card .card-body h3, .card .card-body h4, .card .card-body h5, .card .card-body h6 {
+  font-weight: 400;
+}
+.card .card-body .card-title a {
+  transition: 0.2s ease-in-out;
+}
+.card .card-body .card-title a:hover {
+  transition: 0.2s ease-in-out;
+}
+.card .card-body .card-text {
+  color: #747373;
+  font-size: 0.9rem;
+  font-weight: 400;
+}
+.card .md-form label {
+  font-weight: 300;
+}
+
+.oscurecer {
+  background-image: 
+    linear-gradient(
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.5)
+    )
+    }
+    
+</style>
+
+    <div id="UANDES" style=" /*background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;*/">
+
+        <div class="opaco"></div>
+
+        <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
+           <!-- <img style="max-width: 50%" class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="UANDES">-->
+<img style="max-width: 50%" class="img-fluid col-10 col-md-3 mt-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Logo_DuocUC.svg/711px-Logo_DuocUC.svg.png" alt="UANDES">
+             <div id="cont1" class="p-5 hide ">
+                <h3 class="text-black text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
+                <input id="rut" type="number" class="form-control mt-4 text-center text-black">
+                <button onclick="event1(); return false;" class="btn bg-danger text-white col-12 mt-4"> Validar </button>
+            </div>
+            <div id="cont5" class="p-2 pl-3 pr-3 hide">
+                        <div class="card">     
+                            <div class="card-body">
+                                 <h5 id="error"class="text-black text-center mt-5"></h5>
+                            </div>
+                        </div>
+                    </div>
+            <div id="cont2" class="p-5">
+                <h3 class="text-black text-center">Estimado(a) Ex alunmo: <span id="name"></span></h3>
+<hr>
+                <p class="text-black">
+                    Instituto profecinal Duoc UC le recuerda que usted manteine una deuda por regularizar
+                    <br><br>
+                    Le invitamos a contactarse a la brevedad para ser informado de los metodos de pago disponibles
+                </p><br>
+               <!-- <div class="date btn bg-danger text-white col-12 mt-4">
+                    <span>AGENDAR COMPROMISO DE PAGO</span>
+                    <input id="date1"  type="date" class="btn-date text-danger" style="border: none;" min="<?php // echo date('Y-m-d') ?>" max="<?php // echo date("Y-m-d", $enddate) ?>"/>
+                </div>
+                <button onclick='window.location.href="https://pagos.uss.cl/"; eventosLanding("Pagina de Pagos"); ' class="btn bg-danger text-white col-12 mt-4">PAGAR AHORA</button>
+                
+                <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-danger text-white col-12 mt-4">YA PAGUE</button>-->
+                
+                 <div class="mt-4" style="background:#384259;"> 
+<p class="text-white">Selecciona alguna de estas opciones para comunicarte con un ejecutivo quien podrá asesorale y resolver su consulta</p>
+                     <div class="d-flex justify-content-around align-content-center " > 
+                  <!--  <p class="text-white">Selecciona alguna de estas opciones para comunicarte con un ejecutivo quien podrá asesorale y resolver su consulta</p><br>-->
+                 
+                     <span class="text-white">Mi Cuenta<br>
+                     <a class="ml-2" onclick="eventosLanding('Cuenta');" href="https://solvencia.cl/pagadeudas/"><img width="70" src="https://image.freepik.com/foto-gratis/lupa_172429-232.jpg"></a></span>
+                         <span class="text-white">WhatsApp<br>
+                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56975890470&text=Hola"><img width="70" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/240px-WhatsApp.svg.png"></a></span>
+                       <span class="text-white">Teléfono<br>
+                    <a class="ml-4" onclick="eventosLanding('Llamar');" href="tel:+56975890461"><img width="60" src="https://image.freepik.com/vector-gratis/mano-humana-sosteniendo-telefono-movil_74855-6532.jpg"></a></span>
+                       <span class="text-white">Correo<br>
+                    <a class="ml-4" onclick="eventosLanding('Correo');" href="mailto:katerin.sena@solvencia.cl"><img width="60" src="https://image.freepik.com/psd-gratis/monitor-escritorio-3d-correo_23-2148938933.jpg"></a></span>
+                </div>
+                <br><br>
+            </div>
+        </div>
+           <div id="cont3" class="p-2 pl-5 pr-5 hide">
+                        <div class="card">     
+                            <div class="card-body">
+                                <h5 id="message"class="text-grey text-center mt-3 hide"></h5>
+                            </div>
+                        </div>
+                    </div>
+        </div>
+    </div>
+
+    <script>
+   //EVENT 1
+        
+        $(function(){
+            events({    
+                'name': 'Visita',
+                'landing_id': {!! $landing->id !!},
+                'json_datos': JSON.stringify(getAllUrlParameter())
+            });
+        }); 
+
+         function event1(){
+    
+            let dataSend = {
+                'fourRut': $('#rut').val(),
+                'phone': getUrlParameter('telefono'),
+                'landing_id': {!! $landing->id !!},
+            } 
+            
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.post( "/validateRut", dataSend,function() {
+                console.log(dataSend);
+            })
+            .done(function(e) {
+                console.log(e);
+                e = JSON.parse(e); 
+
+                if(e.code == 200){
+                    $('#cont1').addClass('hide');
+                    $('#name').text(e.data.nombre);
+                    $('#pay').text(e.data.monto);
+                    $('#cont2').removeClass('hide');
+                    $('#date1').on('change', function(){
+                        if($('#date1').val() != ''){
+                            sendMail();
+                        }
+                    });
+                }else{
+                    $('#cont1').addClass('hide');
+                    $('#cont5').removeClass('hide');
+                    $('#error').text("Validación incorrecta, recuerde visitar nuestra pagina web  o dirigirse a nuestra sucursal.");
+                }
+            })
+            .fail(function() {
+                console.log( "error" );
+            });
+
+        }
+        
+
+        let eventosLanding = function(name){
+            
+            let json_datos = getAllUrlParameter(); 
+
+            json_datos.nombre = $('#name').text();
+            json_datos.monto = $('#pay').text();
+           // json_datos.date = $('#date1').val();
+
+            events({    
+                'name': name,
+                'landing_id': {!! $landing->id !!},
+                'json_datos': JSON.stringify(json_datos)
+            });
+        }
+
+     
+
+        function sendMail(msg = false){
+
+            console.log('{!! $landing->name !!}'); 
+            
+            let data; 
+            if(msg !== false){
+                data = {
+                   'mensaje': msg,
+                    'Mombre': getUrlParameter('nombre'),
+                    'monto': getUrlParameter('monto'),
+                    'Phone': getUrlParameter('telefono'),
+                     'Rut': getUrlParameter('rut'),
+                     'Direccion': getUrlParameter('data1'),
+                     'Comuna': getUrlParameter('data2') 
+                   
+                    
+                }
+            }else{
+                let date = $('#date1').val();
+                data = {
+                     'Fecha': date,
+                    'Mombre': getUrlParameter('nombre'),
+                    'monto': getUrlParameter('monto'),
+                    'Phone': getUrlParameter('telefono'),
+                     'Rut': getUrlParameter('rut'),
+                     'Direccion': getUrlParameter('data1'),
+                     'Comuna': getUrlParameter('data2'),
+                    'landing': '{!! $landing->name !!}'
+                } 
+            }
+            
+            var correo = ["jesus.binteraction@gmail.com"];
+            let dataSend = {
+                'data': JSON.stringify(data),
+                 'email': correo
+                //'email':' {!! $landing->email !!}'
+                //'email': 'marcostor13@gmail.com'
+            }
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.post( "/sendMail", dataSend,function() {
+                console.log(dataSend);
+                if(msg !== false){
+                    $('#message').removeClass('hide');
+                     $('#cont2').addClass('hide');
+                    $('#cont3').removeClass('hide');
+                    $('#message').text('Muchas Gracias. Su Solicitud Fue enviada a nuestra área. Nos pondremos en contacto con usted en los próximos días');
+                }else{
+                    $('#message').removeClass('hide');
+                     $('#cont2').addClass('hide');
+                    $('#cont3').removeClass('hide');
+                    $('#message').text('Gracias, Su compromiso de pago fue agendado. Nos pondremos en contacto con usted en los próximos días');
+                    eventosLanding('Compromiso de Pago');
+                }
+            })
+            .done(function(e) {
+                console.log(e);
+            })
+            .fail(function() {
+                console.log( "error" );
+            });
+
+        }
+
+        let events = function(data){     
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            $.post( "/events", data, function() {
+                
+            })
+            .done(function(e) {
+                console.log(e);
+                console.log(e.msg);
+             
+            })
+            .fail(function(e) {
+                console.log(e);               
+            });
+
+        }
+
+        let getUrlParameter = function(sParam) {
+            var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+            for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0].toLowerCase() === sParam.toLowerCase()) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+            }
+        };
+
+        let getAllUrlParameter = function() {
+            var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+            let obj = {}; 
+
+            for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            obj[sParameterName[0].toLowerCase()] = sParameterName[1];            
+            }
+
+            return obj;  
+        };
+
+      
+        
+    
+    </script>
+    
+
+@endsection
