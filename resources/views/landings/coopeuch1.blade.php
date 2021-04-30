5899e168e803a7c11ef697a47da79e6df666b989
maicolbinteraction <59833637+maicolbinteraction@users.noreply.github.com>
2021-04-30 12:19:38 -0400

    Update coopeuch1.blade.php

--- resources/views/landings/coopeuch1.blade.php
+++ resources/views/landings/coopeuch1.blade.php
@@ -110,7 +110,7 @@ $enddate=strtotime("+5 days", $startdate);
 
                     <p>Para cumplir objetivos de mayor plazo y monto, por ejemplo, ahorrar para vacaciones, para algún arreglo de la casa o emergencias.</p>
 
-                    <div class="bg-danger"><img src=""> <font color="black"><a href="https://i.imgur.com/nh5Bh5C.png"></a><a href="https://i.imgur.com/lEbfbwh.png"></a><a href="https://i.imgur.com/u9D7nV9.png"></a> <a style="color: white;"  href="https://www.coopeuch.cl">coopeuch.cl</a>               <a style="color: white; text-align: left;"   href="tel+6002001200">600 200 1200</a></font></div>
+                    <div class="bg-danger"><img src=""> <font color="black"><a href="https://www.facebook.com" src="https://i.imgur.com/nh5Bh5C.png"></a><a href="https://i.imgur.com/lEbfbwh.png"></a><a href="https://i.imgur.com/u9D7nV9.png"></a> <a style="color: white;"  href="https://www.coopeuch.cl">coopeuch.cl</a>               <a style="color: white; text-align: left;"   href="tel+6002001200">600 200 1200</a></font></div>
 
                  
                    <!-- <button onclick="Mens1(); eventosLanding('Pago Transferencia');" class="btn bg-success text-white col-12 mt-2">PAGAR AHORA </button>
