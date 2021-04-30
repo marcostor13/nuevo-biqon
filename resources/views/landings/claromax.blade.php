4b337ceea24e24cb39ace3ee3941083df0f7e1e7
Jesus Cortez <cortezjesus01@gmail.com>
2021-04-30 13:53:10 -0400

    Update claromax.blade.php
    vrt

--- resources/views/landings/claromax.blade.php
+++ resources/views/landings/claromax.blade.php
@@ -32,7 +32,7 @@
 
     <td><img  style="max-width: 100%"  class="img-fluid col-12 mt-4" src="https://i.imgur.com/OF38uTK.png" alt="PlanXS"></td>
 
-    <td class="text-center">
+    <td class="text-center" colspan="3">
         <h4>PROMOCION EXCLUSIVA <strong  class="text-danger  text-center">MIGRACION</strong>    </h4>
       <div style="background:#d7dbdb;">  <h4>MIGRATE A CLARO Y DISFRUTA DE NUESTRO PLAN XS </h4>
         <h4 class="text-danger  text-center">CON EL DOBLE DE GIGAS POR UN AÑO    </h4></div>
