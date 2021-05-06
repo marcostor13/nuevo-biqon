22bf0ac5f1646e42d813885f7ca5e1f6ee8588d1
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-06 12:23:01 -0400

    Update amicar.blade.php
    vc

--- resources/views/landings/amicar.blade.php
+++ resources/views/landings/amicar.blade.php
@@ -112,7 +112,7 @@ $enddate=strtotime("+15 days", $startdate);
                    
                   
 
-                     <button onclick="sendMail('Solicito ser Contactado'); ;eventosLanding('Solicitud Informacion'); return false;" class="btn bg-danger text-white col-12 mt-4">Me Interesa ser Contactado</button>
+                     <button onclick="sendMail('Solicito ser Contactado'); ;eventosLanding('Solicitud Informacion'); return false;" class="btn bg-danger text-white col-12 mt-4">CONTACTAME</button>
 
 
                     <!--   </div>
