073c16cb7093cd4ca1a9619c99eaa0969ac15a38
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-06 13:12:23 -0400

    Update caplider1.blade.php
    vc5

--- resources/views/landings/caplider1.blade.php
+++ resources/views/landings/caplider1.blade.php
@@ -132,7 +132,7 @@ $enddate=strtotime("+5 days", $startdate);
 
                   <div class="text-center">
                     <hr>
-                    <button class="btn btn-warning text-white col-12 mt-4" onclick="sendMail('El cliente solicito Informacion');eventosLanding('Solicitud Informacion'); return false;">Inscribir</button>
+                    <button class="btn btn-danger text-white col-12 mt-4" onclick="sendMail('El cliente solicito Informacion');eventosLanding('Solicitud Informacion'); return false;">Inscribir</button>
                     
                   </div>
                 </div>
