05b368e57059a76691466e02b4b1a88be45c686c
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-07 19:00:10 -0400

    Update uandes.blade.php
    vc

--- resources/views/landings/uandes.blade.php
+++ resources/views/landings/uandes.blade.php
@@ -1,56 +1,117 @@
 @extends('layouts.landing')
 
-@section('title', 'UANDES')
+@section('title', 'UDA')
 
 @section('content')
+<?php
+$startdate=strtotime("Today");
+$enddate=strtotime("+5 days", $startdate);
+$name=$_GET['NOMBRE'];
+?>
 
-<style>
-.opaco{
-    display: none !important; 
+<style type="text/css">
+    
+.card {
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  border: 0;
+  font-weight: 400;
 }
+.card[class*=border] {
+  border: 1px solid #9e9e9e;
+  box-shadow: none;
+}
+.card .card-body h1, .card .card-body h2, .card .card-body h3, .card .card-body h4, .card .card-body h5, .card .card-body h6 {
+  font-weight: 400;
+}
+.card .card-body .card-title a {
+  transition: 0.2s ease-in-out;
+}
+.card .card-body .card-title a:hover {
+  transition: 0.2s ease-in-out;
+}
+.card .card-body .card-text {
+  color: #747373;
+  font-size: 0.9rem;
+  font-weight: 400;
+}
+.card .md-form label {
+  font-weight: 300;
+}
+
+.oscurecer {
+  background-image: 
+    linear-gradient(
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.5)
+    )
+    }
+    
 </style>
 
-   <div id="UANDES" style="background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;">
+    <div id="UDA" style=" /*background: url('{{$landing->background}}'); background-repeat: no-repeat; background-size: cover;*/">
 
         <div class="opaco"></div>
 
-       <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
-            <img style="max-width: 50%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="UANDES">
+        <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
+           <img style="max-width: 50%" class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="UDA">-
 
-            <div id="cont1">
-                <div class="videoWrapper">        
-                    <iframe width="400" height="320" src="{{asset('/files/uandes_becas.mp4')}}" frameborder="0" autoplay allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-
-                <div class="card">     
-                <div class="card-body">
-                <h4 class="text-black text-center"> <strong> ¿Desea que le contactemos? </strong> </h4> </div> </div>
-
-                <button onclick="sendMail('El cliente indica que desea ser contactado'); eventosLanding('contacto'); return false;" class="btn bg-warning text-black col-12 mt-3"> <h3> <strong>SI</strong> </h3> </button>
+<!--<img style="max-width: 50%" class="img-fluid col-10 col-md-3 mt-4" src="https://i.imgur.com/LRZ1x2g.png" alt="UANDES">-->
+           
+             <div id="cont1" class="p-5  hide">
+                <h3 class="text-black text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h3>
+                <input id="rut" type="number" class="form-control mt-4 text-center text-black">
+                <button onclick="event1(); return false;" class="btn bg-warning text-black col-12 mt-4"> Validar </button>
             </div>
-            
-            <div class="d-flex justify-content-around align-content-center mt-4"> 
-                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56985296912&text=Hola,%20tengo%20una%20consulta"><img width="70" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
-                   
-                    <a class="ml-4" onclick="eventosLanding('Llamar');" href="tel:+562226182049"><img width="60" src="https://i.pinimg.com/236x/93/0e/6f/930e6fe9fe45beab222542ae42b05c4f.jpg"></a>
-                   
-                    <a class="ml-4" onclick="eventosLanding('Correo');" href="mailto:mjsandovalg@uandes.cl?subject=Landing%20Video"><img width="60" src="https://es.seaicons.com/wp-content/uploads/2015/10/Email-icon.png"></a>
-                </div>
-        </div>
-
-        <div id="cont3" class="p-2 pl-3 pr-3 hide">
+            <div id="cont5" class="p-2 pl-3 pr-3 hide">
                         <div class="card">     
                             <div class="card-body">
-                                <h5 id="message"class="text-black text-center mt-3 hide"></h5>
+                                 <h5 id="error"class="text-black text-center mt-4"></h5>
                             </div>
                         </div>
                     </div>
-            <!--<div class=" tagline w3-center w3-animate-top black-text">Binteraction.com</div>-->
-        
-    <!--</div>-->
+            <div id="cont2" class="p-1">
+                <div class="card">     
+                            <div class="card-body">
+                <h3 class="text-black text-center">Estimado(a) <span id="name"> <?php echo $name; ?></span></h3>
+<hr>
+                <p class="text-black text-center">
+                  Universidad Los Andes, le recuerda que usted mantiene una deuda por regularizar.<br>
+Le invitamos a contactarse a la brevedad para ser informado de los métodos de pago disponibles. <br> 
+<br>
+                </p><br>
+     </div>
+            </div>
+                 <div class="mt-2 text-center" style="background:#f7f7f7;"> 
+<p class="text-black text-center"><br><strong>Selecciona alguna de estas opciones para comunicarte con un ejecutivo quien podrá asesorarle y resolver su consulta</strong></p>
+                     <div class="d-flex justify-content-around align-content-center text-center"> 
+      
+                    <!-- <span class="text-white  text-center">Mi Cuenta<br>
+                     <a class="ml-2" onclick="eventosLanding('Cuenta');" href="https://solvencia.cl/pagadeudas/"><img width="60" src="https://image.freepik.com/foto-gratis/lupa_172429-232.jpg"></a></span>-->
+                         <span class="text-black  text-center">WhatsApp<br>
+                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56983001247&text=Hola,%20tengo%20una%20consulta%20por%20Universidad%20de%20los%20Andes"><img width="50" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/240px-WhatsApp.svg.png"></a></span>
+                       <span class="text-black  text-center">Teléfono<br>
+                    <a class="ml-2" onclick="eventosLanding('Llamar');" href="tel:6004416010"><img width="50" src="https://image.freepik.com/vector-gratis/mano-humana-sosteniendo-telefono-movil_74855-6532.jpg"></a></span>
+                       <span class="text-black  text-center">Correo<br>
+                    <a class="ml-2" onclick="eventosLanding('Correo');" href="mailto:contacto@solvencia.cl"><img width="50" src="https://image.freepik.com/psd-gratis/monitor-escritorio-3d-correo_23-2148938933.jpg"></a></span>
+                    <span class="text-black  text-center">Pago en Linea<br>
+                     <a class="ml-2" onclick="eventosLanding('Pago en Linea');" href="https://webpay.uandes.cl/"><img width="50" src="https://img.freepik.com/free-vector/plain-credit-card-concept-illustration_114360-199.jpg?size=338&ext=jpg"></a></span>
+                </div>
+                <br><br>
 
-   
-     <script>
+                  </div>
+       
+        </div>
+           <div id="cont3" class="p-2 pl-5 pr-5 hide">
+                        <div class="card">     
+                            <div class="card-body">
+                                <h5 id="message"class="text-grey text-center mt-3 hide"></h5>
+                            </div>
+                        </div>
+                    </div>
+        </div>
+    </div>
+
+    <script>
    //EVENT 1
         
         $(function(){
@@ -92,7 +153,9 @@
                         }
                     });
                 }else{
-                    $('#error').text(e.msg);
+                    $('#cont1').addClass('hide');
+                    $('#cont5').removeClass('hide');
+                    $('#error').text("Validación incorrecta, recuerde visitar nuestra pagina web  o dirigirse a nuestra sucursal.");
                 }
             })
             .fail(function() {
@@ -108,6 +171,7 @@
 
             json_datos.nombre = $('#name').text();
             json_datos.monto = $('#pay').text();
+           // json_datos.date = $('#date1').val();
 
             events({    
                 'name': name,
@@ -125,35 +189,35 @@
             let data; 
             if(msg !== false){
                 data = {
-                    'mensaje': msg,
+                   'mensaje': msg,
+                    'Mombre': getUrlParameter('nombre'),
+                    'monto': getUrlParameter('monto'),
+                    'Phone': getUrlParameter('telefono'),
+                     'Rut': getUrlParameter('rut'),
+                     'Direccion': getUrlParameter('data1'),
+                     'Comuna': getUrlParameter('data2') 
+                   
+                    
+                }
+            }else{
+                let date = $('#date1').val();
+                data = {
+                     'Fecha': date,
                     'Mombre': getUrlParameter('nombre'),
                     'monto': getUrlParameter('monto'),
                     'Phone': getUrlParameter('telefono'),
                      'Rut': getUrlParameter('rut'),
                      'Direccion': getUrlParameter('data1'),
                      'Comuna': getUrlParameter('data2'),
-                     'landing': '{!! $landing->name !!}'               
-                    
-                }
-            }else{
-                let date = $('#date1').val();
-                data = {
-                    'Fecha': date,
-                    'Mombre': getUrlParameter('nombre'),
-                    'Rut': getUrlParameter('rut'),
-                    'monto': getUrlParameter('monto'),
-                    'Phone': getUrlParameter('telefono'),
-                    'Direccion': getUrlParameter('data1'),
-                    'Comuna': getUrlParameter('data2'),
                     'landing': '{!! $landing->name !!}'
                 } 
             }
-
-            var correo = ["gbahamondes@gmail.com","jesus.binteraction@gmail.com"];
+            
+            var correo = ["jesus.binteraction@gmail.com"];
             let dataSend = {
                 'data': JSON.stringify(data),
-                'email': correo
-                //'email': '{!! $landing->email !!}' mjsandovalg@uandes.cl
+                 'email': correo
+                //'email':' {!! $landing->email !!}'
                 //'email': 'marcostor13@gmail.com'
             }
             $.ajaxSetup({
@@ -165,14 +229,15 @@
                 console.log(dataSend);
                 if(msg !== false){
                     $('#message').removeClass('hide');
-                     $('#cont1').addClass('hide');
+                     $('#cont2').addClass('hide');
                     $('#cont3').removeClass('hide');
                     $('#message').text('Muchas Gracias. Su Solicitud Fue enviada a nuestra área. Nos pondremos en contacto con usted en los próximos días');
                 }else{
                     $('#message').removeClass('hide');
                      $('#cont2').addClass('hide');
                     $('#cont3').removeClass('hide');
-                    $('#message').text('Gracias, Su compromiso de pago fue agendado');
+                    $('#message').text('Gracias, Su compromiso de pago fue agendado. Nos pondremos en contacto con usted en los próximos días');
+                    eventosLanding('Compromiso de Pago');
                 }
             })
             .done(function(e) {
