e709b09699866ecac0dc36082288b62f35df1339
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-07 15:08:03 -0400

    Update cpech.blade.php
    vcr

--- resources/views/landings/cpech.blade.php
+++ resources/views/landings/cpech.blade.php
@@ -77,7 +77,8 @@ $enddate=strtotime("+5 days", $startdate);
                     Instituto profesional CPECH, le recuerda que usted mantiene una deuda por regularizar.<br>
 Le invitamos a contactarse a la brevedad para ser informado de los métodos de pago disponibles. 
                 </p><br>
-
+ </div>
+            </div>
                  <div class="mt-2 text-center" style="background:#2c50ff;"> 
 <p class="text-white text-center"><br><strong>Seleccione alguna de estas opciones para comunicarse con un ejecutivo quien podrá asesorarle y resolver su consulta</strong></p>
                      <div class="d-flex justify-content-around align-content-center text-center"> 
@@ -96,8 +97,7 @@ Le invitamos a contactarse a la brevedad para ser informado de los métodos de p
                 <br><br>
 
                   </div>
-            </div>
-            </div>
+           
         </div>
            <div id="cont3" class="p-2 pl-5 pr-5 hide">
                         <div class="card">     
