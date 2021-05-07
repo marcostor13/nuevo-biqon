5450f9e2dfcc47febd790702f0667f4deae2cd14
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-07 13:35:34 -0400

    Update abcdin.blade.php
    cs

--- resources/views/landings/abcdin.blade.php
+++ resources/views/landings/abcdin.blade.php
@@ -81,7 +81,7 @@ Le invitamos a contactarse a la brevedad para ser informado de los metodos de pa
 </div>
             </div>
                  <div class="mt-2 text-center" style="background:#384259;"> 
-<p class="text-white text-center">Seleccione alguna de estas opciones para comunicarse con un Ejecutivo quien podrá asesorarle y resolver sus consultas.</p>
+<p class="text-white text-center"><br>Seleccione alguna de estas opciones para comunicarse con un Ejecutivo quien podrá asesorarle y resolver sus consultas.<br></p>
                      <div class="d-flex justify-content-around align-content-center text-center"> 
       
                     <!-- <span class="text-white  text-center">Mi Cuenta<br>
