fc5a6b92db1d79d85e2b3d698ca2362b409ebe22
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-07 15:09:44 -0400

    Update udandes.blade.php
    vc

--- resources/views/landings/udandes.blade.php
+++ resources/views/landings/udandes.blade.php
@@ -80,19 +80,19 @@ Le invitamos a contactarse a la brevedad para ser informado de los métodos de p
                 </p><br>
      </div>
             </div>
-                 <div class="mt-2 text-center" style="background:#e00034;"> 
-<p class="text-white text-center"><br><strong>Selecciona alguna de estas opciones para comunicarte con un ejecutivo quien podrá asesorarle y resolver su consulta</strong></p>
+                 <div class="mt-2 text-center" style="background:#f7f7f7;"> 
+<p class="text-black text-center"><br><strong>Selecciona alguna de estas opciones para comunicarte con un ejecutivo quien podrá asesorarle y resolver su consulta</strong></p>
                      <div class="d-flex justify-content-around align-content-center text-center"> 
       
                     <!-- <span class="text-white  text-center">Mi Cuenta<br>
                      <a class="ml-2" onclick="eventosLanding('Cuenta');" href="https://solvencia.cl/pagadeudas/"><img width="60" src="https://image.freepik.com/foto-gratis/lupa_172429-232.jpg"></a></span>-->
-                         <span class="text-white  text-center">WhatsApp<br>
+                         <span class="text-black  text-center">WhatsApp<br>
                     <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56983001247&text=Hola"><img width="50" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/240px-WhatsApp.svg.png"></a></span>
-                       <span class="text-white  text-center">Teléfono<br>
+                       <span class="text-black  text-center">Teléfono<br>
                     <a class="ml-2" onclick="eventosLanding('Llamar');" href="tel:6004416010"><img width="50" src="https://image.freepik.com/vector-gratis/mano-humana-sosteniendo-telefono-movil_74855-6532.jpg"></a></span>
-                       <span class="text-white  text-center">Correo<br>
+                       <span class="text-black  text-center">Correo<br>
                     <a class="ml-2" onclick="eventosLanding('Correo');" href="mailto:contacto@solvencia.cl"><img width="50" src="https://image.freepik.com/psd-gratis/monitor-escritorio-3d-correo_23-2148938933.jpg"></a></span>
-                    <span class="text-white  text-center">Pago en Linea<br>
+                    <span class="text-black  text-center">Pago en Linea<br>
                      <a class="ml-2" onclick="eventosLanding('Pago en Linea');" href="https://webpay.uandes.cl/"><img width="50" src="https://img.freepik.com/free-vector/plain-credit-card-concept-illustration_114360-199.jpg?size=338&ext=jpg"></a></span>
                 </div>
                 <br><br>
