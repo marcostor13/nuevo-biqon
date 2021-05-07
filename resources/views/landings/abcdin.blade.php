faa87ad7e3cfd5bed76ecb4d259ad0ef7a8746b1
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-07 13:38:53 -0400

    Update abcdin.blade.php
    vcabc

--- resources/views/landings/abcdin.blade.php
+++ resources/views/landings/abcdin.blade.php
@@ -80,7 +80,7 @@ Le invitamos a contactarse a la brevedad para ser informado de los métodos de p
                 </p><br>
 </div>
             </div>
-                 <div class="mt-2 text-center" style="background:#384259;"> 
+                 <div class="mt-2 text-center" style="background:#db061d;"> 
 <p class="text-white text-center"><br>Seleccione alguna de estas opciones para comunicarse con un Ejecutivo quien podrá asesorarle y resolver sus consultas.<br></p>
                      <div class="d-flex justify-content-around align-content-center text-center"> 
       
