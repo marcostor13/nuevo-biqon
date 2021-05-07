162af66e5a91f4ab3cc6684dac1e93c1097be2f6
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-07 19:40:46 -0400

    Update cpech.blade.php
    4

--- resources/views/landings/cpech.blade.php
+++ resources/views/landings/cpech.blade.php
@@ -86,13 +86,13 @@ Le invitamos a contactarse a la brevedad para ser informado de los métodos de p
                     <!-- <span class="text-white  text-center">Mi Cuenta<br>
                      <a class="ml-2" onclick="eventosLanding('Cuenta');" href="https://solvencia.cl/pagadeudas/"><img width="60" src="https://image.freepik.com/foto-gratis/lupa_172429-232.jpg"></a></span>-->
                          <span class="text-white  text-center">WhatsApp<br>
-                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56983001247&text=Hola,%20tengo%20una%20consulta%20por%20deuda%20CPECH"><img width="50" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/240px-WhatsApp.svg.png"></a></span>
+                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56983001247&text=Hola,%20tengo%20una%20consulta%20por%20deuda%20CPECH"><img width="50" src="https://i.imgur.com/86IRYB5.png"></a></span>
                        <span class="text-white  text-center">Teléfono<br>
-                    <a class="ml-2" onclick="eventosLanding('Llamar');" href="tel:6004416010"><img width="50" src="https://image.freepik.com/vector-gratis/mano-humana-sosteniendo-telefono-movil_74855-6532.jpg"></a></span>
+                    <a class="ml-2" onclick="eventosLanding('Llamar');" href="tel:6004416010"><img width="50" src="https://i.imgur.com/EQmqiCk.png"></a></span>
                        <span class="text-white  text-center">Correo<br>
-                    <a class="ml-2" onclick="eventosLanding('Correo');" href="mailto:contacto@solvencia.cl"><img width="50" src="https://image.freepik.com/psd-gratis/monitor-escritorio-3d-correo_23-2148938933.jpg"></a></span>
+                    <a class="ml-2" onclick="eventosLanding('Correo');" href="mailto:contacto@solvencia.cl"><img width="50" src="https://i.imgur.com/Jg98aT5.png"></a></span>
                     <span class="text-white  text-center">Pago en Línea<br>
-                     <a class="ml-2" onclick="eventosLanding('Pago en Línea');" href="https://portal.cepech.cl/gnr/gnrPagos2PR_TA.ASP?ee=1"><img width="50" src="https://img.freepik.com/free-vector/plain-credit-card-concept-illustration_114360-199.jpg?size=338&ext=jpg"></a></span>
+                     <a class="ml-2" onclick="eventosLanding('Pago en Línea');" href="https://portal.cepech.cl/gnr/gnrPagos2PR_TA.ASP?ee=1"><img width="50" src="https://i.imgur.com/2YPpKWm.png"></a></span>
                 </div>
                 <br><br>
 
