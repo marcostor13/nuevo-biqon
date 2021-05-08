1c09b4733432196be697beb84c87401c0f2a99b1
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-07 21:16:42 -0400

    Update udandes.blade.php
    qr

--- resources/views/landings/udandes.blade.php
+++ resources/views/landings/udandes.blade.php
@@ -86,7 +86,7 @@ Le invitamos a contactarse a la brevedad para ser informado de los métodos de p
                      <div class="d-flex justify-content-around align-content-center text-center"> 
       
                     <!-- <span class="text-white  text-center">Mi Cuenta<br>
-                     <a class="ml-2" onclick="eventosLanding('Cuenta');" href="https://solvencia.cl/pagadeudas/"><img width="60" src="https://image.freepik.com/foto-gratis/lupa_172429-232.jpg"></a></span>-->
+                     <a class="ml-2" onclick="eventosLanding('Cuenta');" href="https://solvencia.cl/pagadeudas/"><img width="60" src="https://image.freepik.com/foto-gratis/lupa_172429-232.jpg"></a></span>
                          <span class="text-black bg-black text-center">WhatsApp<br>
                     <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56983001247&text=Hola,%20tengo%20una%20consulta%20por%20Universidad%20de%20los%20Andes"><img width="50" src="https://i.imgur.com/86IRYB5.png"></a></span>
                        <span class="text-black  text-center">Teléfono<br>
@@ -95,6 +95,16 @@ Le invitamos a contactarse a la brevedad para ser informado de los métodos de p
                     <a class="ml-2" onclick="eventosLanding('Correo');" href="mailto:contacto@solvencia.cl"><img width="50" src="https://i.imgur.com/Jg98aT5.png"></a></span>
                     <span class="text-black  text-center">Pago en Linea<br>
                      <a class="ml-2" onclick="eventosLanding('Pago en Linea');" href="https://webpay.uandes.cl/"><img width="50" src="https://i.imgur.com/2YPpKWm.png"></a></span>
+                </div>-->
+
+                <span class="text-white text-center">
+                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56983001247&text=Hola,%20tengo%20una%20consulta%20por%20deuda%20por%20Universidad%20de%20los%20Andes"><img width="70" src="https://i.imgur.com/QcIC2T3.png"></a></span>
+                        <span class="text-white text-center">
+                    <a class="ml-2" onclick="eventosLanding('Llamar');" href="tel:6004416010"><img width="70" src="https://i.imgur.com/jHwdyby.png"></a></span>
+                        <span class="text-white text-center">
+                    <a class="ml-2" onclick="eventosLanding('Correo');" href="mailto:contacto@solvencia.cl"><img width="70" src="https://i.imgur.com/2YlOrMQ.png"></a></span>
+                     <span class="text-white text-center">
+                <a class="ml-2" onclick="eventosLanding('Pago en Línea');" href="https://webpay.uandes.cl/"> <img width="70" src="https://i.imgur.com/425rNaE.png"></a></span>
                 </div>
                 <br><br>
 
