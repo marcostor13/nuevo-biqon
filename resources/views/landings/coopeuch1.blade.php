b08ff5633313c1edfd655ba0087cabbf65c74287
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-07 20:20:45 -0400

    Update coopeuch1.blade.php
    cv

--- resources/views/landings/coopeuch1.blade.php
+++ resources/views/landings/coopeuch1.blade.php
@@ -139,8 +139,8 @@ $enddate=strtotime("+5 days", $startdate);
                      </tr>
                 </table>
 
-                <div class="mt-2 text-center bg-danger" > 
-<p class="text-white text-center">Selecciona alguna de estas opciones para comunicarte con un ejecutivo quien podrá asesorarle y resolver su consulta</p> 
+                <div class=" text-center bg-danger" > 
+<p class="text-white text-center"><br>Selecciona alguna de estas opciones para comunicarte con un ejecutivo quien podrá asesorarle y resolver su consulta<br></p> 
                      <div class="d-flex justify-content-around align-content-center text-center"> 
 
                         <span class="text-white text-center"> 
