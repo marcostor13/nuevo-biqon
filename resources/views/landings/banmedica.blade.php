4e6cd5a9ef90fe55c78f12b315145a856060212b
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-08 10:32:34 -0400

    Update banmedica.blade.php
    d

--- resources/views/landings/banmedica.blade.php
+++ resources/views/landings/banmedica.blade.php
@@ -97,9 +97,9 @@ Le invitamos a contactarse a la brevedad para ser informado de los métodos de p
                 </div>-->
 
                  <span class="text-white text-center">
-                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56983001247&text=Hola,%20tengo%20una%20consulta%20por%20deuda%20Banmedica"><img width="70" src="https://i.imgur.com/ot39iWI.png"></a></span>
+                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56975890470&text=Hola,%20tengo%20una%20consulta%20por%20deuda%20Banmedica"><img width="70" src="https://i.imgur.com/ot39iWI.png"></a></span>
                         <span class="text-white text-center">
-                    <a class="ml-2" onclick="eventosLanding('Llamar');" href="tel:6004416010"><img width="70" src="https://i.imgur.com/sN7X4XZ.png"></a></span>
+                    <a class="ml-2" onclick="eventosLanding('Llamar');" href="tel:56975890461"><img width="70" src="https://i.imgur.com/sN7X4XZ.png"></a></span>
                         <span class="text-white text-center">
                     <a class="ml-2" onclick="eventosLanding('Correo');" href="mailto:contacto@solvencia.cl"><img width="70" src="https://i.imgur.com/yedwmQG.png"></a></span>
                      <span class="text-white text-center">
