a4950569ead4948ba9c67690b869dfd68385d10c
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-07 20:34:11 -0400

    Update abcdin.blade.php
    cv12

--- resources/views/landings/abcdin.blade.php
+++ resources/views/landings/abcdin.blade.php
@@ -81,20 +81,20 @@ Le invitamos a contactarse a la brevedad para ser informado de los métodos de p
                 </p>
 </div>
             </div>
-                 <div class="mt-2 text-center" style="background:#de003c;"> 
+                <div class=" text-center bg-danger col-12" > 
 <p class="text-white text-center"><br><strong>Seleccione alguna de estas opciones para comunicarse con un Ejecutivo quien podrá asesorarle y resolver sus consultas.</strong><br></p>
                      <div class="d-flex justify-content-around align-content-center text-center"> 
       
                     <!-- <span class="text-white  text-center">Mi Cuenta<br>
                      <a class="ml-2" onclick="eventosLanding('Cuenta');" href="https://solvencia.cl/pagadeudas/"><img width="60" src="https://image.freepik.com/foto-gratis/lupa_172429-232.jpg"></a></span>-->
                          <span class="text-white text-center">Whatsapp<br>
-                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56983001247&text=Hola,%20tengo%20una%20consulta%20por%20deuda%20ABCDIN"><img width="50" src="https://i.imgur.com/86IRYB5.png"></a></span>
+                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56983001247&text=Hola,%20tengo%20una%20consulta%20por%20deuda%20ABCDIN"><img width="50" src="https://i.imgur.com/ot39iWI.png"></a></span>
                         <span class="text-white text-center">Teléfono<br>
-                    <a class="ml-2" onclick="eventosLanding('Llamar');" href="tel:6004416010"><img width="50" src="https://i.imgur.com/EQmqiCk.png"></a></span>
+                    <a class="ml-2" onclick="eventosLanding('Llamar');" href="tel:6004416010"><img width="50" src="https://i.imgur.com/sN7X4XZ.png"></a></span>
                         <span class="text-white text-center">Correo<br>
-                    <a class="ml-2" onclick="eventosLanding('Correo');" href="mailto:contacto@solvencia.cl"><img width="50" src="https://i.imgur.com/Jg98aT5.png"></a></span>
+                    <a class="ml-2" onclick="eventosLanding('Correo');" href="mailto:contacto@solvencia.cl"><img width="50" src="https://i.imgur.com/yedwmQG.png"></a></span>
                      <span class="text-white text-center">Pago en Línea<br>
-                <a class="ml-2" onclick="eventosLanding('Pago en Línea');" href="https://www.abcserviciosfinancieros.cl/paga-tu-cuenta"> <img width="50" src="https://i.imgur.com/2YPpKWm.png"></a></span>
+                <a class="ml-2" onclick="eventosLanding('Pago en Línea');" href="https://www.abcserviciosfinancieros.cl/paga-tu-cuenta"> <img width="70" src="https://i.imgur.com/TI8UBph.png"></a></span>
                 </div>
                 <br><br>
 
