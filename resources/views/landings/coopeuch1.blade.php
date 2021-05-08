7cbdff538eeec0b1f2d07af7a275f05e3f0fcca4
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-08 09:49:13 -0400

    Update coopeuch1.blade.php
    vc

--- resources/views/landings/coopeuch1.blade.php
+++ resources/views/landings/coopeuch1.blade.php
@@ -8,6 +8,7 @@
 <?php
 $startdate=strtotime("Today");
 $enddate=strtotime("+5 days", $startdate);
+$name=$_GET['NOMBRE'];
 ?>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
