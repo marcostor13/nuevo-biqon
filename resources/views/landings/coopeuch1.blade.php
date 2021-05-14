3a47cb3104a8f1ce1cd972e58c6417ad709a67a5
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-14 12:17:03 -0400

    Update coopeuch1.blade.php
    2

--- resources/views/landings/coopeuch1.blade.php
+++ resources/views/landings/coopeuch1.blade.php
@@ -156,7 +156,7 @@ Le invitamos a contactarse a la brevedad para ser informado de los métodos de p
                      <div class="d-flex justify-content-around align-content-center text-center"> 
 
                         <span class="text-white text-center"> 
-                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56983001247&text=Hola,%20tengo%20una%20consulta%20COOPEUCH"><img width="70" src="https://i.imgur.com/ot39iWI.png"></a></span>
+                    <a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56975890453&text=Hola,%20tengo%20una%20consulta%20COOPEUCH"><img width="70" src="https://i.imgur.com/ot39iWI.png"></a></span>
                        <span class="text-danger bg-black  text-center">
                     <a class="ml-2" onclick="eventosLanding('Llamar');" href="tel:6004416010"><img width="70" src="https://i.imgur.com/sN7X4XZ.png"></a></span>
                        <span class="text-danger bg-black  text-center">
