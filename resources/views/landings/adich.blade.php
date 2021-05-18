607ee86425b4f5a9852e33448b5bccc73c4e9883
Jesus Cortez <cortezjesus01@gmail.com>
2021-05-18 12:33:38 -0400

    Update adich.blade.php
    cv

--- resources/views/landings/adich.blade.php
+++ resources/views/landings/adich.blade.php
@@ -57,7 +57,7 @@ $enddate=strtotime("+5 days", $startdate);
         <div class="opaco"></div>
 
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
-             <img style="max-width:320px; max-height:180px" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="ZONASUR">
+             <img style="max-width:350px; max-height:220px" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="ZONASUR">
 
              <div id="cont1" class="p-5 ">
                 <h4 class="text-black text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h4>
@@ -75,7 +75,7 @@ $enddate=strtotime("+5 days", $startdate);
                 <h3 class="text-black text-center">Estimado(a) Sr(a) <span id="name"></span></h3>
 
                <p class="text-black" align="text-center">
-                    Le recordamos que usted tiene una solicitud de hora médica en Nuestra Asociacion,  con el Dr <strong> BINTERACTION CHILE </strong> para el día <?php echo date("d-m-Y", $enddate) ?> a las 03:00 PM
+                    Le recordamos que usted tiene una solicitud de hora médica en Nuestra Asociacion,  con el Dr <strong> BINTERACTION CHILE </strong> para el día <?php echo date("d-m-Y", $enddate) ?> a las 15:00 Hrs
                     <br> Favor confirmar su asistencia<br></p>              
 
                <button onclick="sendMail('ASISTIRE A MI HORA'); ;eventosLanding('SI'); return false;" class="btn bg-primary text-white col-12 mt-4">SI</button>
