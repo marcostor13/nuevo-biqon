41de4966fed555c9f028d147664742035d498eea
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-06-24 15:49:54 -0400

    Update mcredito_casa.blade.php
    SADSD

--- resources/views/landings/mcredito_casa.blade.php
+++ resources/views/landings/mcredito_casa.blade.php
@@ -43,7 +43,7 @@ $enddate=strtotime("+6 days", $startdate);
 
                 <p class="text-black">
 
-                   #QuedateEnCasa y prefiere nuestros canales digitales! Puedes consultar el estado de tu Crédito Automotriz, solicitar información, conocer nuestros beneficios y contactar con nuestros ejecutivos. Todo en <a href="www.Mundocredito.cl"> www.Mundocredito.cl </a>
+                   #QuedateEnCasa y prefiere nuestros canales digitales! Puedes consultar el estado de tu Crédito Automotriz, solicitar información, conocer nuestros beneficios y contactar con nuestros ejecutivos. Todo en <a href="https://www.Mundocredito.cl"> www.Mundocredito.cl </a>
 
                 <br>
                 <br>
@@ -58,11 +58,11 @@ $enddate=strtotime("+6 days", $startdate);
 
 
 
-         <a onclick="eventosLanding('Contactar con un Ejecutivo');" href="tel:+56967599345" class="btn bg-primary text-white col-12 mt-4"><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png">Contactar Ejecutivo</a>
+         <a onclick="eventosLanding('Contactar con un Ejecutivo');" href="tel:+56978274059" class="btn bg-primary text-white col-12 mt-4"><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png">Contactar Ejecutivo</a>
 
 
                
-                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56967599345&text=Hola,%20tengo%20una%20consulta" class="btn bg-primary text-white col-12 mt-4"><img width="40" src="https://i.pinimg.com/originals/79/dc/31/79dc31280371b8ffbe56ec656418e122.png">Enviar SMS por Whatsapp</a>
+                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56978274059&text=Hola,%20tengo%20una%20consulta" class="btn bg-primary text-white col-12 mt-4"><img width="40" src="https://i.pinimg.com/originals/79/dc/31/79dc31280371b8ffbe56ec656418e122.png">Enviar SMS por Whatsapp</a>
 
 
                 <button onclick='window.location.href="http://servipag.mundocredito.cl/PagoCuotas.php"; eventosLanding("Pagina de Pagos");' class="btn bg-primary text-white col-12 mt-4">PAGAR AHORA</button>
