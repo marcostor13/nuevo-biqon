a9bb7eff3aa39987f678b44dbd889953663e26de
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-06-24 15:50:09 -0400

    Update falabella_casa.blade.php
    XSASA

--- resources/views/landings/falabella_casa.blade.php
+++ resources/views/landings/falabella_casa.blade.php
@@ -43,7 +43,7 @@ $enddate=strtotime("+6 days", $startdate);
 
                 <p class="text-black">
 
-                   #QuedateEnCasa y prefiere nuestros canales digitales! Puedes consultar el estado de tu Tarjeta, solicitar información, conocer nuestros beneficios y contactar con nuestros ejecutivos.Todo en <a href="www.bancofalabella.cl"> www.bancofalabella.cl </a>
+                   #QuedateEnCasa y prefiere nuestros canales digitales! Puedes consultar el estado de tu Tarjeta, solicitar información, conocer nuestros beneficios y contactar con nuestros ejecutivos.Todo en <a href="https://www.bancofalabella.cl"> www.bancofalabella.cl </a>
 
                 <br>
                 <br>
@@ -57,11 +57,11 @@ $enddate=strtotime("+6 days", $startdate);
                 </div>
 
 
-                   <a onclick="eventosLanding('Contactar con un Ejecutivo');" href="tel:+56967599345" class="btn bg-success text-white col-12 mt-4"><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png">Contactar Ejecutivo</a>
+                   <a onclick="eventosLanding('Contactar con un Ejecutivo');" href="tel:+56978274059" class="btn bg-success text-white col-12 mt-4"><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png">Contactar Ejecutivo</a>
 
 
                
-                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56967599345&text=Hola,%20tengo%20una%20consulta" class="btn bg-success text-white col-12 mt-4"><img width="40" src="https://i.pinimg.com/originals/79/dc/31/79dc31280371b8ffbe56ec656418e122.png">Enviar SMS por Whatsapp</a>
+                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56978274059&text=Hola,%20tengo%20una%20consulta" class="btn bg-success text-white col-12 mt-4"><img width="40" src="https://i.pinimg.com/originals/79/dc/31/79dc31280371b8ffbe56ec656418e122.png">Enviar SMS por Whatsapp</a>
 
 
                 <button onclick='window.location.href="https://www.bancofalabella.cl/pagar-cmr"; eventosLanding("Pagina de Pagos");' class="btn bg-success text-white col-12 mt-4">PAGAR AHORA</button>
