59dd8cf929a413243ec840e41091b9f930df1e84
Jesus Cortez <cortezjesus01@gmail.com>
2021-11-04 15:23:27 -0300

    Update claro_movil.blade.php
    vc1

--- resources/views/landings/claro_movil.blade.php
+++ resources/views/landings/claro_movil.blade.php
@@ -279,7 +279,7 @@
                 } 
             }
 
-           var correo = ["jesus.binteraction@gmail.com"];
+           var correo = ["pedro.guajardo@solvencia.cl"];
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
