dcb542bd980a1c0b2752d6efde64932a9fff3a2f
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-11-05 16:58:31 -0300

    Update claro_hogar2_.blade.php
    DSDSD

--- resources/views/landings/claro_hogar2_.blade.php
+++ resources/views/landings/claro_hogar2_.blade.php
@@ -159,12 +159,12 @@
     </div>
 
      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
        
+        <span class="sr-only">Previous</span>
     </a>
     <a class="right carousel-control" href="#myCarousel" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right"></span>
-   
+        <span class="sr-only">Next</span>
     </a>
     </div>
     </div>
