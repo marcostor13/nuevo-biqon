0665fcdde2406bb96cce2ff9e5fdb83dc3cd3425
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-11-05 17:13:00 -0300

    Update claro_hogar2_.blade.php
    jj

--- resources/views/landings/claro_hogar2_.blade.php
+++ resources/views/landings/claro_hogar2_.blade.php
@@ -47,6 +47,12 @@
   font-weight: 300;
 }
 
+.oscurecer {
+  background-image: 
+    linear-gradient(
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.5)
+    )
     }
     .med {
   background-image: 
@@ -153,11 +159,11 @@
     </div>
 
      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
     <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
     </div>
