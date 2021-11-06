9cc80bcf5f6c92d598b6740779997a4bd4f2a5f3
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-11-05 21:10:23 -0300

    Update claro_movil_2.blade.php
    dsdsdsd

--- resources/views/landings/claro_movil_2.blade.php
+++ resources/views/landings/claro_movil_2.blade.php
@@ -191,7 +191,76 @@ body {
   }
 }
 
+body{
+  background-color: lightgrey;
+  font-family: sans-serif;
+}
 
+.container{
+  margin: auto;
+  background-color: white;
+  width: 800px;
+  padding: 30px;
+}
+
+ul, li {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+}
+
+ul.slider{
+  position: relative;
+  width: 800px;
+  height: 300px;
+}
+
+ul.slider li {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    opacity: 0;
+    width: inherit;
+    height: inherit;
+    transition: opacity .5s;
+    background:#fff;
+}
+
+ul.slider li img{
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+}
+
+ul.slider li:first-child {
+    opacity: 1; /*Mostramos el primer <li>*/
+}
+
+ul.slider li:target {
+    opacity: 1; /*Mostramos el <li> del enlace que pulsemos*/
+}
+
+.menu{
+  text-align: center;
+  margin: 20px;
+}
+
+.menu li{
+  display: inline-block;
+  text-align: center;
+}
+
+.menu li a{
+  display: inline-block;
+  color: white;
+  text-decoration: none;
+  background-color: grey;
+  padding: 10px;
+  width: 20px;
+  height: 20px;
+  font-size: 20px;
+  border-radius: 100%;
+}
 
 
 
@@ -231,52 +300,34 @@ body {
          <div class="card-body">
 
 
-   <div class="wrapper">
-  <div class="slider" id="slider">
-    <ul class="slides">
-      <li class="slide" id="slide1">
-        <a href="#">
-          <p class="caption"></p>
-          <img class="col-12 mt-2" style="max-width: 350px; max-height:350px "src="https://imgur.com/DWibohM.jpg" alt="photo 1">
-        </a>
-      </li>
-      <li class="slide" id="slide2">
-        <a href="#">
-          <p class="caption"></p>
-          <img class="col-12 mt-2" style="max-width: 350px; max-height:350px "src="https://imgur.com/DWibohM.jpg" alt="photo 2">
-        </a>
-      </li>
-      <li class="slide" id="slide3">
-        <a href="#">
-          <p class="caption"></p>
-          <img class="col-12 mt-2" style="max-width: 350px; max-height:350px "src="https://imgur.com/DWibohM.jpg" alt="photo 3">
-        </a>
-      </li>
-      <li class="slide" id="slide4">
-        <a href="#">
-          <p class="caption"></p>
-          <img class="col-12 mt-2" style="max-width: 350px; max-height:350px "src="https://imgur.com/DWibohM.jpg" alt="photo 4">
-        </a>
-      </li>
-      <li class="slide" id="slide5">
-        <a href="#">
-           <p class="caption"></p>
-           <img class="col-12 mt-2" style="max-width: 350px; max-height:350px "src="https://imgur.com/DWibohM.jpg" alt="photo 5">
-        </a>
-      </li>
-      <li class="slide">
-        <a href="#">
-          <p class="caption"></p>
-          <img class="col-12 mt-2" style="max-width: 350px; max-height:350px "src="https://imgur.com/DWibohM.jpg" alt="photo 1">
-        </a>
-      </li>
-    </ul>
-    <ul class="slider-controler">
-      <li><a href="#slide1">&bullet;</a></li>
-      <li><a href="#slide2">&bullet;</a></li>
-      
-    </ul>
-  </div>
+  <div class="container">
+  
+  <ul class="slider">
+    <li id="slide1">
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3947459/car.jpg"/>
+    </li>
+    <li id="slide2">
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3947459/sunset.jpg"/>
+    </li>
+    <li id="slide3">
+      <h1>Ejemplo con otros elementos</h1>
+      <p>Esto es un párrafo de ejemplo para comprobar que podemos meter cualquier tipo de elementos en el slider</p>
+      <a href="https://kikopalomares.com/">¡Corre a mi web para más contenido!</a>
+    </li>
+  </ul>
+  
+  <ul class="menu">
+    <li>
+      <a href="#slide1">1</a>
+    </li>
+    <li>
+      <a href="#slide2">2</a>
+    </li>
+     <li>
+      <a href="#slide3">3</a>
+    </li>
+  </ul>
+  
 </div>
     <script>
 
