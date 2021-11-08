f5763e90ff386d21364e67c6f20584f53dc383f4
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-11-08 13:46:09 -0300

    Update claro_hogar2_.blade.php
    sdsdsds

--- resources/views/landings/claro_hogar2_.blade.php
+++ resources/views/landings/claro_hogar2_.blade.php
@@ -440,7 +440,7 @@
                     $('#message').removeClass('hide');
                      $('#cont2').addClass('hide');
                      $('#cont3').removeClass('hide');
-                     $('#message').text('Muchas Gracias. Su Solicitud Fue enviada a nuestra área. Nos pondremos en contacto con usted');
+                     $('#message').text('Muchas Gracias. Su Solicitud Fue enviada a nuestra área.');
 
                 }else{
                     $('#message').removeClass('hide');
