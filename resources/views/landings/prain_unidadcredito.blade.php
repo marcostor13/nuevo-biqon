db13f44c933c928f9e27cfd75cde1be36fd6232a
Jesus Cortez <cortezjesus01@gmail.com>
2021-11-10 16:16:33 -0300

    Update prain_unidadcredito.blade.php
    vc

--- resources/views/landings/prain_unidadcredito.blade.php
+++ resources/views/landings/prain_unidadcredito.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <?php
 $startdate=strtotime("Today");
-$enddate=strtotime("+6 days", $startdate);
+$enddate=strtotime("+9 days", $startdate);
 //$name = $_GET['NOMBRE'];
 ?>
 <style>
