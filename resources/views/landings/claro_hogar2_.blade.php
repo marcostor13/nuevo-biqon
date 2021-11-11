2fed7d1c7ae3e51552985830d548bac6fc07e2c8
Jesus Cortez <cortezjesus01@gmail.com>
2021-11-11 14:32:20 -0300

    Update claro_hogar2_.blade.php
    X

--- resources/views/landings/claro_hogar2_.blade.php
+++ resources/views/landings/claro_hogar2_.blade.php
@@ -94,8 +94,7 @@
             <h3  class="text-primary "><strong> Conoce nuestros Planes Claro Hogar </strong> </h3>
             <h4> Escoge el paquete ideal para ti y tu familia</h4> </center>
             <div id="opt1" class="p-2 pl-3">
-                        <!--<div class="card">     
-                            <div class="card-body">-->
+                       
                                <center>
                                <a onclick="Mens1();" class="btn bg-white text-danger col-12 mt-2"> <img class="col-12 mt-2" style="max-width:400px;" src="https://imgur.com/GWkleqW.png"></a> <br>
 
@@ -106,25 +105,7 @@
 
  
                                 <button onclick="sendMail('No deseo recibir Mas Noticias de Claro'); ;eventosLanding('No deseo Productos Claro'); return false;" class="btn bg-white text-danger col-12 mt-4"><h4>No deseo recibir Mas Mensajes de claro</h4></button>
-                               <!--button onclick="Mens1();" class="btn bg-white text-danger col-4 mt-2"> <strong>   1 Play: <br>Internet </strong> </button>
-                               <button onclick="Mens2();" class="btn bg-white text-danger col-4 mt-2"> <strong>  2 Play: <br>Internet + Tv</strong> </button>
-                               <button onclick="Mens3();" class="btn bg-white text-danger col-4 mt-2"> <strong>  3 Play: <br>Internet + Tv + Telefonía</strong> </button>-->
-                       <!--  <table>
-                    <tr class= "bg-danger">
-                    <td><img onclick="Mens1();"class="col-3 mt-2" style="max-width:200px;" src="https://i.imgur.com/9hoo6Uk.png"></td>
-                    <td><button onclick="Mens1();" class="btn bg-danger text-white col-4 mt-2"> <strong>   1 Play: <br>Internet </strong> </button></td>
-                 </tr>
-
-                    <tr class=>
-                    <td><img onclick="Mens2();"class="col-3 mt-2" style="max-width:200px;" src="https://i.imgur.com/B9n98KE.png"></td>
-                    <td><button onclick="Mens2();" class="btn bg-white text-danger col-4 mt-2"> <strong>  2 Play: <br>Internet + Tv</strong> </button></td>
-                    </tr>
-
-                    <tr>
-                    <td><img onclick="Mens3();"class="col-3 mt-2" style="max-width:200px;" src="https://i.imgur.com/fDpn2k2.png"></td>
-                    <td><button onclick="Mens3();" class="btn bg-white text-danger col-4 mt-2"> <strong>  3 Play: <br>Internet + Tv + Telefonía</strong> </td>
-                   </tr>
-                    </table>-->
+                               
                               
             </div>
           
