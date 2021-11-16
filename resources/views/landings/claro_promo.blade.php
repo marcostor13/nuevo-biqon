5bb4d3b62108e2e73c96d4c169b2fb894f2ac8e0
Jesus Cortez <cortezjesus01@gmail.com>
2021-11-16 18:28:51 -0300

    Update claro_promo.blade.php
    cv

--- resources/views/landings/claro_promo.blade.php
+++ resources/views/landings/claro_promo.blade.php
@@ -67,7 +67,7 @@ $enddate=strtotime("+5 days", $startdate);
                        </div> 
                     </div>
 
-                    <div id="cont2" class="p-2 pl-3 pr-3 ">
+                    <div id="cont2" class=" ">
 
                     <center> 
                     <div class="d-flex justify-content-around align-content-center mt-4">
@@ -76,22 +76,18 @@ $enddate=strtotime("+5 days", $startdate);
                                 </div>
 
 
-                        <div class="card">     
+                       <!--   <div class="card">     
                             <div class="card-body">
-                             <!--   <h3 class="text-grey text-center"> Estimado(a) <strong> <span id="name"></span> </strong> </h3>-->
+                              <h3 class="text-grey text-center"> Estimado(a) <strong> <span id="name"></span> </strong> </h3>-->
                                <!--<p class="text-white"> <h4> 
                          ¿Quieres aumentar tu velocidad de Internet y agregar Television a tu plan hogar?</h4> </p> 
-                               <p> Transforma tu plan de 200 mb en 400 Mb + TV y obten un descuento de $17.000 x 12 meses</p>
--->
-                                
-                               
-
+                               <p> Transforma tu plan de 200 mb en 400 Mb + TV y obten un descuento de $17.000 x 12 meses</p>c-->
                                 <button onclick="sendMail('Solicito ser Contactado Promo  INTERNET 400MB + Tv Pro HD'); ;eventosLanding('Solicitud Informacion'); return false;" class="btn bg-danger text-white col-12 mt-2"><h4>Contactar</h4></button>
 
                                  <button onclick="sendMail('No deseo recibir Mas Noticias de Claro'); ;eventosLanding('No deseo Productos Claro'); return false;" class="btn bg-white text-danger col-12 mt-2"><h5>No deseo recibir Mas Mensajes de Claro</h5></button>
                                  </center>
-                            </div>   
-                        </div>
+                            <!--</div>   
+                        </div>-->
                     </div>
           
              
