104acb300ddb6ba0a530a8830851451484d72a39
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-12-02 13:50:10 -0300

    Update binternavidad1.blade.php
    dwdwdd

--- resources/views/landings/binternavidad1.blade.php
+++ resources/views/landings/binternavidad1.blade.php
@@ -60,7 +60,7 @@ $enddate=strtotime("+5 days", $startdate);
             <img style="max-width: 50%" class="img-fluid col-md-3 mt-2" src="https://imgur.com/QG9Npsy.jpg" alt="USS">
             
 
-<center><img width="100%" src="https://imgur.com/eMxR8JR.jpg"></center>
+<center><img width="113%" src="https://imgur.com/eMxR8JR.jpg"></center>
            <!-- <a target="_blank" href="https://www.advance.uss.cl/?utm_source=google&utm_medium=search&utm_campaign=advance&utm_term=generico&utm_content=nacional&gclid=EAIaIQobChMIsLn10Z-e4wIVFwSRCh0QaAqIEAAYASAAEgJdNvD_BwE" class="btn bg-danger text-white col-8 mt-4">Ir a la página web
               <i class="fas fa-graduation-cap ml-2"></i>
             </a>-->
