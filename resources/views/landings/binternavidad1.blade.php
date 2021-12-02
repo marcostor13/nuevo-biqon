15f14da05cf8deef6c1abcbcfdd276afa08eec86
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-12-02 11:45:39 -0300

    Update binternavidad1.blade.php
    sdsdsd

--- resources/views/landings/binternavidad1.blade.php
+++ resources/views/landings/binternavidad1.blade.php
@@ -57,7 +57,7 @@ $enddate=strtotime("+5 days", $startdate);
 
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-80">
             <!--<img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="USS">-->
-            <img style="max-width: 40%" class="img-fluid col-md-3 mt-2" src="https://firebasestorage.googleapis.com/v0/b/biqon-d2511.appspot.com/o/landings%2Flogos%2F1617974764515_unnamed%20(2).png?alt=media&token=28e9512a-8472-434e-98ad-0066598bd2af" alt="USS">
+            <img style="max-width: 40%" class="img-fluid col-md-3 mt-2" src="https://imgur.com/QG9Npsy.jpg" alt="USS">
             
 
            <!-- <a target="_blank" href="https://www.advance.uss.cl/?utm_source=google&utm_medium=search&utm_campaign=advance&utm_term=generico&utm_content=nacional&gclid=EAIaIQobChMIsLn10Z-e4wIVFwSRCh0QaAqIEAAYASAAEgJdNvD_BwE" class="btn bg-danger text-white col-8 mt-4">Ir a la página web
