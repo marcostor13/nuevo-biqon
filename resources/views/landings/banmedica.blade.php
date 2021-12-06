efe8ef71c1a0c8e46c91a248dfe21468336fc842
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-12-06 18:25:38 -0300

    Update banmedica.blade.php
    cscscsc

--- resources/views/landings/banmedica.blade.php
+++ resources/views/landings/banmedica.blade.php
@@ -80,9 +80,9 @@ $enddate=strtotime("+5 days", $startdate);
 
 <br>
 </div>
-           <!-- </div>
+           < </div>
                  <div class="mt-2 text-center col-12" style="background:#fff;"> 
-<p class="text-black text-center"><br><strong>Seleccione alguna de estas opciones para comunicarse con un ejecutivo quien podrá asesorarle y resolver su consulta</strong></p>
+<p class="text-black text-center"><br><strong></strong></p>
                      <div class="d-flex justify-content-around align-content-center text-center"> 
       
                     <!-- <span class="text-white  text-center">Mi Cuenta<br>
