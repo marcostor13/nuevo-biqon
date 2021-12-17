41089e47cc8a6def897cd24381ef2f84e39d799d
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-12-17 14:29:13 -0300

    Update amicar_fortaleza.blade.php
    dsdsdd

--- resources/views/landings/amicar_fortaleza.blade.php
+++ resources/views/landings/amicar_fortaleza.blade.php
@@ -45,8 +45,6 @@ $enddate=strtotime("+15 days", $startdate);
 
                     <div class="d-flex justify-content-around align-content-center mt-4"> 
                <!-- <a class="ml-4" onclick="eventosLanding('WHATSAPP');" href="https://wa.me/56930132218"><img width="60" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>-->
-
-                <a class="ml-2" onclick="eventosLanding('LLAMADA');" href="tel:+56975890486"><img width="50" src="https://imgur.com/h9LYxxu.png"></a>
                    
                 <a class="ml-4" onclick="eventosLanding('WHATSAPP');" href="https://api.whatsapp.com/send?phone=56988078022&text=Hola,%20tengo%20una%20consulta"><img width="50" src="https://i.imgur.com/zwRWdi8.png"></a>
 
