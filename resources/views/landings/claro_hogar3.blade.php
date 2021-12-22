9e4263a20e2a45c12d96842f79532218cab19568
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-12-22 12:59:53 -0300

    Update claro_hogar3.blade.php
    cscscsc

--- resources/views/landings/claro_hogar3.blade.php
+++ resources/views/landings/claro_hogar3.blade.php
@@ -158,8 +158,8 @@
                         <div class="card">     
                             <div class="card-body">
                                <!--<button onclick="Mens1();" class="btn bg-danger text-white col-12 mt-2"><img width="100" src="https://i.imgur.com/9hoo6Uk.png"> <strong>   1 Play: Internet </strong> </button>--> 
-                             <button onclick="Mens2();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://imgur.com/RMl8rwF.png"> <strong></strong> </button>
-                             <button onclick="Mens3();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://imgur.com/6S1xMqg.png"> <strong> </strong> </button>
+                             <button onclick="Mens2();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://i.ibb.co/FstbZ6X/2-22.jpg"> <strong></strong> </button>
+                             <button onclick="Mens3();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://i.ibb.co/qxrvrd6/3-22.jpg"> <strong> </strong> </button>
                             </div>
                         </div>
             </div>
@@ -222,9 +222,9 @@
     <div id="opt12" class="p-2 pl-3">
                         <div class="card">     
                             <div class="card-body">
-                               <button onclick="Mens1();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://imgur.com/GWkleqW.png"> <strong> </strong> </button>
+                               <button onclick="Mens1();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://i.ibb.co/99R21bv/1-22.jpg"> <strong> </strong> </button>
                                 <!--<button onclick="Mens2();" class="btn bg-danger text-white col-12 mt-2"><img width="100" src="https://i.imgur.com/B9n98KE.png"> <strong>  2 Play: Internet + Tv</strong> </button>-->
-                               <button onclick="Mens3();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://imgur.com/6S1xMqg.png"> <strong></strong> </button>
+                               <button onclick="Mens3();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://i.ibb.co/qxrvrd6/3-22.jpg"> <strong></strong> </button>
                             </div>
                         </div>
             </div>
@@ -281,8 +281,8 @@
     <div id="opt11" class="p-2 pl-3">
                         <div class="card">     
                             <div class="card-body">
-                            <button onclick="Mens1();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://imgur.com/GWkleqW.png"> <strong> </button> 
-                            <button onclick="Mens2();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://imgur.com/RMl8rwF.png"> <strong> </strong> </button>
+                            <button onclick="Mens1();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://i.ibb.co/99R21bv/1-22.jpg"> <strong> </button> 
+                            <button onclick="Mens2();" class="btn bg-white text-danger col-12 mt-2"><img width="300" src="https://i.ibb.co/FstbZ6X/2-22.jpg"> <strong> </strong> </button>
                                <!--<button onclick="Mens3();" class="btn bg-danger text-white col-12 mt-2"><img width="100" src="https://i.imgur.com/fDpn2k2.png"> <strong>  3 Play: Internet + Tv + Telefonía</strong> </button>-->
                             </div>
                         </div>
