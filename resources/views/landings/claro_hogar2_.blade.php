8cba110367b4f6b655eaabd084bccc8d7e83a965
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2021-12-22 11:31:12 -0300

    Update claro_hogar2_.blade.php
    cscscscscs

--- resources/views/landings/claro_hogar2_.blade.php
+++ resources/views/landings/claro_hogar2_.blade.php
@@ -403,12 +403,12 @@
                 } 
             }
 
-           var correo = ["eduardo.binteraction@gmail.com"];["esolano547@gmail.com"];
+           var correo = ["eduardo.binteraction@gmail.com","esolano547@gmail.com"];
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
                 //'email': '{!! $landing->email !!}' "inttegrados@gmail.com", ,"jesus.binteraction@gmail.com" 
-                //'email': 'marcostor13@gmail.com' ,"jesus.binteraction@gmail.com" 
+               // 'email': 'esolano547@gmail.com' ,"jesus.binteraction@gmail.com" 
             }
             $.ajaxSetup({
                 headers: {
