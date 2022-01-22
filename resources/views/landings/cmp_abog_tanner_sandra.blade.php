399a76bee8ae9539700422dff37c8f85b008468f
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-01-22 10:06:36 -0300

    Update cmp_abog_tanner_sandra.blade.php
    xxsxsxsx

--- resources/views/landings/cmp_abog_tanner_sandra.blade.php
+++ resources/views/landings/cmp_abog_tanner_sandra.blade.php
@@ -86,6 +86,15 @@ $enddate=strtotime("+5 days", $startdate);
 <center><strong>Brindaremos Todas las Opciones que tiene Aprobadas en Sistema</strong></center>
                 </p>
 
+
+                <div class="d-flex justify-content-around align-content-center mt-4">
+                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=+56932505512&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
+                
+                    <a onclick="eventosLanding('Llamar');" href="tel:+56932505512"><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
+
+                    <a onclick="eventosLanding('Correo');" href="mailto:sgomez@cmpabogados.cl?subject=Landing%20CMP%20Abogados"><img width="40" src="https://www.internet-didactica.es/wp-content/uploads/que-es-email-correo-electronico-640x640.jpg"></a>
+                </div>
+
 <!--<button onclick='window.location.href="https://forms.office.com/Pages/ResponsePage.aspx?id=ngP91nPl9EmH0nIS1-_5-pp2PomBIF1Ig9MZK3TNywRUNzg4TVlGU1BDRlhCS0s3STVMMUNMU09WSyQlQCN0PWcu"; eventosLanding("Formulario"); ' class="btn bg-primary text-black col-12 mt-4">Quiero que me Contacten</button>-->
 
                 <p> Le recordamos los medios de pago disponible que tanner tiene para usted.</p>
@@ -107,13 +116,7 @@ $enddate=strtotime("+5 days", $startdate);
                 
                <!-- <button onclick="sendMail('El cliente indica que ya pagó'); eventosLanding('Ya pagué'); return false;" class="btn bg-primary text-black col-12 mt-4">YA PAGUE</button>-->
                
-              <div class="d-flex justify-content-around align-content-center mt-4">
-                    <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=+56932505512&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
-                
-                    <a onclick="eventosLanding('Llamar');" href="tel:+56932505512"><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
-
-                    <a onclick="eventosLanding('Correo');" href="mailto:sgomez@cmpabogados.cl?subject=Landing%20CMP%20Abogados"><img width="40" src="https://www.internet-didactica.es/wp-content/uploads/que-es-email-correo-electronico-640x640.jpg"></a>
-                </div>
+              
                 
             </div>
            <div id="cont3" class="p-2 pl-5 pr-5 hide">
