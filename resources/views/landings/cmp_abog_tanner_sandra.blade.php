79451c77b05c631ee28cde91f43190d4826d4745
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-01-22 09:46:37 -0300

    Update cmp_abog_tanner_sandra.blade.php
    sdsds

--- resources/views/landings/cmp_abog_tanner_sandra.blade.php
+++ resources/views/landings/cmp_abog_tanner_sandra.blade.php
@@ -61,8 +61,7 @@ $enddate=strtotime("+5 days", $startdate);
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-start p-0">
 
                 <img style="max-width:250px; max-height:250px "  class="img-fluid col-10 col-md-3 mt-4" src="{{$landing->logo}}" alt="CMPAbogados">
-              <br>
-              
+
             <div id="cont1" class="p-5 ">
                 <h4 class="text-black text-center">Por tu seguridad, confírmanos los primeros 4 dígitos de tu RUT</h4>
                 <input id="rut" type="number" class="form-control mt-4 text-center text-black">
