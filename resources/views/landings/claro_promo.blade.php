ae2f2f12101e7f3199569eda99b0d5317f510e55
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-01-27 16:31:26 -0300

    Update claro_promo.blade.php
    xaxax

--- resources/views/landings/claro_promo.blade.php
+++ resources/views/landings/claro_promo.blade.php
@@ -85,7 +85,7 @@ $enddate=strtotime("+5 days", $startdate);
                                <!-- <button onclick="sendMail('Solicito ser Contactado Promo  INTERNET 400MB + Tv Pro HD'); ;eventosLanding('Solicitud Informacion'); return false;" class="btn bg-danger text-white col-12 mt-2"><h4>Contactar</h4></button>-->
 <h4> ¿Para mas informacion o Contratacíon Aqui?</h4> </p> 
 
-                               <a class="ml-4" onclick="eventosLanding('WHATSAPP');" href="https://api.whatsapp.com/send?phone=56946954910&text=Hola,%20Quisiera%20Mas%20Informacion%20sobre%20Plan%202play%20internet"><img width="50" src="https://i.imgur.com/zwRWdi8.png"></a>
+                               <a class="ml-2" onclick="eventosLanding('WHATSAPP');" href="https://api.whatsapp.com/send?phone=56946954910&text=Hola,%20Quisiera%20Mas%20Informacion%20sobre%20Plan%202play%20internet"><img width="50" src="https://i.imgur.com/zwRWdi8.png"></a>
 
                                <br>
 
