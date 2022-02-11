da608a6b402d418b2b74c5be15838ebf619c2107
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-02-11 16:05:07 -0300

    Update ualba.blade.php
    xxxx

--- resources/views/landings/ualba.blade.php
+++ resources/views/landings/ualba.blade.php
@@ -63,17 +63,17 @@ $enddate=strtotime("+5 days", $startdate);
               <i class="fas fa-graduation-cap ml-2"></i>
             </a>
 
-            <div id="cont1" class="p-3 ">
+            <div id="cont1" class="p-1 ">
                   <div class="card"> 
 
                 <h3 class="text-black text-center">Estimado(a): <span id="name"></span></h3>    
                 <div class="card-body">
 
                   <!-- Heading -->
-                  <h4 class="dark-grey-text text-center">
+                  <h3 class="dark-grey-text text-center">
                     <strong>¿Eres técnico y deseas estudiar una carrera profesional online? ¡Nosotros te aceptamos! 
 ¿Estás pensando en tu futuro? Estudia 100% online una segunda carrera con Advance.</strong>
-                  </h4>
+                  </h3>
                   <hr>
 
                   <div class="d-flex justify-content-between  m-0">
