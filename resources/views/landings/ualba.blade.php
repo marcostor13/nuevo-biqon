ca1075706b08dbb9316618f7fcdd038fd6cb4bbc
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-02-11 16:03:06 -0300

    Update ualba.blade.php
    sassasas

--- resources/views/landings/ualba.blade.php
+++ resources/views/landings/ualba.blade.php
@@ -63,10 +63,10 @@ $enddate=strtotime("+5 days", $startdate);
               <i class="fas fa-graduation-cap ml-2"></i>
             </a>
 
-<div id="cont2" class="p-5 hide">
-                <h3 class="text-black text-center">Estimado(a) <span id="name"></span></h3>
             <div id="cont1" class="p-3 ">
-                  <div class="card">     
+                  <div class="card"> 
+
+                <h3 class="text-black text-center">Estimado(a): <span id="name"></span></h3>    
                 <div class="card-body">
 
                   <!-- Heading -->
