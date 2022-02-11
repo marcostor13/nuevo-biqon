f23115f0ca798580c934da8f2a21d12f96536121
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-02-11 17:30:29 -0300

    Update ualba.blade.php
    axaxaxxa

--- resources/views/landings/ualba.blade.php
+++ resources/views/landings/ualba.blade.php
@@ -119,11 +119,12 @@ $enddate=strtotime("+5 days", $startdate);
                   </div>
 
                   <div class="text-center">
+<br>
 
 <center>  Nuestra Web!<a href="https://admision.udalba.cl/programas-advance2/"> wwww.admision.udalba.cl</a> </center>
                       
 
-                    
+
                     <hr>
                     <button class="btn bg-danger text-white col-12 mt-4" onclick="sendMail('El cliente solicito Informacion');eventosLanding('Solicitud Informacion'); return false;">Contactame</button>
                   
