36b7bd17c88c67fc16448099131f7e03e7b35b9f
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-02-11 16:12:17 -0300

    Update ualba.blade.php
    xaxax

--- resources/views/landings/ualba.blade.php
+++ resources/views/landings/ualba.blade.php
@@ -131,7 +131,7 @@ $enddate=strtotime("+5 days", $startdate);
 
                  <a onclick="eventosLanding('Correo');" href="mailto:advance@dalba.cl?subject=Hola,%20Quisiera%20Mas%20Informacion"><img width="50" src="https://imgur.com/izFpLFz.jpg"></a>
 
-              <a onclick="eventosLanding('Llamar');" href="tel:+56935407683"><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
+              <a onclick="eventosLanding('Llamar');" href="tel:+56935407683"><img width="50" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
 
                <a class="ml-4" onclick="eventosLanding('pagina web');" href="https://admision.udalba.cl/programas-advance2/"><img width="50" src="https://imgur.com/Tu3WSx7.png"></a>
                     </center>
