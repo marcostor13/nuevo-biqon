81ca5375c6f670809147b99df11826bc768bd449
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-02-11 17:20:00 -0300

    Update ualba.blade.php
    dsdsdsd

--- resources/views/landings/ualba.blade.php
+++ resources/views/landings/ualba.blade.php
@@ -149,11 +149,11 @@ $enddate=strtotime("+5 days", $startdate);
     </div>
   <center> <strong>Para mas Informacion:</strong> </center>
                   
-                 <a class="ml-2"href="https://api.whatsapp.com/send?phone=56935407683&text=Hola,%20Quisiera%20Mas%20Informacion"><img width="50" src="https://i.imgur.com/zwRWdi8.png"></a>
+            <center>    <a class="ml-2"href="https://api.whatsapp.com/send?phone=56935407683&text=Hola,%20Quisiera%20Mas%20Informacion"><img width="50" src="https://i.imgur.com/zwRWdi8.png"></a>
 
                  <a onclick="eventosLanding('Correo');" href="mailto:advance@dalba.cl?subject=Hola,%20Quisiera%20Mas%20Informacion"><img width="50" src="https://imgur.com/izFpLFz.jpg"></a>
 
-              <a onclick="eventosLanding('Llamar');" href="tel:+56935407683"><img width="50" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
+              <a onclick="eventosLanding('Llamar');" href="tel:+56935407683"><img width="50" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a></center> 
      <script>
    //EVENT 1
        $(function(){
