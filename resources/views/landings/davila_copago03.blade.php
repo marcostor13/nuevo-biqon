c9cefe90a123dd9dd20ab745468a6e09ef3b5c12
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-03-07 13:10:54 -0300

    Update davila_copago03.blade.php
    ddwdwdd

--- resources/views/landings/davila_copago03.blade.php
+++ resources/views/landings/davila_copago03.blade.php
@@ -76,8 +76,8 @@
                             <div class="card-body">
                                 <h4 class="text-grey text-center"> Estimado(a) <strong> Paciente <!-- <span id="name"></span> --></strong> </h4>
 
-                                <p class="text-white text-center"> <h5> 
-                                 Para regularizar su cuenta pendiente, lo invitamos a utilizar la modalidad de pago en línea, en la web de Clinica Davila, seleccionando la opción PAGAR.</h5> </p>
+                                <<h5 class="text-white text-justificado"> 
+                                 Para regularizar su cuenta pendiente, lo invitamos a utilizar la modalidad de pago en línea, en la web de Clinica Davila, seleccionando la opción PAGAR.</h5> 
 
                                <strong> <p> N° de Admisión de su cuenta es:  <h5 id="dato1" class="dato1"> </h5></p></strong> 
                                <!--<strong> <h4 id="dato1" class="dato1"> </h4>  </strong>-->
