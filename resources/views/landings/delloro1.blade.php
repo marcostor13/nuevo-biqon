56bb12a1be95244a8804e99bde3afee7609272e4
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-03-21 20:33:08 -0300

    Update delloro1.blade.php
    cscsscsc

--- resources/views/landings/delloro1.blade.php
+++ resources/views/landings/delloro1.blade.php
@@ -93,15 +93,19 @@ $cliente = $_GET['DATA2'];
                   <p class="text-black text-center">
                  Para contactarnos puede hacerlo por los siguientes medios:
                  <br>
-Fono de Contacto: 968391674
+                 <br>
+Fono de Contacto:<strong> 968391674</strong>
 <br>
-Correo electrónico: DGIRON@DELLOROABOGADOS.CL
+Correo electrónico: <strong>DGIRON@DELLOROABOGADOS.CL</strong>
 <br>
-Nombre Ejecutivo: DIANA GIRON
+Nombre Ejecutivo:<strong> DIANA GIRON</strong>
 <br>
  </p>
        También, puede comunicarse con nosotros a nuestro Servicio de Atención de Clientes al <a href="tel:+226964361"> 226964361</a>, o dirigirse a nuestra oficina comercial ubicada en Carmencita 25, Of. 82, Las Condes.
          
+
+         <p><small>Esta gestión no corresponde a un procedimiento que persiga la ejecución de sus bienes. La Ley 19.496 establece los derechos de los consumidores respecto de las acciones de cobranza extrajudiciales, entre ellas: se prohíbe que afecte su privacidad o situación laboral; los llamados de cobranza sólo se pueden realizar de lunes a sábados entre las 8:00 y las 20:00 horas; el consumidor tienen derecho a pagar su deuda directamente a la empresa a la que le debe; los gastos de cobranza extrajudicial deberán cumplir los límites establecidos en inciso 2 del artículo 37 de misma Ley, y se aplicarán transcurridos los primero 20 días desde la mora. Infórmese responsablemente sobre sus derechos en www.sernac.cl</p>
+
 </div>
            <!-- </div>
                  <div class="mt-2 text-center col-12" style="background:#F1EFEE;">
