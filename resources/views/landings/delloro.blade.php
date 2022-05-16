718f22616ee1098b63186e3fa81be8fbcfbed5c7
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-05-16 12:59:14 -0400

    Update delloro.blade.php
    sssss

--- resources/views/landings/delloro.blade.php
+++ resources/views/landings/delloro.blade.php
@@ -109,7 +109,7 @@ y regularice su situación con Mundo Crédito
                 <div class="d-flex justify-content-around align-content-center mt-4">
                     <a onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=+56945369456&text=Hola,%20tengo%20una%20consulta"><img width="40" src="https://i.pinimg.com/originals/6b/6f/95/6b6f9559658ad9c3d371977a674e2a56.png"></a>
                 
-                    <a onclick="eventosLanding('Llamar');" href="tel:+56945369456 "><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>
+                   <!-- <a onclick="eventosLanding('Llamar');" href="tel:+56945369456 "><img width="40" src="https://puertascolmena.com/wp-content/uploads/2019/05/img2.png"></a>-->
 
                     <a onclick="eventosLanding('Correo');" href="mailto:f.belloy@delloroabogados.cl?subject=Landing%20DellOro&body=Adjuto%20es%20el%20Comprobante%20de%20Pago">
                         <img width="40" src="https://es.seaicons.com/wp-content/uploads/2015/10/Email-icon.png"></a>
