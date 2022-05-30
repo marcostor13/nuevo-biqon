ed42cc66c0c6615bbf5e6d97676cd8f8139b5ad1
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-05-30 15:02:56 -0400

    Update ualba.blade.php
    sssssssss

--- resources/views/landings/ualba.blade.php
+++ resources/views/landings/ualba.blade.php
@@ -276,7 +276,7 @@ $enddate=strtotime("+5 days", $startdate);
                     'landing': '{!! $landing->name !!}'
                 } 
             }
-            var correo = ["advance@udalba.cl"];
+            var correo = ["eduardo.binteraction@gmail.com","jacqueline.rodriguez@udalba.cl","nancy.poblete@udalba.cl"];
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
