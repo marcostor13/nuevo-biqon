0ce1e8791a9e43acb8d289bd19eecaf00d3f5fa5
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-06-09 14:42:10 -0400

    Update amicar_seleccion.blade.php
    sssss

--- resources/views/landings/amicar_seleccion.blade.php
+++ resources/views/landings/amicar_seleccion.blade.php
@@ -58,7 +58,7 @@ $enddate=strtotime("+5 days", $startdate);
 
        <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-80">
 <center>
- <img style="max-width: 100%" class="img-fluid col-md-3 mt-2" src="https://imgur.com/rOkxBSO.png" alt="AMICAR"></center>
+ <img style="max-width: 120%" class="img-fluid col-md-3 mt-2" src="https://imgur.com/bZmpCPr.png" alt="AMICAR"></center>
 
           
            <!-- <center>  Nuestra Web!<a href="https://admision.udalba.cl/programas-advance2/"> wwww.admision.udalba.cl</a> </center>
