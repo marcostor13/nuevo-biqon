7336c93437632e1463ee9172180e8d1598d03bd8
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-06-13 14:47:51 -0400

    Update naztech.blade.php
    ssssss

--- resources/views/landings/naztech.blade.php
+++ resources/views/landings/naztech.blade.php
@@ -246,22 +246,17 @@
                 
                 <tr>
                 
-                  <td><a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56958115224&text=Hola,%20tengo%20una%20consulta%20"><img width="80" src="https://imgur.com/FUTp4DG.png"></a></span>
+                  <td><a class="ml-2" onclick="eventosLanding('Whatsapp');" href="https://api.whatsapp.com/send?phone=56958115224&text=Hola,%20tengo%20una%20consulta%20po"><img width="80" src="https://imgur.com/FUTp4DG.png"></a></span>
                   <td> </td><td> </td>
                   <td><a class="ml-2" onclick="eventosLanding('instagram');" href="https://www.instagram.com/naztech.chile/"><img width="100" src="https://imgur.com/19TOYgi.png"></a></span>
                   <td> </td><td> </td>
                   <td><a class="ml-2" onclick="eventosLanding('facebook');"href="https://www.facebook.com/naztechchile/"><img width="80" src="https://imgur.com/qvvD9oZ.png"></a></span>
                   <td> </td><td> </td>
-                  <td><a class="ml-2" onclick="eventosLanding('pagina web');"  href="https://www.naztech.cl/"> <img width="120" src="https://imgur.com/C5J9nIc.png"></a></span>
+                  <td><a class="ml-2" onclick="eventosLanding('pagina web');"  href="mailto:hcortez@naztech.cl.com?subject=Mas%20informacion%20de%20productos&body"> <img width="120" src="https://imgur.com/VNdssgs.png"></a></span>
                 
                 </tr>
                 
                 </table>
-              </center>
-
-<center>
-              <a class="ml-2" onclick="eventosLanding('pagina web');"  href="https://www.naztech.cl/"> <img width="120" src="https://imgur.com/C5J9nIc.png"></a></span>
-
               </center>
             
           </div>
