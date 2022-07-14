1280dbf7a5cd8354e173ef22b2160b40a598e269
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-07-14 11:33:16 -0400

    Update amicar_agenda.blade.php
    ddddd

--- resources/views/landings/amicar_agenda.blade.php
+++ resources/views/landings/amicar_agenda.blade.php
@@ -206,7 +206,7 @@ $enddate=strtotime("+15 days", $startdate);
                 } 
             }
 
-            var correo = ["esolano547@gmail.com"];
+            var correo = ["andrea.vergara@amicar.cl","carla.torres@amicar.cl"];
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
