f0aed5c995a953a7257dcd3d63621c9b2062ee18
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-07-14 10:46:45 -0400

    Update amicar_agenda.blade.php
    xxxx

--- resources/views/landings/amicar_agenda.blade.php
+++ resources/views/landings/amicar_agenda.blade.php
@@ -29,7 +29,7 @@ $enddate=strtotime("+15 days", $startdate);
 
  
 
-    <a onclick="sendMail('QUISIERA AGENDAR'); eventosLanding('AGENDAR'); return false;" class="btn bg-white text-danger col-12 mt-4"><h4>
+    <a onclick="sendMail('Quisiera agendar'); eventosLanding('AGENDAR'); return false;">
 
     <center><img width="100%" src="https://imgur.com/Y3Sw6ay.jpg"></center>
 </a>
