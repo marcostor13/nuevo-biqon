6c4987a588d872927b34448386ae11c40133e1b0
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-07-28 15:22:39 -0400

    Update amicar_renovacion_2.blade.php
    sssss

--- resources/views/landings/amicar_renovacion_2.blade.php
+++ resources/views/landings/amicar_renovacion_2.blade.php
@@ -104,8 +104,8 @@ producto opción inteligente
     
              <div  style="color:#FF4500"class="d-flex justify-content-between  m-0">
                     <div style="color:#FF4500"class="md-form col-12  m-0">
-                                      <textarea type="text" style="color:#FF4500" name="mensaje" for="mensaje" required placeholder="Describe Brevemente en menos de 300 Carácteres" class="form-control"maxlength="300" id="correo">
-                            </textarea>
+                                      <input  type="text" style="color:#FF4500" name="mensaje" for="mensaje" required placeholder="Describe Brevemente en menos de 300 Carácteres" class="form-control"maxlength="300" id="correo">
+                            </input>
 
                       <label for="form2"></label>
 
