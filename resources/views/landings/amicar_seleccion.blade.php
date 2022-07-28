518850dfa6493e5876841df30a2b076247d51e2a
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-07-28 14:00:45 -0400

    Update amicar_seleccion.blade.php
    xssxxxx

--- resources/views/landings/amicar_seleccion.blade.php
+++ resources/views/landings/amicar_seleccion.blade.php
@@ -83,6 +83,7 @@ $enddate=strtotime("+5 days", $startdate);
                   </h4>
 
                   <br>
+<h4 style="color:#000000">Nos interesa conocer que ópcion es de tu preferencia</h4>
         
                     <br>
                   </h4>
@@ -91,8 +92,13 @@ $enddate=strtotime("+5 days", $startdate);
 
 
  <div style="color:#FF4500" class="md-form col-12 m-0"> 
-                    
-
+                    <select  style="color:#FF4500"class="form-control" id="preferencia" required>
+                      <option style="color:#FF4500"value="" data-select2-id="4">Selecciona una Opcion</option>
+                      
+      <option value="RENOVAR" data-select2-id="8">RENOVAR</option>
+      <option value="REFINANCIAR" data-select2-id="9">REFINANCIAR</option>
+      <option value="PREPAGAR" data-select2-id="10">PREPAGAR</option>
+                    </select>
                   </div>
 
 
@@ -102,28 +108,28 @@ $enddate=strtotime("+5 days", $startdate);
 <br>
 
 
+<h4 style="color:#000000">Si tu preferencia es Renovar que modelo es de tu intéres y en que mes</h4>
                     <br>
                   </h4>
 <center>
-               
-
+               <label style="color:#FF4500"for="Modelo" class="form-label"> Ingresar Modelo</label>
                 <br>
 
                 <div style="color:#FF4500" class="d-flex justify-content-between  m-0">
                     <div style="color:#FF4500"class="md-form col-12  m-0">
-                      
+                      <input style="color:#FF4500"type="text" id="Modelo" class="form-control" required placeholder="Ingresar Modelo">
                       <label for="form3"></label>
                     </div>
                   </div>
 
                   <br>
 
-                 
+                  <label style="color:#FF4500"for="Modelo" class="form-label"> Ingresar Mes</label>
                 <br>
 
                 <div style="color:#FF4500" class="d-flex justify-content-between  m-0">
                     <div style="color:#FF4500"class="md-form col-12  m-0">
-                      
+                      <input style="color:#FF4500"type="text" id="Mes" class="form-control" required placeholder="Ingresar Mes">
                       <label for="form3"></label>
                     </div>
                   </div>
@@ -131,11 +137,11 @@ $enddate=strtotime("+5 days", $startdate);
                   <br>
 
 
-
+<h4 style="color:#000000">Favor ingresar el correo para contactar</h4>
 
                     <br>
                   </h4>
-                  
+                  <label style="color:#FF4500"for="Modelo" class="form-label"> Ingresar Correo</label>
                   <br>
                    <div  style="color:#FF4500"class="d-flex justify-content-between  m-0">
                     <div style="color:#FF4500"class="md-form col-12  m-0">
@@ -284,7 +290,7 @@ $enddate=strtotime("+5 days", $startdate);
                     'landing': '{!! $landing->name !!}'
                 } 
             }
-            var correo = ["eduardo547@gmail.com","eduardo.binteraction@gmail.com"];
+            var correo = ["renovaciones@amicar.cl"," carla.torres@amicar.cl"];
             let dataSend = {
                 'data': JSON.stringify(data),
                 'email': correo
