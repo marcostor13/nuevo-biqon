7784b80e57df2b166ccb7634b7d6068e02670697
LuisTobarInda <58691879+LuisTobarInda@users.noreply.github.com>
2022-07-28 13:12:08 -0400

    Update amicar_renovacion_2.blade.php
    ssssss

--- resources/views/landings/amicar_renovacion_2.blade.php
+++ resources/views/landings/amicar_renovacion_2.blade.php
@@ -98,7 +98,7 @@ producto opción inteligente
 
 
 
-                    <button class="btn bg-danger text-white col-12 mt-2" onclick="sendMail('repuesta del proceso');eventosLanding('BOTON RESPUESTA'); return false;">Enviar información</button>
+                    <button class="btn bg-danger text-white col-12 mt-2" onclick="sendMail('Repuesta del Proceso De Renovacion');eventosLanding('BOTON RESPUESTA'); return false;">Enviar información</button>
 
                   <div class="text-center">
 <br>
