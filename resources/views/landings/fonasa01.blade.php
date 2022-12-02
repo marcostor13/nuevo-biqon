1b24f6e8dd05b9baf62cb341f0deb830b12ef3e1
eduardo <jvostro1200@gmail.com>
2022-12-02 16:29:27 -0300

    Update fonasa01.blade.php
    dsdsdsd

--- resources/views/landings/fonasa01.blade.php
+++ resources/views/landings/fonasa01.blade.php
@@ -165,11 +165,13 @@ $name = $_GET['NOMBRE'];
                     <a onclick="eventosLanding('Correo');" href="mailto: judicial@herediaabogados.cl?subject=Cobranza%20Fonasa"><img width="40" src="https://www.marketingdirecto.com/wp-content/uploads/2014/03/correo-electronico.png"></a>
                 
                 </div>-->
-                
+
+                <br>
+          <strong>    
         <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
          <h5 id="message"class="text-grey text-center mt-3"> Si al momento de recibir esta información, su cuenta está regularizada, rogamos omitir este aviso</h5>
         </div>
-  
+  </strong>  
     </div>
 
      <script>
