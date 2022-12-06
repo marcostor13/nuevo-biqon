579ec11ed56ef50d7fc63227355147f8df888a67
eduardo <jvostro1200@gmail.com>
2022-12-06 13:56:40 -0300

    Update inacap2.blade.php
    lpllpl

--- resources/views/landings/inacap2.blade.php
+++ resources/views/landings/inacap2.blade.php
@@ -24,7 +24,7 @@ $enddate=strtotime("+15 days", $startdate);
 
             <div id="cont1">
                 <div class="videoWrapper">        
-                    <iframe width="400" height="300" src="{{asset('/files/inacap.mp4')}}" frameborder="0" autoplay allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="400" height="350" src="{{asset('/files/inacap.mp4')}}" frameborder="0" autoplay allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
                     <!---->
                 </div>
