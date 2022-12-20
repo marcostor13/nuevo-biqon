324551814d0cd3d6ec172571eb655bc0fc07057d
eduardo <jvostro1200@gmail.com>
2022-12-20 13:33:20 -0300

    Update vikingos.blade.php
    XSXSXSX

--- resources/views/landings/vikingos.blade.php
+++ resources/views/landings/vikingos.blade.php
@@ -19,9 +19,8 @@ $enddate=strtotime("+15 days", $startdate);
 
       <!-- <div class="content container-fluid d-flex flex-column align-items-center justify-content-center p-0 w-100">
             <img style="max-width: 60%" class="img-fluid col-md-3 mt-2" src="{{$landing->logo}}" alt="AMICAR">
-
+--->
             <div id="cont1" align="align-items-center align-content-center">
-    --->
 
 <div id="cont5" class="p-2 pl-3 pr-3 hide">
                         <div class="card">     
